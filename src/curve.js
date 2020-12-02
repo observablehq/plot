6248fdfb6d695ecac42fbc3b7b46278dce030a40
Mike Bostock <mbostock@gmail.com>
2020-12-02 13:18:22 -0800

    curve

--- src/curve.js
+++ src/curve.js
@@ -1,10 +1,49 @@
-import {curveBasis, curveLinear, curveStep} from "d3-shape";
+import {
+  curveBasis,
+  curveBasisClosed,
+  curveBasisOpen,
+  curveBundle,
+  curveCardinal,
+  curveCardinalClosed,
+  curveCardinalOpen,
+  curveCatmullRom,
+  curveCatmullRomClosed,
+  curveCatmullRomOpen,
+  curveLinear,
+  curveLinearClosed,
+  curveMonotoneX,
+  curveMonotoneY,
+  curveNatural,
+  curveStep,
+  curveStepAfter,
+  curveStepBefore
+} from "d3-shape";
 
+const curves = new Map([
+  ["basis", curveBasis],
+  ["basis-closed", curveBasisClosed],
+  ["basis-open", curveBasisOpen],
+  ["bundle", curveBundle],
+  ["cardinal", curveCardinal],
+  ["cardinal-closed", curveCardinalClosed],
+  ["cardinal-open", curveCardinalOpen],
+  ["catmull-rom", curveCatmullRom],
+  ["catmull-rom-closed", curveCatmullRomClosed],
+  ["catmull-rom-open", curveCatmullRomOpen],
+  ["linear", curveLinear],
+  ["linear-closed", curveLinearClosed],
+  ["monotone-x", curveMonotoneX],
+  ["monotone-y", curveMonotoneY],
+  ["natural", curveNatural],
+  ["step", curveStep],
+  ["step-after", curveStepAfter],
+  ["step-before", curveStepBefore]
+]);
+
+// TODO Tension and other parameters (e.g., curve: ["cardinal", 1])?
 export function Curve(curve = "linear") {
-  switch (curve) {
-    case "basis": return curveBasis;
-    case "step": return curveStep;
-    case "linear": return curveLinear;
-    default: throw new Error(`unknown curve: ${curve}`);
-  }
+  if (curve && typeof curve.point === "function") return curve; // custom curve
+  const c = (curve + "").toLowerCase();
+  if (!curves.has(c)) throw new Error(`unknown curve: ${c}`);
+  return curves.get(c);
 }
