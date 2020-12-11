b69e250fd164ac46a99af4fe70b2ad5f09fba7a1
Mike Bostock <mbostock@gmail.com>
2020-12-10 23:19:06 -0800

    no undefined group

--- src/transforms/group.js
+++ src/transforms/group.js
@@ -1,9 +1,10 @@
-import {group, groups} from "d3-array";
+import {groups} from "d3-array";
+import {defined} from "../defined.js";
 import {field} from "../mark.js";
 
 export function group1(key) {
   if (typeof key !== "function") key = field(key + "");
-  return data => group(data, key);
+  return data => groups(data, key).filter(defined1);
 }
 
 export function group2(x, y) {
@@ -12,4 +13,11 @@ export function group2(x, y) {
   return data => groups(data, x, y)
     .flatMap(([x, xgroup]) => xgroup
     .map(([y, ygroup]) => [x, y, ygroup]));
+}
+
+// Since marks don’t render when channel values are undefined (or null or NaN),
+// we apply the same logic when grouping. If you want to preserve the group for
+// undefined data, map it to an “other” value first.
+function defined1([key]) {
+  return defined(key);
 }
