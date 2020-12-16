9582683cae113702a9b346e408a4e3992e457041
Mike Bostock <mbostock@gmail.com>
2020-12-15 18:08:02 -0800

    span

--- src/marks/bar.js
+++ src/marks/bar.js
@@ -1,7 +1,7 @@
 import {ascending} from "d3-array";
 import {create} from "d3-selection";
 import {filter} from "../defined.js";
-import {Mark, number, maybeColor, maybeZero, identity, indexOf, zero} from "../mark.js";
+import {Mark, number, maybeColor, maybeZero, indexOf} from "../mark.js";
 import {Style, applyDirectStyles, applyIndirectStyles, applyBandTransform} from "../style.js";
 
 export class AbstractBar extends Mark {
@@ -60,73 +60,79 @@ export class AbstractBar extends Mark {
   }
 }
 
-export class BarX extends AbstractBar {
-  constructor(data, {x1, x2, y, ...options} = {}) {
-    super(
-      data,
-      [
-        {name: "x1", value: x1, scale: "x"},
-        {name: "x2", value: x2, scale: "x"},
-        {name: "y", value: y, scale: "y", type: "band", optional: true}
-      ],
-      options
-    );
-  }
+export class AbstractBarX extends AbstractBar {
   _transform(selection, {x}) {
     selection.call(applyBandTransform, x, false);
-  }
-  _positions({x1: X1, x2: X2, y: Y}) {
-    return [X1, X2, Y];
   }
   _x({x}, {x1: X1, x2: X2}) {
     const {insetLeft} = this;
     return i => Math.min(x(X1[i]), x(X2[i])) + insetLeft;
   }
-  _y({y}, {y: Y}, {marginTop}) {
-    const {insetTop} = this;
-    return Y ? i => y(Y[i]) + insetTop : marginTop + insetTop;
-  }
   _width({x}, {x1: X1, x2: X2}) {
     const {insetLeft, insetRight} = this;
     return i => Math.max(0, Math.abs(x(X2[i]) - x(X1[i])) - insetLeft - insetRight);
   }
-  _height({y}, {y: Y}, {marginTop, marginBottom, height}) {
+}
+
+export class BarX extends AbstractBarX {
+  constructor(data, {x1, x2, y = indexOf, ...options} = {}) {
+    super(
+      data,
+      [
+        {name: "x1", value: x1, scale: "x"},
+        {name: "x2", value: x2, scale: "x"},
+        {name: "y", value: y, scale: "y", type: "band"}
+      ],
+      options
+    );
+  }
+  _positions({x1: X1, x2: X2, y: Y}) {
+    return [X1, X2, Y];
+  }
+  _y({y}, {y: Y}) {
+    const {insetTop} = this;
+    return i => y(Y[i]) + insetTop;
+  }
+  _height({y}) {
     const {insetTop, insetBottom} = this;
-    const bandwidth = Y ? y.bandwidth() : height - marginTop - marginBottom;
+    const bandwidth = y.bandwidth();
     return Math.max(0, bandwidth - insetTop - insetBottom);
   }
 }
 
-export class BarY extends AbstractBar {
-  constructor(data, {x, y1, y2, ...options} = {}) {
+export class AbstractBarY extends AbstractBar {
+  _transform(selection, {y}) {
+    selection.call(applyBandTransform, false, y);
+  }
+  _x({x}, {x: X}) {
+    const {insetLeft} = this;
+    return i => x(X[i]) + insetLeft;
+  }
+  _width({x}) {
+    const {insetLeft, insetRight} = this;
+    const bandwidth = x.bandwidth();
+    return Math.max(0, bandwidth - insetLeft - insetRight);
+  }
+}
+
+export class BarY extends AbstractBarY {
+  constructor(data, {x = indexOf, y1, y2, ...options} = {}) {
     super(
       data,
       [
         {name: "y1", value: y1, scale: "y"},
         {name: "y2", value: y2, scale: "y"},
-        {name: "x", value: x, scale: "x", type: "band", optional: true}
+        {name: "x", value: x, scale: "x", type: "band"}
       ],
       options
     );
   }
-  _transform(selection, {y}) {
-    selection.call(applyBandTransform, false, y);
-  }
   _positions({y1: Y1, y2: Y2, x: X}) {
     return [Y1, Y2, X];
-  }
-  _x({x}, {x: X}, {marginLeft}) {
-    const {insetLeft} = this;
-    return X ? i => x(X[i]) + insetLeft : marginLeft + insetLeft;
   }
   _y({y}, {y1: Y1, y2: Y2}) {
     const {insetTop} = this;
     return i => Math.min(y(Y1[i]), y(Y2[i])) + insetTop;
-  }
-  _width({x}, {x: X}, {marginRight, marginLeft, width}) {
-    const {insetLeft, insetRight} = this;
-    const bandwidth = X ? x.bandwidth() : width - marginRight - marginLeft;
-    return Math.max(0, bandwidth - insetLeft - insetRight);
   }
   _height({y}, {y1: Y1, y2: Y2}) {
     const {insetTop, insetBottom} = this;
@@ -134,20 +140,12 @@ export class BarY extends AbstractBar {
   }
 }
 
-export function barX(data, {x, x1, x2, y, ...options} = {}) {
-  if (x === undefined && x1 === x && x2 === x && y === x) {
-    x1 = zero, x2 = identity, y = indexOf;
-  } else {
-    ([x1, x2] = maybeZero(x, x1, x2));
-  }
-  return new BarX(data, {...options, x1, x2, y});
+export function barX(data, {x, x1, x2, ...options} = {}) {
+  ([x1, x2] = maybeZero(x, x1, x2));
+  return new BarX(data, {...options, x1, x2});
 }
 
-export function barY(data, {x, y, y1, y2, ...options} = {}) {
-  if (y === undefined && y1 === y && y2 === y && x === y) {
-    y1 = zero, y2 = identity, x = indexOf;
-  } else {
-    ([y1, y2] = maybeZero(y, y1, y2));
-  }
-  return new BarY(data, {...options, x, y1, y2});
+export function barY(data, {y, y1, y2, ...options} = {}) {
+  ([y1, y2] = maybeZero(y, y1, y2));
+  return new BarY(data, {...options, y1, y2});
 }
---
+++ src/marks/span.js
@@ -0,0 +1,63 @@
+import {maybeZero} from "../mark.js";
+import {applyBandTransform} from "../style.js";
+import {AbstractBarX, AbstractBarY} from "./bar.js";
+
+export class SpanX extends AbstractBarX {
+  constructor(data, {x1, x2, ...options} = {}) {
+    super(
+      data,
+      [
+        {name: "x1", value: x1, scale: "x"},
+        {name: "x2", value: x2, scale: "x"}
+      ],
+      options
+    );
+  }
+  _positions({x1: X1, x2: X2}) {
+    return [X1, X2];
+  }
+  _y(scales, channels, {marginTop}) {
+    const {insetTop} = this;
+    return marginTop + insetTop;
+  }
+  _height(scales, channels, {marginTop, marginBottom, height}) {
+    const {insetTop, insetBottom} = this;
+    const bandwidth = height - marginTop - marginBottom;
+    return Math.max(0, bandwidth - insetTop - insetBottom);
+  }
+}
+
+export class SpanY extends AbstractBarY {
+  constructor(data, {y1, y2, ...options} = {}) {
+    super(
+      data,
+      [
+        {name: "y1", value: y1, scale: "y"},
+        {name: "y2", value: y2, scale: "y"}
+      ],
+      options
+    );
+  }
+  _positions({y1: Y1, y2: Y2}) {
+    return [Y1, Y2];
+  }
+  _x(scales, channels, {marginLeft}) {
+    const {insetLeft} = this;
+    return marginLeft + insetLeft;
+  }
+  _width(scales, channels, {marginRight, marginLeft, width}) {
+    const {insetLeft, insetRight} = this;
+    const bandwidth = width - marginRight - marginLeft;
+    return Math.max(0, bandwidth - insetLeft - insetRight);
+  }
+}
+
+export function spanX(data, {x, x1, x2, ...options} = {}) {
+  ([x1, x2] = maybeZero(x, x1, x2));
+  return new SpanX(data, {...options, x1, x2});
+}
+
+export function spanY(data, {y, y1, y2, ...options} = {}) {
+  ([y1, y2] = maybeZero(y, y1, y2));
+  return new SpanY(data, {...options, y1, y2});
+}
