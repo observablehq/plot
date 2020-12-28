6ac2fc5441001c0a243c75f825c819acc2639c7c
Philippe Rivière <fil@rezo.net>
2020-12-28 11:36:07 +0100

    faceted groups

--- test/plots/index.js
+++ test/plots/index.js
@@ -29,6 +29,7 @@ export {default as penguinCulmen} from "./penguin-culmen.js";
 export {default as penguinMassSexSpecies} from "./penguin-mass-sex-species.js";
 export {default as penguinMassSex} from "./penguin-mass-sex.js";
 export {default as penguinMass} from "./penguin-mass.js";
+export {default as penguinFaceted} from "./penguin-faceted.js";
 export {default as randomWalk} from "./random-walk.js";
 export {default as seattleTemperatureBand} from "./seattle-temperature-band.js";
 export {default as sfTemperatureBand} from "./sf-temperature-band.js";
