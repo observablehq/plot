3c73c80ab874a17315cfb94ce7aafce8255f98c3
Mike Bostock <mbostock@gmail.com>
2020-12-31 09:45:17 -0800

    tape -r esm

--- test/cjs-test.js
+++ test/cjs-test.js
@@ -1,5 +1,5 @@
-const Plot = require("../");
-const tape = require("tape-await");
+import * as Plot from "@observablehq/plot";
+import tape from "tape-await";
 
 tape("exports Frame", test => {
   test.ok(Object.keys(Plot).includes("plot"));
