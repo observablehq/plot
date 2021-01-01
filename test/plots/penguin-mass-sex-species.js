646040c2b4cf2b1b22092ee967a26a04f782cc2d
Mike Bostock <mbostock@gmail.com>
2020-12-31 16:17:35 -0800

    penguinMassSexSpecies

---
+++ test/plots/penguin-mass-sex-species.js
@@ -0,0 +1,22 @@
+import * as Plot from "@observablehq/plot";
+import * as d3 from "d3";
+
+export default async function() {
+  const data = await d3.csv("data/penguins.csv", d3.autoType);
+  return Plot.plot({
+    marginLeft: 70,
+    x: {
+      round: true,
+      label: "Body mass (g) →"
+    },
+    facet: {
+      data,
+      x: "sex",
+      y: "species"
+    },
+    marks: [
+      Plot.binX(data, {x: "body_mass_g"}),
+      Plot.ruleY([0])
+    ]
+  });
+}
