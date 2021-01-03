2718d16f91187cffdbdd8c0feeece1058e03fa1b
Mike Bostock <mbostock@gmail.com>
2021-01-02 16:47:43 -0800

    fix bin2

--- src/transforms/bin.js
+++ src/transforms/bin.js
@@ -1,26 +1,12 @@
-import {bin as binner, cross, sum} from "d3-array";
+import {bin as binner, cross} from "d3-array";
 import {valueof, first, second, maybeValue, range, offsetRange} from "../mark.js";
 
 export function bin1(options = {}) {
   let {value, domain, thresholds, cumulative} = maybeValue(options);
+  const bin = binof({value, domain, thresholds});
   return (data, facets) => {
-    const values = valueof(data, value);
-    const bin = binner().value(i => values[i]);
-    if (domain !== undefined) bin.domain(domain);
-    if (thresholds !== undefined) bin.thresholds(thresholds);
-    let bins = bin(range(values));
-    if (facets !== undefined) {
-      const index = [];
-      const data = [];
-      for (const facet of facets.map(set)) {
-        let b = bins.map(bin => binsubset(bin, facet));
-        b = cumulative ? accumulate(cumulative < 0 ? b.reverse() : b) : b;
-        b = b.filter(nonempty);
-        index.push(offsetRange(b, data.length));
-        data.push(...b); // TODO optimize
-      }
-      return {index, data};
-    }
+    let bins = bin(data);
+    if (facets !== undefined) return binfacets(bins, facets, cumulative);
     if (cumulative) bins = accumulate(cumulative < 0 ? bins.reverse() : bins);
     bins = bins.filter(nonempty);
     return {index: range(bins), data: bins};
@@ -28,23 +14,41 @@ export function bin1(options = {}) {
 }
 
 export function bin2({x = {}, y = {}, domain, thresholds} = {}) {
-  const binX = bin1({domain, thresholds, value: first, ...maybeValue(x)});
-  const binY = bin1({domain, thresholds, value: second, ...maybeValue(y)});
-  return (...args) => {
-    return cross(binX(...args), binY(...args).map(binset), (x, y) => {
-      return {
-        x0: x.x0,
-        x1: x.x1,
-        y0: y.x0,
-        y1: y.x1,
-        length: sum(x, d => y.has(d))
-      };
-    }).filter(nonempty);
+  const binX = binof({domain, thresholds, value: first, ...maybeValue(x)});
+  const binY = binof({domain, thresholds, value: second, ...maybeValue(y)});
+  return (data, facets) => {
+    let bins = cross(binX(data).filter(nonempty), binY(data).filter(nonempty).map(binset), binsubset);
+    if (facets !== undefined) return binfacets(bins, facets);
+    bins = bins.filter(nonempty);
+    return {index: range(bins), data: bins};
   };
 }
 
 function set(index) {
   return new Set(index);
+}
+
+function binof({value, domain, thresholds}) {
+  return data => {
+    const values = valueof(data, value);
+    const bin = binner().value(i => values[i]);
+    if (domain !== undefined) bin.domain(domain);
+    if (thresholds !== undefined) bin.thresholds(thresholds);
+    return bin(range(values));
+  };
+}
+
+function binfacets(bins, facets, cumulative) {
+  const index = [];
+  const data = [];
+  for (const facet of facets.map(set)) {
+    let b = bins.map(bin => binsubset(bin, facet));
+    b = cumulative ? accumulate(cumulative < 0 ? b.reverse() : b) : b;
+    b = b.filter(nonempty);
+    index.push(offsetRange(b, data.length));
+    data.push(b);
+  }
+  return {index, data: data.flat()};
 }
 
 function binset(bin) {
