a8c2f23d06bb63808581fb129cd2d705c2d28aca
Mike Bostock <mbostock@gmail.com>
2021-01-28 09:42:29 -0800

    style object

--- src/style.js
+++ src/style.js
@@ -1,81 +1,80 @@
 import {string, number} from "./mark.js";
 
-function attr(key, name, impliedValue) {
-  return new AttrProperty(key, name, impliedValue);
+function attr(name, impliedValue) {
+  return new AttrProperty(name, impliedValue);
 }
 
-function style(key, name, impliedValue) {
-  return new StyleProperty(key, name, impliedValue);
+function style(name, impliedValue) {
+  return new StyleProperty(name, impliedValue);
 }
 
 class Property {
-  constructor(key, name, impliedValue) {
-    this.key = key;
+  constructor(name, impliedValue = null) {
     this.name = name;
     this.impliedValue = impliedValue;
     this.type = typeof impliedValue === "number" ? number : string;
   }
-  define(mark, style) {
-    const value = this.type(style[this.key]);
-    mark[this.key] = value === this.impliedValue ? undefined : value;
+  value(value) {
+    value = this.type(value);
+    return value === this.impliedValue ? undefined : value;
   }
 }
 
 class AttrProperty extends Property {
-  apply(mark, selection) {
-    applyAttr(selection, this.name, mark[this.key]);
+  apply(selection, value) {
+    applyAttr(selection, this.name, value);
   }
 }
 
 class StyleProperty extends Property {
-  apply(mark, selection) {
-    applyStyle(selection, this.name, mark[this.key]);
+  apply(selection, value) {
+    applyStyle(selection, this.name, value);
   }
 }
 
-const indirectProperties = [
-  attr("fill", "fill", "currentColor"),
-  attr("fillOpacity", "fill-opacity", 1),
-  attr("stroke", "stroke", "none"),
-  attr("strokeWidth", "stroke-width", 1),
-  attr("strokeOpacity", "stroke-opacity", 1),
-  attr("strokeLinejoin", "stroke-linejoin", "miter"),
-  attr("strokeLinecap", "stroke-linecap", "butt"),
-  attr("strokeMiterlimit", "stroke-miterlimit", 1),
-  attr("strokeDasharray", "stroke-dasharray", null),
-  attr("fontFamily", "font-family", "sans-serif"),
-  attr("fontSize", "font-size", "10px"),
-  attr("fontSizeAdjust", "font-size-adjust", "none"),
-  attr("fontStretch", "font-stretch", "normal"),
-  attr("fontStyle", "font-style", "normal"),
-  attr("fontVariant", "font-variant", "normal"),
-  attr("fontWeight", "font-weight", "normal")
-];
+const indirectProperties = {
+  fill: attr("fill", "currentColor"),
+  fillOpacity: attr("fill-opacity", 1),
+  stroke: attr("stroke", "none"),
+  strokeWidth: attr("stroke-width", 1),
+  strokeOpacity: attr("stroke-opacity", 1),
+  strokeLinejoin: attr("stroke-linejoin", "miter"),
+  strokeLinecap: attr("stroke-linecap", "butt"),
+  strokeMiterlimit: attr("stroke-miterlimit", 1),
+  strokeDasharray: attr("stroke-dasharray"),
+  fontFamily: attr("font-family", "sans-serif"),
+  fontSize: attr("font-size", "10px"),
+  fontSizeAdjust: attr("font-size-adjust", "none"),
+  fontStretch: attr("font-stretch", "normal"),
+  fontStyle: attr("font-style", "normal"),
+  fontVariant: attr("font-variant", "normal"),
+  fontWeight: attr("font-weight", "normal")
+};
 
-const directProperties = [
-  style("mixBlendMode", "mix-blend-mode", "normal")
-];
+const directProperties = {
+  mixBlendMode: style("mix-blend-mode", "normal")
+};
 
-const properties = [
+const properties = {
   ...directProperties,
   ...indirectProperties
-];
+};
 
 export function Style(mark, style = {}) {
-  for (const property of properties) {
-    property.define(mark, style);
+  for (const key in properties) {
+    mark[key] = properties[key].value(style[key]);
   }
 }
 
 export function applyIndirectStyles(selection, mark) {
-  for (const property of indirectProperties) {
-    property.apply(mark, selection);
+  for (const key in indirectProperties) {
+    indirectProperties[key].apply(selection, mark[key]);
   }
 }
 
 export function applyDirectStyles(selection, mark) {
-  for (const property of directProperties) {
-    property.apply(mark, selection);
+  for (const key in directProperties) {
+    directProperties[key].apply(selection, mark[key]);
   }
 }
 
