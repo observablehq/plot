c625ba5de10be14a57b386e0a57d0b540ca3d4b1
Mike Bostock <mbostock@gmail.com>
2021-02-16 10:33:43 -0800

    fix #122; Frame inset

--- src/marks/frame.js
+++ src/marks/frame.js
@@ -1,15 +1,24 @@
 import {create} from "d3-selection";
-import {Mark} from "../mark.js";
+import {Mark, number} from "../mark.js";
 import {Style, applyDirectStyles, applyIndirectStyles} from "../style.js";
 
 export class Frame extends Mark {
   constructor({
     fill = "none",
-    stroke = fill == null || fill === "none" ? "currentColor" : "none",
+    stroke = fill === null || fill === "none" ? "currentColor" : "none",
+    inset = stroke === null || stroke === "none" ? 0 : 0.5,
+    insetTop = inset,
+    insetRight = inset,
+    insetBottom = inset,
+    insetLeft = inset,
     ...style
   } = {}) {
     super();
     Style(this, {fill, stroke, ...style});
+    this.insetTop = number(insetTop);
+    this.insetRight = number(insetRight);
+    this.insetBottom = number(insetBottom);
+    this.insetLeft = number(insetLeft);
   }
   render(
     index,
@@ -17,15 +26,13 @@ export class Frame extends Mark {
     channels,
     {marginTop, marginRight, marginBottom, marginLeft, width, height}
   ) {
-    const {stroke} = this;
-    const offset = stroke === "none" ? 0 : 0.5; // crisp edges
     return create("svg:rect")
         .call(applyIndirectStyles, this)
         .call(applyDirectStyles, this)
-        .attr("x", marginLeft + offset)
-        .attr("y", marginTop + offset)
-        .attr("width", width - marginLeft - marginRight)
-        .attr("height", height - marginTop - marginBottom)
+        .attr("x", marginLeft + this.insetLeft)
+        .attr("y", marginTop + this.insetTop)
+        .attr("width", width - marginLeft - marginRight - this.insetLeft - this.insetRight)
+        .attr("height", height - marginTop - marginBottom - this.insetTop - this.insetBottom)
       .node();
   }
 }
--- test/plots/empty.js
+++ test/plots/empty.js
@@ -9,6 +9,9 @@ export default async function() {
     },
     y: {
       domain: [0, 1]
-    }
+    },
+    marks: [
+      Plot.frame()
+    ]
   });
 }
