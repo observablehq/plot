c6587476c078c5e678a83eedc06c0b736d096108
Mike Bostock <mbostock@gmail.com>
2021-03-02 11:23:48 -0800

    redesign musicRevenue

--- test/plots/music-revenue.js
+++ test/plots/music-revenue.js
@@ -3,21 +3,20 @@ import * as d3 from "d3";
 
 export default async function() {
   const data = await d3.csv("data/riaa-us-revenue.csv", d3.autoType);
+  const stack = {x: "year", y: "revenue", z: "format", rank: "appearance", reverse: true};
   return Plot.plot({
+    x: {
+      label: null
+    },
     y: {
-      axis: null
+      grid: true,
+      label: "↑ Annual revenue (billions, adj.)",
+      transform: d => d / 1000
     },
     marks: [
-      Plot.stackAreaY(data, {
-        x: "year",
-        z: "format",
-        y: "revenue",
-        offset: "wiggle",
-        fill: "group",
-        curve: "cardinal",
-        stroke: "white",
-        title: "format"
-      })
+      Plot.stackAreaY(data, {...stack, fill: "group", title: d => `${d.format}\n${d.group}`}),
+      Plot.lineY(data, Plot.stackY2({...stack, stroke: "white", strokeWidth: 1})),
+      Plot.ruleY([0])
     ]
   });
 }
