87f99f1dedba502323523c21bc02179d89beff58
Mike Bostock <mbostock@gmail.com>
2021-03-04 13:47:11 -0800

    add example

---
+++ test/plots/police-deaths-bar.js
@@ -0,0 +1,13 @@
+import * as Plot from "@observablehq/plot";
+import * as d3 from "d3";
+
+export default async function() {
+  const data = await d3.csv("data/police-deaths.csv", d3.autoType);
+  return Plot.plot({
+    marks: [
+      Plot.barX(data, Plot.stackX({x: "police", fill: "race"})),
+      Plot.textX(data, Plot.stackXMid({x: "police", z: "race", text: "race"})),
+      Plot.ruleX([0, 100])
+    ]
+  });
+}
