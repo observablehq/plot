5f679205270874eb3e75dc5005ff5c7df188b4e5
Mike Bostock <mbostock@gmail.com>
2021-03-06 09:58:09 -0800

    fix select, normalize

--- src/transforms/normalize.js
+++ src/transforms/normalize.js
@@ -1,43 +1,44 @@
 import {group} from "d3-array";
-import {lazyChannel, maybeComposeTransform, maybeZ, valueof} from "../mark.js";
+import {maybeComposeTransform, maybeLazyChannel, maybeZ, valueof} from "../mark.js";
 
 export function normalizeX({x, x1, x2, ...options} = {}) {
-  const [transform, X, X1, X2] = normalize(options, x, x1, x2);
+  const [transform, X, X1, X2] = normalize([x, x1, x2], options);
   return {...options, transform, x: X, x1: X1, x2: X2};
 }
 
 export function normalizeY({y, y1, y2, ...options} = {}) {
-  const [transform, Y, Y1, Y2] = normalize(options, y, y1, y2);
+  const [transform, Y, Y1, Y2] = normalize([y, y1, y2], options);
   return {...options, transform, y: Y, y1: Y1, y2: Y2};
 }
 
 // TODO If the first value is undefined, scan forward?
-function normalChange(I, V, N) {
-  const basis = +V[I[0]];
+function normalChange(I, S, T) {
+  const basis = +S[I[0]];
   for (const i of I) {
-    N[i] = V[i] / basis;
+    T[i] = S[i] / basis;
   }
 }
 
-function normalize({transform, ...options} = {}, ...keys) {
+function normalize(inputs, options) {
+  const channels = inputs.map(i => [i, ...maybeLazyChannel(i)]);
   const z = maybeZ(options);
-  const channels = (keys = keys.filter(k => k != null)).map(lazyChannel);
   const normal = normalChange; // TODO option
   return [
-    maybeComposeTransform(transform, (data, index) => {
+    maybeComposeTransform(options, (data, index) => {
       const n = data.length;
       const Z = valueof(data, z);
-      for (let i = 0; i < keys.length; ++i) {
-        const V = valueof(data, keys[i], Float64Array);
-        const N = channels[i][1](new Float64Array(n));
+      for (const [source,, setTarget] of channels) {
+        if (source == null) continue;
+        const S = valueof(data, source, Float64Array);
+        const T = setTarget(new Float64Array(n));
         for (const facet of index) {
           for (const I of Z ? group(facet, i => Z[i]).values() : [facet]) {
-            normal(I, V, N);
+            normal(I, S, T);
           }
         }
       }
       return {data, index};
     }),
-    ...channels.map(([channel]) => channel)
+    ...channels.map(([, target]) => target)
   ];
 }
--- src/transforms/select.js
+++ src/transforms/select.js
@@ -2,27 +2,27 @@ import {greatest, group, least} from "d3-array";
 import {maybeComposeTransform, maybeZ, valueof} from "../mark.js";
 
 export function selectFirst(options) {
-  return {...options, transform: select(options, first)};
+  return {...options, transform: select(first, undefined, options)};
 }
 
 export function selectLast(options) {
-  return {...options, transform: select(options, last)};
+  return {...options, transform: select(last, undefined, options)};
 }
 
 export function selectMinX({x, ...options} = {}) {
-  return {...options, x, transform: select(min, x)};
+  return {...options, x, transform: select(min, x, options)};
 }
 
 export function selectMinY({y, ...options} = {}) {
-  return {...options, y, transform: select(min, y)};
+  return {...options, y, transform: select(min, y, options)};
 }
 
 export function selectMaxX({x, ...options} = {}) {
-  return {...options, x, transform: select(max, x)};
+  return {...options, x, transform: select(max, x, options)};
 }
 
 export function selectMaxY({y, ...options} = {}) {
-  return {...options, y, transform: select(max, y)};
+  return {...options, y, transform: select(max, y, options)};
 }
 
 // TODO If the value (for some required channel) is undefined, scan forward?
@@ -43,16 +43,16 @@ function* max(I, X) {
   yield greatest(I, i => X[i]);
 }
 
-function select({transform, ...options} = {}, selector, v) {
+function select(selectIndex, v, options) {
   const z = maybeZ(options);
-  return maybeComposeTransform(transform, (data, index) => {
+  return maybeComposeTransform(options, (data, index) => {
     const Z = valueof(data, z);
     const V = valueof(data, v);
     const selectedIndex = [];
     for (const facet of index) {
       const selectedFacet = [];
       for (const I of Z ? group(facet, i => Z[i]).values() : [facet]) {
-        for (const i of selector(I, V)) {
+        for (const i of selectIndex(I, V)) {
           selectedFacet.push(i);
         }
       }
