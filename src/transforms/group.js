1faded5990a6a81e4e92d4a7d6dc858034cb1fe3
Mike Bostock <mbostock@gmail.com>
2021-03-06 12:02:48 -0800

    restore group transform

--- src/transforms/group.js
+++ src/transforms/group.js
@@ -1,65 +1,80 @@
 import {groups} from "d3-array";
 import {defined} from "../defined.js";
-import {valueof, range, offsetRange, maybeLabel, first, second, identity} from "../mark.js";
+import {valueof, range, offsetRange, maybeLabel, first, second, identity, maybeComposeTransform} from "../mark.js";
 
-export function groupX({x, normalize, ...options} = {}) {
-  const [transform, X, y] = group1(x, normalize);
-  return {...options, transform, x: X, y};
+export function groupX({x, ...options} = {}) {
+  const [transform, X, L] = group1(x, options);
+  return {...options, transform, x: X, y: L};
 }
 
-export function groupY({y, normalize, ...options} = {}) {
-  const [transform, Y, x] = group1(y, normalize);
-  return {...options, transform, y: Y, x};
+export function groupY({y, ...options} = {}) {
+  const [transform, Y, L] = group1(y, options);
+  return {...options, transform, y: Y, x: L};
 }
 
-function group1(x = identity, k) {
-  const y = (k = k === true ? 100 : +k) ? normalizedLength2(k) : length2;
+export function group({x, y, out, ...options} = {}) {
+  const [transform, X, Y, L] = group2(x, y, options);
+  return {...options, transform, x: X, y: Y, [out]: L};
+}
+
+function group1(x = identity, {normalize, ...options} = {}) {
+  normalize = normalize === true ? 100 : +normalize;
+  const y = normalize ? normalizedLength2(normalize) : length2;
   return [
-    (data, facets) => {
-      if (k) y.normalize(data);
+    maybeComposeTransform(options, (data, index) => {
+      if (normalize) y.normalize(data);
       const X = valueof(data, x);
-      let g = groups(range(data), i => X[i]).filter(defined1);
-      return regroup(g, facets);
-    },
+      return regroup(
+        groups(range(data), i => X[i]).filter(defined1),
+        index,
+        subset2,
+        nonempty2
+      );
+    }),
     maybeLabel(first, x),
     y
   ];
 }
 
-export function group({x = first, y = second, out, ...options} = {}) {
-  return {
-    ...options,
-    transform(data, facets) {
+function group2(x = first, y = second, options) {
+  return [
+    maybeComposeTransform(options, (data, index) => {
       const X = valueof(data, x);
       const Y = valueof(data, y);
-      let g = groups(range(data), i => X[i], i => Y[i]).filter(defined1);
-      g = g.flatMap(([x, xgroup]) => xgroup.filter(defined1).map(([y, ygroup]) => [x, y, ygroup]));
-      return regroup(g, facets);
-    },
-    x: maybeLabel(first, x),
-    y: maybeLabel(second, y),
-    [out]: length3
-  };
+      return regroup(
+        groups(range(data), i => X[i], i => Y[i]).filter(defined1).flatMap(([x, gx]) => gx.filter(defined1).map(([y, gy]) => [x, y, gy])),
+        index,
+        subset3,
+        nonempty3
+      );
+    }),
+    maybeLabel(first, x),
+    maybeLabel(second, y),
+    length3
+  ];
 }
 
 // When faceting, subdivides the given groups according to the facet indexes.
-function regroup(groups, facets) {
-  if (facets === undefined) return {index: range(groups), data: groups};
-  const index = [];
-  const data = [];
+function regroup(groups, index, subset, nonempty) {
+  const groupIndex = [];
+  const groupData = [];
   let k = 0;
-  for (const facet of facets.map(subset)) {
-    let g = groups.map(facet).filter(nonempty1);
-    index.push(offsetRange(g, k));
-    data.push(g);
-    k += g.length;
+  for (const facet of index) {
+    const g = groups.map(subset(facet)).filter(nonempty);
+    groupIndex.push(offsetRange(g, k));
+    k = groupData.push(...g);
   }
-  return {index, data: data.flat()};
+  return {data: groupData, index: groupIndex};
 }
 
-function subset(facet) {
+function subset2(facet) {
   const f = new Set(facet);
   return ([key, group]) => [key, group.filter(i => f.has(i))];
+}
+
+function subset3(facet) {
+  const f = new Set(facet);
+  return ([keyx, keyy, group]) => [keyx, keyy, group.filter(i => f.has(i))];
 }
 
 // Since marks don’t render when channel values are undefined (or null or NaN),
@@ -70,28 +85,32 @@ function defined1([key]) {
 }
 
 // When faceting, some groups may be empty; these are filtered out.
-function nonempty1([, {length}]) {
+function nonempty2([, {length}]) {
   return length > 0;
 }
 
-function length2([, group]) {
-  return group.length;
+function nonempty3([,, {length}]) {
+  return length > 0;
 }
 
-function length3([,, group]) {
-  return group.length;
+function length2([, {length}]) {
+  return length;
 }
 
-length2.label = length3.label = "Frequency";
+function length3([,, {length}]) {
+  return length;
+}
+
+length2.label = length2.label = "Frequency";
 
 // Returns a channel definition that’s the number of elements in the given group
 // (length2 above) as a proportion of the total number of elements in the data
 // scaled by k. If k is true, it is treated as 100 for percentages; otherwise,
 // it is typically 1.
 function normalizedLength2(k) {
-  let length; // set lazily by the transform
-  const value = ([, group]) => group.length * k / length;
-  value.normalize = data => void (length = data.length);
+  let n; // set lazily by the transform
+  const value = ([, {length}]) => length * k / n;
+  value.normalize = data => void (n = data.length);
   value.label = `Frequency${k === 100 ? " (%)" : ""}`;
   return value;
 }
