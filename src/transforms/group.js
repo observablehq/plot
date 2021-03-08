83da5336cf47d41b6866582a8b9faad2f4d24d7a
Mike Bostock <mbostock@gmail.com>
2021-03-08 13:34:53 -0800

    group1 sort by x

--- src/transforms/group.js
+++ src/transforms/group.js
@@ -1,6 +1,6 @@
-import {groups} from "d3-array";
+import {groups, group as grouper, sort} from "d3-array";
 import {defined} from "../defined.js";
-import {valueof, range, offsetRange, maybeLabel, first, second, identity, maybeTransform} from "../mark.js";
+import {valueof, range, offsetRange, maybeColor, maybeLabel, maybeTransform, maybeLazyChannel, lazyChannel, first, second, identity, take} from "../mark.js";
 
 export function groupX({x, ...options} = {}) {
   const [transform, X, y, z, fill, stroke] = group1(x, options);
@@ -33,7 +33,6 @@ function group1(x = identity, options = {}) {
       const Z = valueof(data, z);
       const F = valueof(data, vfill);
       const S = valueof(data, vstroke);
-      const B = groups(range(data), i => X[i]);
       const binIndex = [];
       const binData = [];
       const G = Z || F || S;
@@ -46,21 +45,17 @@ function group1(x = identity, options = {}) {
       let i = 0;
       for (const facet of index) {
         const binFacet = [];
-        for (const I of G ? group(facet, i => G[i]).values() : [facet]) {
-          const set = new Set(I);
-          for (const [x, b] of B) {
+        for (const I of G ? grouper(facet, i => G[i]).values() : [facet]) {
+          for (const [x, f] of sort(grouper(I, i => X[i]), first)) {
             if (!defined(x)) continue;
-            const f = b.filter(i => set.has(i));
             const l = f.length;
-            if (l > 0) {
-              binFacet.push(i++);
-              binData.push(take(data, f));
-              BX.push(x);
-              BY.push(k ? l * k / n : l);
-              if (Z) BZ.push(Z[f[0]]);
-              if (F) BF.push(F[f[0]]);
-              if (S) BS.push(S[f[0]]);
-            }
+            binFacet.push(i++);
+            binData.push(take(data, f));
+            BX.push(x);
+            BY.push(k ? l * k / n : l);
+            if (Z) BZ.push(Z[f[0]]);
+            if (F) BF.push(F[f[0]]);
+            if (S) BS.push(S[f[0]]);
           }
         }
         binIndex.push(binFacet);
@@ -73,21 +68,6 @@ function group1(x = identity, options = {}) {
     F,
     S
   ];
-  // const [y, normalizeY] = maybeNormalizeLength2(normalize);
-  // return [
-  //   maybeTransform(options, (data, index) => {
-  //     if (normalizeY) normalizeY(data);
-  //     const X = valueof(data, x);
-  //     return regroup(
-  //       groups(range(data), i => X[i]).filter(defined1),
-  //       index,
-  //       subset2,
-  //       nonempty2
-  //     );
-  //   }),
-  //   maybeLabel(first, x),
-  //   y
-  // ];
 }
 
 function group2(x = first, y = second, options) {
@@ -122,11 +102,6 @@ function regroup(groups, index, subset, nonempty) {
   return {data: groupData, index: groupIndex};
 }
 
-function subset2(facet) {
-  const f = new Set(facet);
-  return ([key, group]) => [key, group.filter(i => f.has(i))];
-}
-
 function subset3(facet) {
   const f = new Set(facet);
   return ([keyx, keyy, group]) => [keyx, keyy, group.filter(i => f.has(i))];
@@ -140,33 +115,12 @@ function defined1([key]) {
 }
 
 // When faceting, some groups may be empty; these are filtered out.
-function nonempty2([, {length}]) {
-  return length > 0;
-}
-
 function nonempty3([,, {length}]) {
   return length > 0;
-}
-
-function length2([, {length}]) {
-  return length;
 }
 
 function length3([,, {length}]) {
   return length;
 }
 
-length2.label = length3.label = "Frequency";
-
-// Returns a channel definition that’s the number of elements in the given group
-// (length2 above) as a proportion of the total number of elements in the data
-// scaled by k. If k is true, it is treated as 100 for percentages; otherwise,
-// it is typically 1.
-function maybeNormalizeLength2(normalize) {
-  const k = normalize === true ? 100 : +normalize;
-  if (!k) return [length2];
-  let n; // set lazily by the transform
-  const value = ([, {length}]) => length * k / n;
-  value.label = `Frequency${k === 100 ? " (%)" : ""}`;
-  return [value, ({length}) => void (n = length)];
-}
+length3.label = "Frequency";
