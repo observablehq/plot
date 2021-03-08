e34441b460e398db98efdafe8cbd6eef98cf0b4f
Philippe Rivière <fil@rezo.net>
2021-03-08 09:21:46 +0100

    generealize z-bin1 to z-bin2 example http://localhost:8008/?test=penguinSexMassCulmenSpecies
    remove unused functions

---
+++ test/plots/penguin-sex-mass-culmen-species.js
@@ -0,0 +1,27 @@
+import * as Plot from "@observablehq/plot";
+import * as d3 from "d3";
+
+export default async function() {
+  const data = await d3.csv("data/penguins.csv", d3.autoType);
+  
+  return Plot.plot({
+    facet: {
+      data,
+      x: "sex"
+    },
+    inset: 10,
+    marks: [
+      Plot.frame(),
+      Plot.dot(data, Plot.binR({
+        thresholds: 10,
+        x: "body_mass_g",
+        y: "culmen_length_mm",
+        stroke: "species",
+        fill: "species",
+        fillOpacity: .1
+      }))
+    ],
+    width: 610,
+    height: 300
+  });
+}
