9563a96049d32112bc340567c766d0b7dd3ff795
Mike Bostock <mbostock@gmail.com>
2021-03-09 11:01:44 -0800

    tweak usCongressAge example

--- test/plots/us-congress-age.js
+++ test/plots/us-congress-age.js
@@ -3,24 +3,23 @@ import * as d3 from "d3";
 
 export default async function() {
   const data = await d3.csv("data/us-congress-members.csv", d3.autoType);
+  const age = d => 2021 - d.birth;
+  const ageRange = d3.extent(data, age);
   return Plot.plot({
-    y: { axis: null },
+    height: 300,
     x: {
-      // a complicated way to get (min, max) inclusive
-      domain: d3.range(...d3.extent(data, d => 2021 - d.birth).map((d, i) => d + i)),
-      tickSize: 0,
-      grid: true
+      domain: d3.range(...ageRange.map((d, i) => d + i)),
+      ticks: d3.ticks(...ageRange, 10),
+      label: "Age →",
+      labelAnchor: "right"
+    },
+    y: {
+      grid: true,
+      label: "↑ Frequency"
     },
     marks: [
-      Plot.ruleX([d3.median(data, d => 2021 - d.birth)], { strokeDasharray: [2, 2] }),
-      Plot.stackBarY(data, {
-        x: d => 2021 - d.birth,
-        stroke: "white",
-        strokeWidth: 2,
-        title: "full_name"
-      })
-    ],
-    height: 400,
-    width: 950
+      Plot.dotY(data, Plot.stackY2({x: age, fill: "currentColor", title: "full_name"})),
+      Plot.ruleY([0])
+    ]
   });
 }
