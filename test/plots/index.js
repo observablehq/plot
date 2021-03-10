88fb482a0191d9f548987c4309214ae461688e87
Mike Bostock <mbostock@gmail.com>
2021-03-10 12:30:38 -0800

    add crimea examples

--- test/plots/index.js
+++ test/plots/index.js
@@ -7,6 +7,8 @@ export {default as ballotStatusRace} from "./ballot-status-race.js";
 export {default as beckerBarley} from "./becker-barley.js";
 export {default as caltrain} from "./caltrain.js";
 export {default as carsParcoords} from "./cars-parcoords.js";
+export {default as crimeanWarOverlapped} from "./crimean-war-overlapped.js";
+export {default as crimeanWarStacked} from "./crimean-war-stacked.js";
 export {default as covidIhmeProjectedDeaths} from "./covid-ihme-projected-deaths.js";
 export {default as d3Survey2015Comfort} from "./d3-survey-2015-comfort.js";
 export {default as d3Survey2015Why} from "./d3-survey-2015-why.js";
