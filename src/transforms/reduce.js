a4ba18cab0b66ccfc7d64dfec9358e33bd7eca6e
Mike Bostock <mbostock@gmail.com>
2021-03-12 10:59:54 -0800

    reduce data

--- src/transforms/reduce.js
+++ src/transforms/reduce.js
@@ -1,5 +1,5 @@
-import {group, min, max, median} from "d3-array";
-import {lazyChannel, maybeColor, maybeLazyChannel, maybeTransform, take, valueof} from "../mark.js";
+import {group, min, max, mean, median} from "d3";
+import {identity, lazyChannel, maybeColor, maybeLazyChannel, maybeTransform, take, valueof} from "../mark.js";
 
 // Group on x if present.
 export function reduceX(outputs, {x, ...options} = {}) {
@@ -21,7 +21,8 @@ export function reduce(outputs, options = {}) {
   return {...G && {[out]: G}, ...transform};
 }
 
-function reduce1(z, outputs, options) {
+function reduce1(z, {data: reduceData = identity, ...outputs} = {}, options) {
+  reduceData = maybeReduce(reduceData);
   const channels = Object.entries(outputs).map(([key, reduce]) => {
     const input = maybeInput(key, options);
     if (input == null) throw new Error(`missing channel: ${key}`);
@@ -33,24 +34,24 @@ function reduce1(z, outputs, options) {
     {
       ...options,
       transform: maybeTransform(options, (data, index) => {
-        const reduceIndex = [];
-        const reduceData = [];
+        const outIndex = [];
+        const outData = [];
         const X = channels.map(({input}) => valueof(data, input));
         const Z = valueof(data, z);
         const RX = channels.map(({setOutput}) => setOutput([]));
         const RZ = Z && setRZ([]);
         let i = 0;
         for (const facet of index) {
-          const reduceFacet = [];
+          const outFacet = [];
           for (const I of Z ? group(facet, i => Z[i]).values() : [facet]) {
-            reduceFacet.push(i++);
-            reduceData.push(take(data, I));
+            outFacet.push(i++);
+            outData.push(reduceData(I, data));
             channels.forEach(({reduce}, i) => RX[i].push(reduce(I, X[i])));
             if (Z) RZ.push(Z[I[0]]);
           }
-          reduceIndex.push(reduceFacet);
+          outIndex.push(outFacet);
         }
-        return {data: reduceData, index: reduceIndex};
+        return {data: outData, index: outIndex};
       }),
       ...Object.fromEntries(channels.map(({key, output}) => [key, output]))
     },
--- test/plots/morley-boxplot.js
+++ test/plots/morley-boxplot.js
@@ -3,16 +3,35 @@ import * as d3 from "d3";
 
 export default async function() {
   const morley = await d3.csv("data/morley.csv", d3.autoType);
+  const expts = d3.rollups(morley, data => box(data, d => d.Speed), d => d.Expt);
   return Plot.plot({
     x: {
       grid: true,
       inset: 6
     },
     marks: [
-      Plot.ruleY(morley, Plot.reduceY({x1: "min", x2: "max"}, {x: "Speed", y: "Expt"})),
-      Plot.barX(morley, Plot.reduceY({x1: x => d3.quantile(x, 0.25), x2: x => d3.quantile(x, 0.75)}, {x: "Speed", y: "Expt", fill: "#ccc"})),
-      Plot.tickX(morley, Plot.reduceY({x: "median"}, {x: "Speed", y: "Expt", strokeWidth: 2})),
-      Plot.dot(morley, {x: "Speed", y: "Expt"})
+      Plot.ruleY(expts, {x1: ([, b]) => b.r1, x2: ([, b]) => b.r2, y: ([expt]) => expt}),
+      Plot.barX(expts, {x1: ([, b]) => b.q1, x2: ([, b]) => b.q3, y: ([expt]) => expt, fill: "#ccc"}),
+      Plot.tickX(expts, {x: ([, b]) => b.q2, y: ([expt]) => expt, strokeWidth: 2}),
+      Plot.dot(expts.flatMap(([, b]) => b.outliers), {x: "Speed", y: "Expt"})
     ]
   });
+}
+
+function box(data, accessor = x => x) {
+  const values = Float64Array.from(data, accessor);
+  const q2 = d3.median(values);
+  const q1 = d3.quantile(values, 0.25); // first quartile
+  const q3 = d3.quantile(values, 0.75); // second quartile
+  const iqr = q3 - q1; // interquartile range
+  const r1 = Math.max(d3.min(values), q1 - iqr * 1.5);
+  const r2 = Math.min(d3.max(values), q3 + iqr * 1.5);
+  return {
+    q1,
+    q2,
+    q3,
+    r1,
+    r2,
+    outliers: data.filter((v, i) => values[i] < r1 || values[i] > r2)
+  };
 }
