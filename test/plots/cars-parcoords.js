ed2ecd5560fe505506395ce6d0a60fb7f6a79369
Mike Bostock <mbostock@gmail.com>
2021-03-12 16:31:43 -0800

    normalize parcoords

--- test/plots/cars-parcoords.js
+++ test/plots/cars-parcoords.js
@@ -2,19 +2,14 @@ import * as Plot from "@observablehq/plot";
 import * as d3 from "d3";
 
 export default async function() {
-  let data = await d3.csv("data/cars.csv", d3.autoType);
-  const dimensions = data.columns.slice(1);
+  const cars = await d3.csv("data/cars.csv", d3.autoType);
+  const dimensions = cars.columns.slice(1);
 
   // Reshape wide data to make it tidy.
   // TODO Instead of normalizing, separate scales for each dimension.
-  data = dimensions.flatMap(c => {
-    const scale = d3.scaleLinear().domain(d3.extent(data, d => d[c]));
-    return data.map(d => {
-      return {
-        name: d.name,
-        dimension: c,
-        value: d[c] == null ? NaN : scale(d[c])
-      };
+  const data = dimensions.flatMap(dimension => {
+    return cars.map(({name, [dimension]: value}) => {
+      return {name, dimension, value};
     });
   });
 
@@ -32,12 +27,16 @@ export default async function() {
         y: "dimension"
       }),
       Plot.line(data, {
-        x: "value",
-        y: "dimension",
-        z: "name",
-        stroke: "#444",
-        strokeWidth: 0.5,
-        strokeOpacity: 0.5
+        ...Plot.normalizeX({
+          basis: "extent",
+          x: "value",
+          y: "dimension",
+          z: "dimension",
+          stroke: "#444",
+          strokeWidth: 0.5,
+          strokeOpacity: 0.5
+        }),
+        z: "name"
       })
     ],
     marginLeft: 100
