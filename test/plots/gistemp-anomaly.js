415b3b04262e2d444a153a620eb7b956715781c2
Mike Bostock <mbostock@gmail.com>
2021-03-13 19:53:31 -0800

    BuRd gistemp

--- test/plots/gistemp-anomaly.js
+++ test/plots/gistemp-anomaly.js
@@ -10,7 +10,8 @@ export default async function() {
       grid: true
     },
     color: {
-      type: "diverging"
+      type: "diverging",
+      scheme: "BuRd"
     },
     marks: [
       Plot.ruleY([0]),
