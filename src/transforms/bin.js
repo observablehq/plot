54aded94cb1d5da305d1366303924e3e0fd2a9c9
Mike Bostock <mbostock@gmail.com>
2021-03-16 14:37:51 -0700

    smart out

--- src/transforms/bin.js
+++ src/transforms/bin.js
@@ -4,16 +4,16 @@ import {valueof, first, second, range, identity, lazyChannel, maybeLazyChannel, 
 import {offset} from "../style.js";
 
 // Group on y, z, fill, or stroke, if any, then bin on x.
-export function binX({x, out = "y", inset, insetLeft, insetRight, ...options} = {}) {
+export function binX({x, y, out = y == null ? "y" : "fill", inset, insetLeft, insetRight, ...options} = {}) {
   ([insetLeft, insetRight] = maybeInset(inset, insetLeft, insetRight));
-  const [transform, x1, x2, l] = bin1(x, "y", options);
+  const [transform, x1, x2, l] = bin1(x, "y", {y, ...options});
   return {x1, x2, ...transform, inset, insetLeft, insetRight, [out]: l};
 }
 
 // Group on x, z, fill, or stroke, if any, then bin on y.
-export function binY({y, out = "x", inset, insetTop, insetBottom, ...options} = {}) {
+export function binY({y, x, out = x == null ? "x" : "fill", inset, insetTop, insetBottom, ...options} = {}) {
   ([insetTop, insetBottom] = maybeInset(inset, insetTop, insetBottom));
-  const [transform, y1, y2, l] = bin1(y, "x", options);
+  const [transform, y1, y2, l] = bin1(y, "x", {x, ...options});
   return {y1, y2, ...transform, inset, insetTop, insetBottom, [out]: l};
 }
 
