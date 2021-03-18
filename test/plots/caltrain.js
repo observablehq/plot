64cca7aa44b64f3fd37a38d306691093c8f19192
Mike Bostock <mbostock@gmail.com>
2021-03-18 09:37:46 -0700

    tweak caltrain

--- test/plots/caltrain.js
+++ test/plots/caltrain.js
@@ -2,8 +2,9 @@ import * as Plot from "@observablehq/plot";
 import * as d3 from "d3";
 
 export default async function() {
-  const data = await d3.csv("data/caltrain.csv");
+  const caltrain = await d3.csv("data/caltrain.csv");
   return Plot.plot({
+    width: 240,
     x: {
       axis: null
     },
@@ -12,41 +13,39 @@ export default async function() {
       axis: null
     },
     color: {
-      domain: ["L", "B", "N"],
-      range: ["rgb(183, 116, 9)", "rgb(196, 62, 29)", "rgb(34, 34, 34)"]
+      domain: "NLB",
+      range: ["currentColor", "peru", "brown"]
     },
-    width: 240,
     marks: [
-      Plot.text([[1, 3]], {text: ["Northbound"], textAnchor: "start"}),
-      Plot.text([[-1, 3]], {text: ["Southbound"], textAnchor: "end"}),
-      Plot.text(
-        new Set(data.map(d => d.hours)),
-        {
-          x: () => 0,
-          y: d => d,
-          text: d => `${(d - 1) % 12 + 1}${(d % 24) >= 12 ? "p": "a"}`
-        }
-      ),
-      Plot.text(
-        data.filter(d => d.orientation === "N"),
-        Plot.stackX2({
-          x: () => 1,
-          y: "hours",
-          text: d => d.minutes.padStart(2, "0"),
-          fill: "type",
-          textAnchor: "start"
-        })
-      ),
-      Plot.text(
-        data.filter(d => d.orientation === "S"),
-        Plot.stackX2({
-          x: () => -1,
-          y: "hours",
-          text: d => d.minutes.padStart(2, "0"),
-          fill: "type",
-          textAnchor: "end"
-        })
-      ),
+      Plot.text([[1, 3]], {
+        text: ["Northbound"],
+        textAnchor: "start"
+      }),
+      Plot.text([[-1, 3]], {
+        text: ["Southbound"],
+        textAnchor: "end"
+      }),
+      Plot.text(new Set(caltrain.map(d => d.hours)), {
+        x: () => 0,
+        y: d => d,
+        text: d => `${(d - 1) % 12 + 1}${(d % 24) >= 12 ? "p": "a"}`
+      }),
+      Plot.text(caltrain, Plot.stackX2({
+        filter: d => d.orientation === "N",
+        x: () => 1,
+        y: "hours",
+        text: d => d.minutes.padStart(2, "0"),
+        fill: "type",
+        textAnchor: "start"
+      })),
+      Plot.text(caltrain, Plot.stackX2({
+        filter: d => d.orientation === "S",
+        x: () => -1,
+        y: "hours",
+        text: d => d.minutes.padStart(2, "0"),
+        fill: "type",
+        textAnchor: "end"
+      })),
       Plot.ruleX([-0.5, 0.5])
     ]
   });
