f09e5786f54fa51bf1dec23f0faf52329ce36b94
Philippe Rivière <fil@rezo.net>
2021-03-19 16:41:24 +0100

    new window reducers: deviation, sum, variance, difference, ratio

--- src/transforms/window.js
+++ src/transforms/window.js
@@ -1,5 +1,5 @@
 import {mapX, mapY} from "./map.js";
-import {max, mean, min} from "d3";
+import {deviation, max, mean, min, sum, variance} from "d3";
 
 export function windowX({k, reduce, shift, ...options} = {}) {
   return mapX(roll(k, reduce, shift), options);
@@ -10,36 +10,44 @@ export function windowY({k, reduce, shift, ...options} = {}) {
 }
 
 function roll(k, reduce = "mean", shift = "centered") {
-  switch (reduce) {
-    case "max": reduce = max; break;
-    case "mean": reduce = mean; break;
-    case "min": reduce = min; break;
+  let acceptnans = true;
+  if (typeof reduce === "string") {
+    switch (reduce.toLowerCase()) {
+      case "deviation": reduce = deviation; acceptnans = false; break;
+      case "max": reduce = max; acceptnans = false; break;
+      case "mean": reduce = mean; acceptnans = false; break;
+      case "min": reduce = min; acceptnans = false; break;
+      case "sum": reduce = sum; acceptnans = false; break;
+      case "variance": reduce = variance; acceptnans = false; break;
+      case "difference": reduce = difference; break;
+      case "ratio": reduce = ratio; break;
+    }
   }
   if (typeof reduce !== "function") throw new Error(`invalid reduce: ${reduce}`);
   if (!((k = Math.floor(k)) > 0)) throw new Error(`invalid k: ${k}`);
   shift = shift.toLowerCase();
-  const m1 = shift === "leading" ? 1
-           : shift === "trailing" ? k
-           : (k >> 1) + (k % 2);
+  const m1 = shift === "leading" ? 0
+           : shift === "trailing" ? k - 1
+           : (k >> 1) + (k % 2) - 1;
   return {
     map(I, S, T) {
-      const C = new Float64Array(k).fill(NaN);
-      let nans = k;
+      let v;
+      const C = Float64Array.from(I, i => (v = S[i]) === +v ? v : NaN);
       const n = I.length;
+      let nans = k;
       for (let i = 0; i < n; ++i) {
-        const v = C[i % k] = S[I[i]];
-        if (v === +v) {
-          nans--;
-        } else {
-          nans = k;
-        }
-        if (nans <= 0) {
-          const j = i - k;
-          if (j >= -1) {
-            T[I[j + m1]] = reduce(C);
-          }
+        if ((acceptnans || (nans = isNaN(C[i]) ? k : nans - 1) <= 0) && i >= k - 1) {
+          T[I[i - k + m1 + 1]] = reduce(C.subarray(i - k + 1, i + 1));
         }
       }
     }
   };
+  
+  function difference(A) {
+    return A[A.length - 1] - A[0];
+  }
+
+  function ratio(A) {
+    return A[A.length - 1] / A[0];
+  }
 }
