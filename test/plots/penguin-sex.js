a47168be107cbd7628e07264e05cb1848b665fd9
Mike Bostock <mbostock@gmail.com>
2021-03-28 11:31:13 -0700

    rename

--- test/plots/penguin-sex.js
+++ test/plots/penguin-sex.js
@@ -2,10 +2,10 @@ import * as Plot from "@observablehq/plot";
 import * as d3 from "d3";
 
 export default async function() {
-  const data = await d3.csv("data/penguins.csv", d3.autoType);
+  const penguins = await d3.csv("data/penguins.csv", d3.autoType);
   return Plot.plot({
     marks: [
-      Plot.barY(data, Plot.groupX({y: "count"}, {x: "sex"})),
+      Plot.barY(penguins, Plot.groupX({y: "count"}, {x: "sex"})),
       Plot.ruleY([0])
     ]
   });
