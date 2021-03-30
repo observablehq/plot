0d9225247931ffbbafcf1e8a93b1d0d8ea523d4e
Mike Bostock <mbostock@gmail.com>
2021-03-30 13:01:13 -0700

    default to en-US

--- src/format.js
+++ src/format.js
@@ -1,4 +1,4 @@
-export function formatMonth(locale, month = "short") {
+export function formatMonth(locale = "en-US", month = "short") {
   const format = new Intl.DateTimeFormat(locale, {timeZone: "UTC", month});
   return i => {
     if (i != null && !isNaN(i = new Date(Date.UTC(2000, +i)))) {
@@ -7,7 +7,7 @@ export function formatMonth(locale, month = "short") {
   };
 }
 
-export function formatWeekday(locale, weekday = "short") {
+export function formatWeekday(locale = "en-US", weekday = "short") {
   const format = new Intl.DateTimeFormat(locale, {timeZone: "UTC", weekday});
   return i => {
     if (i != null && !isNaN(i = new Date(Date.UTC(2001, 0, +i)))) {
