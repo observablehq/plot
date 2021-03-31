650529ab865e2e403c1147c77c5371ec6a71e843
Mike Bostock <mbostock@gmail.com>
2021-03-31 13:38:42 -0700

    simplify example

--- test/plots/penguin-species-island-sex.js
+++ test/plots/penguin-species-island-sex.js
@@ -2,27 +2,20 @@ import * as Plot from "@observablehq/plot";
 import * as d3 from "d3";
 
 export default async function() {
-  const data = await d3.csv("data/penguins.csv", d3.autoType);
+  const penguins = await d3.csv("data/penguins.csv", d3.autoType);
   return Plot.plot({
-    facet: {
-      data,
-      x: "species"
-    },
-    fx: {
-      domain: d3.groupSort(data, ({length}) => length, d => d.species).reverse()
-    },
     x: {
-      domain: ["MALE", "FEMALE", null],
       tickFormat: d => d === null ? "N/A" : d
     },
     y: {
       grid: true
     },
-    color: {
-      scheme: "greys"
+    facet: {
+      data: penguins,
+      x: "species"
     },
     marks: [
-      Plot.barY(data, Plot.stackY(Plot.groupX({y: "count"}, {x: "sex", fill: "island", stroke: "black"}))),
+      Plot.barY(penguins, Plot.stackY(Plot.groupX({y: "count"}, {x: "sex", fill: "island"}))),
       Plot.ruleY([0])
     ]
   });
