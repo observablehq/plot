8b4709ec0d7371a8c01ec36b29d9b9bdacc550da
Mike Bostock <mbostock@gmail.com>
2021-04-08 13:53:08 -0700

    identity test

--- test/plots/identity-scale.js
+++ test/plots/identity-scale.js
@@ -1,4 +1,7 @@
 import * as Plot from "@observablehq/plot";
+import * as d3 from "d3";
+
+const random = d3.randomLcg(42);
 
 export default async function() {
   return Plot.plot({
@@ -14,8 +17,8 @@ export default async function() {
     },
     marks: [
       Plot.dot({length: 100}, {
-        x: () => 600 * Math.random(),
-        y: () => 100 + 500 * Math.random(),
+        x: () => 600 * random(),
+        y: () => 100 + 500 * random(),
         fill: () => "red",
         stroke: () => "blue"
       })
