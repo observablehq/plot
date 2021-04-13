e3722e0838eb877750872a99e373320f11fd9d4e
Mike Bostock <mbostock@gmail.com>
2021-04-13 16:57:30 -0700

    reduce data if no input channel

---
+++ test/plots/athletes-sport-sex.js
@@ -0,0 +1,25 @@
+import * as Plot from "@observablehq/plot";
+import * as d3 from "d3";
+
+export default async function() {
+  const athletes = await d3.csv("data/athletes.csv", d3.autoType);
+  const female = data => d3.mean(data, d => d.sex === "female");
+  return Plot.plot({
+    marginLeft: 100,
+    height: 500,
+    x: {
+      label: "Women (%) →",
+      domain: [0, 100],
+      ticks: 10,
+      percent: true,
+      grid: true
+    },
+    y: {
+      label: null,
+      domain: d3.groupSort(athletes, female, d => d.sport)
+    },
+    marks: [
+      Plot.barX(athletes, Plot.groupY({x: female}, {y: "sport"}))
+    ]
+  });
+}
--- test/plots/index.js
+++ test/plots/index.js
@@ -9,6 +9,7 @@ export {default as athletesHeightWeightBinStroke} from "./athletes-height-weight
 export {default as athletesHeightWeightSex} from "./athletes-height-weight-sex.js";
 export {default as athletesNationality} from "./athletes-nationality.js";
 export {default as athletesSexWeight} from "./athletes-sex-weight.js";
+export {default as athletesSportSex} from "./athletes-sport-sex.js";
 export {default as athletesSportWeight} from "./athletes-sport-weight.js";
 export {default as athletesWeight} from "./athletes-weight.js";
 export {default as athletesWeightCumulative} from "./athletes-weight-cumulative.js";
