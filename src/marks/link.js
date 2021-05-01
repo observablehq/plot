d964221f1cee5293a572a8125d2ee7538b15b0ea
Philippe Rivière <fil@rezo.net>
2021-05-01 19:02:45 +0200

    use path directly

--- src/marks/link.js
+++ src/marks/link.js
@@ -1,4 +1,4 @@
-import {create, line as shapeLine} from "d3";
+import {create, path} from "d3";
 import {filter} from "../defined.js";
 import {Mark, maybeColor, maybeNumber, title} from "../mark.js";
 import {Curve} from "../curve.js";
@@ -57,7 +57,6 @@ export class Link extends Mark {
     {x1: X1, y1: Y1, x2: X2, y2: Y2, title: L, stroke: S, strokeOpacity: SO}
   ) {
     const index = filter(I, X1, Y1, X2, Y2, S, SO);
-    const line = shapeLine().curve(this.curve);
     return create("svg:g")
         .call(applyIndirectStyles, this)
         .call(applyTransform, x, y, 0.5, 0.5)
@@ -65,7 +64,15 @@ export class Link extends Mark {
           .data(index)
           .join("path")
             .call(applyDirectStyles, this)
-            .attr("d", i => line([[X1[i], Y1[i]], [X2[i], Y2[i]]]))
+            .attr("d", i => {
+              const p = path();
+              const c = this.curve(p);
+              c.lineStart();
+              c.point(X1[i], Y1[i]);
+              c.point(X2[i], Y2[i]);
+              c.lineEnd();
+              return p + "";
+            })
             .call(applyAttr, "stroke", S && (i => S[i]))
             .call(applyAttr, "stroke-opacity", SO && (i => SO[i]))
             .call(title(L)))
