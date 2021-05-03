da17f3a5de751c64ee26d73e71d4cb91fd237d96
Mike Bostock <mbostock@gmail.com>
2021-05-02 21:03:44 -0700

    test style

--- test/plots/letter-frequency-cloud.js
+++ test/plots/letter-frequency-cloud.js
@@ -4,14 +4,18 @@ import * as d3 from "d3";
 export default async function() {
   const alphabet = await d3.csv("data/alphabet.csv", d3.autoType);
   const random = d3.randomLcg(3);
-  const m = d3.max(alphabet, d => d.frequency);
-  const x = alphabet.map(random);
-  const y = alphabet.map(random);
+  const max = d3.max(alphabet, d => d.frequency);
   return Plot.plot({
-    x: { axis: null, domain: [-0.1, 1.1] },
-    y: { axis: null, domain: [-0.1, 1.2] },
+    x: {
+      axis: null,
+      domain: [-0.1, 1.1]
+    },
+    y: {
+      axis: null,
+      domain: [-0.1, 1.2]
+    },
     marks: [
-      Plot.text(alphabet, {x, y, text: "letter", fontSize: d => 10 + 200 * (d.frequency / m)})
+      Plot.text(alphabet, {x: random, y: random, text: "letter", fontSize: d => 10 + 200 * (d.frequency / max)})
     ]
   });
 }
