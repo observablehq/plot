3947b93b51709479eca800699a9bdf1ec42315ef
Philippe Rivière <fil@rezo.net>
2021-06-13 22:21:42 +0200

    legendRadius, built with Plot

--- src/legends.js
+++ src/legends.js
@@ -1,5 +1,6 @@
 import {legendColor} from "./legends/color.js";
 import {legendOpacity} from "./legends/opacity.js";
+import {legendRadius} from "./legends/radius.js";
 
 export function createLegends(descriptors, dimensions) {
   const legends = [];
@@ -7,6 +8,7 @@ export function createLegends(descriptors, dimensions) {
     let {legend, ...options} = descriptors[key];
     if (key === "color" && legend === true) legend = legendColor;
     if (key === "opacity" && legend === true) legend = legendOpacity;
+    if (key === "r" && legend === true) legend = legendRadius;
     if (typeof legend === "function") {
       const l = legend(options, dimensions);
       if (l instanceof Node) legends.push(l);
--- src/legends/opacity.js
+++ src/legends/opacity.js
@@ -1,15 +1,13 @@
-import {scale} from "../scales.js";
 import {legendColor} from "./color.js";
 
-export function legendOpacity(plotOrScale, {width: maxWidth = 640} = {}) {
+export function legendOpacity(plotOrScale, dimensions) {
   if (!plotOrScale) return;
   const opacity = "scales" in plotOrScale ? plotOrScale.scales.opacity : plotOrScale;
   if (!opacity) return;
-  const {width, ...options} = opacity;
   return legendColor({
     ...opacity,
     range: undefined,
     interpolate: undefined,
     scheme: "greys"
-  });
+  }, dimensions);
 }
