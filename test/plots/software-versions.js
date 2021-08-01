e92ddb1d027ce21a7cfe1f1cf25939dd3ead6ad1
Mike Bostock <mbostock@gmail.com>
2021-08-01 12:51:52 -0700

    simplify example

--- test/plots/software-versions.js
+++ test/plots/software-versions.js
@@ -3,29 +3,14 @@ import * as d3 from "d3";
 
 export default async function() {
   const data = await d3.csv("data/software-versions.csv");
-  const options = Plot.stackX(
-    Plot.groupZ(
-      {
-        x: "proportion", // uses sumOrCount in the patched version
-        text: "first"
-      },
-      {
-        x: "key", // this is only strings
-        fill: "key",
-        text: "key",
-        insetRight: 1,
-        order: "value"
-      }
-    )
-  );
   return Plot.plot({
-    marks: [
-      Plot.ruleX([0]),
-      Plot.barX(data, options),
-      Plot.textX(data, { ...options, fill: "#000" })
-    ],
     x: {
       percent: true
-    }
+    },
+    marks: [
+      Plot.barX(data, Plot.stackX(Plot.groupZ({x: "proportion"}, {fill: "version", insetLeft: 0.5, insetRight: 0.5}))),
+      Plot.textX(data, Plot.stackX(Plot.groupZ({x: "proportion", text: "first"}, {z: "version", text: "version"}))),
+      Plot.ruleX([0, 1])
+    ]
   });
 }
