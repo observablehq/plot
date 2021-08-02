939eefd0563862169c34225d07ef0b6de3ee8c3d
Mike Bostock <mbostock@gmail.com>
2021-08-02 12:05:43 -0700

    a more representative word cloud

--- test/plots/word-cloud.js
+++ test/plots/word-cloud.js
@@ -2,36 +2,30 @@ import * as Plot from "@observablehq/plot";
 import * as d3 from "d3";
 
 export default async function() {
+  const random = d3.randomLcg(32);
 
   // Compute a set of “words” from the text. As with any natural language task,
   // this is messy and approximate.
-  const words = Array.from(d3.rollup(
-    (await d3.text("data/moby-dick-chapter-1.txt"))
-      .replace(/’/g, "") // remove apostrophes
-      .split(/\b/g) // split at word boundaries
-      .map(word => word.replace(/[^a-z]+/ig, "")) // strip non-letters
-      .filter(word => word),
-    ([first]) => ({
-      len: first.length,
-      word: first
-    }),
-    word => word.length
-  ).values());
-  const random = d3.randomLcg(32);
+  const words = (await d3.text("data/moby-dick-chapter-1.txt"))
+    .replace(/’/g, "") // remove apostrophes
+    .split(/\b/g) // split at word boundaries
+    .map(word => word.replace(/[^a-z]+/ig, "")) // strip non-letters
+    .filter(word => word) // ignore non-letter words
+    .map(word => word.toLowerCase()); // normalize to lowercase
+
   return Plot.plot({
-    x: { axis: null, inset: 80 },
-    y: { axis: null, inset: 30 },
+    inset: 20,
+    x: {axis: null},
+    y: {axis: null},
     marks: [
-      Plot.text(words, {
+      Plot.text(words, Plot.groupZ({
+        text: d => d.length > 1 ? `${d[0]} (${d.length})` : "",
+        fontSize: d => 4 * Math.sqrt(d.length)
+      }, {
         x: random,
         y: random,
-        text: "word",
-        fill: "word",
-        fillOpacity: 0.25,
-        stroke: "word",
-        strokeOpacity: 0.75,
-        fontSize: d => 100 / Math.sqrt(d.len)
-      })
+        z: d => d
+      }))
     ]
   });
 }
