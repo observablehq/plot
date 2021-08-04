2c71d54f6a5e9b31fcdb09b93a97a5f70eeceff0
Philippe Rivière <fil@rezo.net>
2021-08-04 10:19:47 +0200

    upgrade tests

--- test/scales/scales-test.js
+++ test/scales/scales-test.js
@@ -1,205 +1,205 @@
 import * as Plot from "@observablehq/plot";
-import tape from "tape-await";
+import assert from "assert";
 import {JSDOM} from "jsdom";
 const {window} = new JSDOM("");
 global.document = window.document;
 
-tape("plot(…).scales exposes the plot’s scales", test => {
+it("plot(…).scales exposes the plot’s scales", () => {
   const plot = Plot.dot([1, 2], {x: d => d, y: d => d}).plot();
-  test.equal(typeof plot.scales, "object");
+  assert.strictEqual(typeof plot.scales, "object");
   const scales = plot.scales;
-  test.equal(Object.entries(scales).length, 2);
-  test.assert("x" in scales);
-  test.assert("y" in scales);
+  assert.strictEqual(Object.entries(scales).length, 2);
+  assert("x" in scales);
+  assert("y" in scales);
 });
 
-tape("plot(…).scales[key] is computed once", test => {
+it("plot(…).scales[key] is computed once", () => {
   const plot = Plot.dot([1, 2], {x: d => d, y: d => d}).plot();
-  test.assert(plot.scales.x === plot.scales.x);
+  assert(plot.scales.x === plot.scales.x);
 });
 
-tape("plot(…).scales[key] is computed lazily", test => {
+it("plot(…).scales[key] is computed lazily", () => {
   const plot = Plot.dot([1, 2], {x: d => d, y: d => d}).plot();
-  test.assert(plot.scales.x === plot.scales.x);
+  assert(plot.scales.x === plot.scales.x);
 });
 
-tape("plot(…).scales.x exposes the plot’s x scale", test => {
+it("plot(…).scales.x exposes the plot’s x scale", () => {
   const x = Plot.dot([1, 2], {x: d => d}).plot().scales.x;
-  test.deepEqual(x.domain, [1, 2]);
-  test.deepEqual(x.range, [20, 620]);
-  test.equal(typeof x.interpolate, "function");
-  test.equal(x.type, "linear");
-  test.equal(x.clamp, false);
-  test.equal(typeof Plot.scale(x), "function");
+  assert.deepStrictEqual(x.domain, [1, 2]);
+  assert.deepStrictEqual(x.range, [20, 620]);
+  assert.strictEqual(typeof x.interpolate, "function");
+  assert.strictEqual(x.type, "linear");
+  assert.strictEqual(x.clamp, false);
+  assert.strictEqual(typeof Plot.scale(x), "function");
 });
 
-tape("plot(…).scales.y exposes the plot’s y scale", test => {
+it("plot(…).scales.y exposes the plot’s y scale", () => {
   const y0 = Plot.dot([1, 2], {x: d => d}).plot().scales.y;
-  test.equal(y0, undefined);
+  assert.strictEqual(y0, undefined);
   const y = Plot.dot([1, 2], {y: d => d}).plot().scales.y;
-  test.deepEqual(y.domain, [1, 2]);
-  test.deepEqual(y.range, [380, 20]);
-  test.equal(typeof y.interpolate, "function");
-  test.equal(y.type, "linear");
-  test.equal(y.clamp, false);
-  test.equal(typeof Plot.scale(y), "function");
+  assert.deepStrictEqual(y.domain, [1, 2]);
+  assert.deepStrictEqual(y.range, [380, 20]);
+  assert.strictEqual(typeof y.interpolate, "function");
+  assert.strictEqual(y.type, "linear");
+  assert.strictEqual(y.clamp, false);
+  assert.strictEqual(typeof Plot.scale(y), "function");
 });
 
-tape("plot(…).scales.fx exposes the plot’s fx scale", test => {
+it("plot(…).scales.fx exposes the plot’s fx scale", () => {
   const fx0 = Plot.dot([1, 2], {x: d => d}).plot().scales.fx;
-  test.equal(fx0, undefined);
+  assert.strictEqual(fx0, undefined);
   const data = [1, 2];
   const fx = Plot.dot(data, {y: d => d}).plot({facet: {data, x: data}}).scales.fx;
-  test.deepEqual(fx.domain, [1, 2]);
-  test.deepEqual(fx.range, [40, 620]);
-  test.equal(typeof fx.interpolate, "undefined");
-  test.equal(fx.type, "band");
-  test.equal(fx.clamp, undefined);
-  test.equal(typeof Plot.scale(fx), "function");
+  assert.deepStrictEqual(fx.domain, [1, 2]);
+  assert.deepStrictEqual(fx.range, [40, 620]);
+  assert.strictEqual(typeof fx.interpolate, "undefined");
+  assert.strictEqual(fx.type, "band");
+  assert.strictEqual(fx.clamp, undefined);
+  assert.strictEqual(typeof Plot.scale(fx), "function");
 });
 
-tape("plot(…).scales.fy exposes the plot’s fy scale", test => {
+it("plot(…).scales.fy exposes the plot’s fy scale", () => {
   const fy0 = Plot.dot([1, 2], {x: d => d}).plot().scales.fy;
-  test.equal(fy0, undefined);
+  assert.strictEqual(fy0, undefined);
   const data = [1, 2];
   const fy = Plot.dot(data, {y: d => d}).plot({facet: {data, y: data}}).scales.fy;
-  test.deepEqual(fy.domain, [1, 2]);
-  test.deepEqual(fy.range, [20, 380]);
-  test.equal(typeof fy.interpolate, "undefined");
-  test.equal(fy.type, "band");
-  test.equal(fy.clamp, undefined);
-  test.equal(typeof Plot.scale(fy), "function");
+  assert.deepStrictEqual(fy.domain, [1, 2]);
+  assert.deepStrictEqual(fy.range, [20, 380]);
+  assert.strictEqual(typeof fy.interpolate, "undefined");
+  assert.strictEqual(fy.type, "band");
+  assert.strictEqual(fy.clamp, undefined);
+  assert.strictEqual(typeof Plot.scale(fy), "function");
 });
 
-tape("plot(…).scales.color exposes a continuous color scale", test => {
+it("plot(…).scales.color exposes a continuous color scale", () => {
   const color0 = Plot.dot([1, 2], {x: d => d}).plot().scales.color;
-  test.equal(color0, undefined);
+  assert.strictEqual(color0, undefined);
   const data = [1, 2, 3, 4, 5];
   const color = Plot.dot(data, {y: d => d, fill: d => d}).plot().scales.color;
-  test.deepEqual(color.domain, [1, 5]);
-  test.deepEqual(color.range, [0, 1]);
-  test.equal(typeof color.interpolate, "function");
-  test.equal(color.type, "linear");
-  test.equal(color.clamp, false);
-  test.equal(typeof Plot.scale(color), "function");
+  assert.deepStrictEqual(color.domain, [1, 5]);
+  assert.deepStrictEqual(color.range, [0, 1]);
+  assert.strictEqual(typeof color.interpolate, "function");
+  assert.strictEqual(color.type, "linear");
+  assert.strictEqual(color.clamp, false);
+  assert.strictEqual(typeof Plot.scale(color), "function");
 });
 
-tape("plot(…).scales.color exposes an ordinal color scale", test => {
+it("plot(…).scales.color exposes an ordinal color scale", () => {
   const data = ["a", "b", "c", "d"];
   const color = Plot.dot(data, {y: d => d, fill: d => d}).plot({ color: { type: "ordinal" }}).scales.color;
-  test.deepEqual(color.domain, data);
-  test.deepEqual(color.range, ['rgb(35, 23, 27)', 'rgb(46, 229, 174)', 'rgb(254, 185, 39)', 'rgb(144, 12, 0)']);
-  test.equal(typeof color.interpolate, "undefined");
-  test.equal(color.type, "ordinal");
-  test.equal(color.clamp, undefined);
-  test.equal(typeof Plot.scale(color), "function");
+  assert.deepStrictEqual(color.domain, data);
+  assert.deepStrictEqual(color.range, ['rgb(35, 23, 27)', 'rgb(46, 229, 174)', 'rgb(254, 185, 39)', 'rgb(144, 12, 0)']);
+  assert.strictEqual(typeof color.interpolate, "undefined");
+  assert.strictEqual(color.type, "ordinal");
+  assert.strictEqual(color.clamp, undefined);
+  assert.strictEqual(typeof Plot.scale(color), "function");
 });
 
-tape("plot(…).scales.color exposes a categorical color scale", test => {
+it("plot(…).scales.color exposes a categorical color scale", () => {
   const data = ["a", "b", "c", "d"];
   const color = Plot.dot(data, {y: d => d, fill: d => d}).plot({ color: { type: "categorical" }}).scales.color;
-  test.deepEqual(color.domain, data);
-  test.deepEqual(color.range, ['#4e79a7', '#f28e2c', '#e15759', '#76b7b2', '#59a14f', '#edc949', '#af7aa1', '#ff9da7', '#9c755f', '#bab0ab']);
-  test.equal(typeof color.interpolate, "undefined");
-  test.equal(color.type, "categorical");
-  test.equal(color.clamp, undefined);
-  test.equal(typeof Plot.scale(color), "function");
+  assert.deepStrictEqual(color.domain, data);
+  assert.deepStrictEqual(color.range, ['#4e79a7', '#f28e2c', '#e15759', '#76b7b2', '#59a14f', '#edc949', '#af7aa1', '#ff9da7', '#9c755f', '#bab0ab']);
+  assert.strictEqual(typeof color.interpolate, "undefined");
+  assert.strictEqual(color.type, "categorical");
+  assert.strictEqual(color.clamp, undefined);
+  assert.strictEqual(typeof Plot.scale(color), "function");
 });
 
-tape("plot(…).scales.r exposes a radius scale", test => {
+it("plot(…).scales.r exposes a radius scale", () => {
   const r0 = Plot.dot([1, 2], {x: d => d}).plot().scales.r;
-  test.equal(r0, undefined);
+  assert.strictEqual(r0, undefined);
   const data = [1, 2, 3, 4, 9];
   const r = Plot.dot(data, {r: d => d}).plot().scales.r;
-  test.deepEqual(r.domain, [0, 9]);
-  test.deepEqual(r.range, [0, Math.sqrt(40.5)]);
-  test.equal(typeof r.interpolate, "function");
-  test.equal(r.type, "sqrt");
-  test.equal(r.clamp, false);
-  test.equal(typeof Plot.scale(r), "function");
+  assert.deepStrictEqual(r.domain, [0, 9]);
+  assert.deepStrictEqual(r.range, [0, Math.sqrt(40.5)]);
+  assert.strictEqual(typeof r.interpolate, "function");
+  assert.strictEqual(r.type, "sqrt");
+  assert.strictEqual(r.clamp, false);
+  assert.strictEqual(typeof Plot.scale(r), "function");
 });
 
-tape("plot(…).scales.opacity exposes a linear scale", test => {
+it("plot(…).scales.opacity exposes a linear scale", () => {
   const opacity0 = Plot.dot([1, 2], {x: d => d}).plot().scales.opacity;
-  test.equal(opacity0, undefined);
+  assert.strictEqual(opacity0, undefined);
   const data = [1, 2, 3, 4, 9];
   const opacity = Plot.dot(data, {fillOpacity: d => d}).plot().scales.opacity;
-  test.deepEqual(opacity.domain, [0, 9]);
-  test.deepEqual(opacity.range, [0, 1]);
-  test.equal(typeof opacity.interpolate, "function");
-  test.equal(opacity.type, "linear");
-  test.equal(opacity.clamp, false);
-  test.equal(typeof Plot.scale(opacity), "function");
+  assert.deepStrictEqual(opacity.domain, [0, 9]);
+  assert.deepStrictEqual(opacity.range, [0, 1]);
+  assert.strictEqual(typeof opacity.interpolate, "function");
+  assert.strictEqual(opacity.type, "linear");
+  assert.strictEqual(opacity.clamp, false);
+  assert.strictEqual(typeof Plot.scale(opacity), "function");
 });
 
-tape("plot(…).scales expose inset domain", test => {
-  test.deepEqual(scaleOpt({inset: null}).range, [20, 620]);
-  test.deepEqual(scaleOpt({inset: 7}).range, [27, 613]);
+it("plot(…).scales expose inset domain", () => {
+  assert.deepStrictEqual(scaleOpt({inset: null}).range, [20, 620]);
+  assert.deepStrictEqual(scaleOpt({inset: 7}).range, [27, 613]);
 });
 
-tape("plot(…).scales expose clamp", test => {
-  test.equal(scaleOpt({clamp: false}).clamp, false);
-  test.equal(scaleOpt({clamp: true}).clamp, true);
+it("plot(…).scales expose clamp", () => {
+  assert.strictEqual(scaleOpt({clamp: false}).clamp, false);
+  assert.strictEqual(scaleOpt({clamp: true}).clamp, true);
 });
 
-tape("plot(…).scales expose rounded scales", test => {
-  test.equal(Plot.scale(scaleOpt({round: false}))(Math.SQRT2), 144.26406871192853);
-  test.equal(Plot.scale(scaleOpt({round: true}))(Math.SQRT2), 144);
-  test.equal(scaleOpt({round: true}).interpolate(0, 100)(Math.SQRT1_2), 71);
+it("plot(…).scales expose rounded scales", () => {
+  assert.strictEqual(Plot.scale(scaleOpt({round: false}))(Math.SQRT2), 144.26406871192853);
+  assert.strictEqual(Plot.scale(scaleOpt({round: true}))(Math.SQRT2), 144);
+  assert.strictEqual(scaleOpt({round: true}).interpolate(0, 100)(Math.SQRT1_2), 71);
 });
 
-tape("plot(…).scales expose label", test => {
-  test.equal(scaleOpt({}).label, "x →");
-  test.equal(scaleOpt({label: "value"}).label, "value");
+it("plot(…).scales expose label", () => {
+  assert.strictEqual(scaleOpt({}).label, "x →");
+  assert.strictEqual(scaleOpt({label: "value"}).label, "value");
 });
 
-tape("plot(…).scales expose color label", test => {
+it("plot(…).scales expose color label", () => {
   const x = Plot.dot([{x: 1}, {x: 2}, {x: 3}], {fill: "x"}).plot().scales.color;
-  test.equal(x.label, "x");
+  assert.strictEqual(x.label, "x");
   const y = Plot.dot([{x: 1}, {x: 2}, {x: 3}], {fill: "x"}).plot({color: {label: "y"}}).scales.color;
-  test.equal(y.label, "y");
+  assert.strictEqual(y.label, "y");
 });
 
-tape("plot(…).scales expose radius label", test => {
+it("plot(…).scales expose radius label", () => {
   const x = Plot.dot([{x: 1}, {x: 2}, {x: 3}], {r: "x"}).plot().scales.r;
-  test.equal(x.label, "x");
+  assert.strictEqual(x.label, "x");
   const r = Plot.dot([{x: 1}, {x: 2}, {x: 3}], {r: "x"}).plot({r: {label: "radius"}}).scales.r;
-  test.equal(r.label, "radius");
+  assert.strictEqual(r.label, "radius");
 });
 
-tape("plot(…).scales expose pow exponent", test => {
+it("plot(…).scales expose pow exponent", () => {
   const x = Plot.dotX([]).plot({x: { type: "pow", exponent: 0.3 }}).scales.x;
-  test.equal(x.type, "pow");
-  test.equal(x.exponent, 0.3);
+  assert.strictEqual(x.type, "pow");
+  assert.strictEqual(x.exponent, 0.3);
   const y = Plot.dotX([]).plot({x: { type: "sqrt" }}).scales.x;
-  test.equal(y.type, "sqrt");
-  test.equal(y.exponent, undefined);
+  assert.strictEqual(y.type, "sqrt");
+  assert.strictEqual(y.exponent, undefined);
 });
 
-tape("plot(…).scales expose log base", test => {
+it("plot(…).scales expose log base", () => {
   const x = Plot.dotX([]).plot({x: { type: "log", base: 2 }}).scales.x;
-  test.equal(x.type, "log");
-  test.equal(x.base, 2);
+  assert.strictEqual(x.type, "log");
+  assert.strictEqual(x.base, 2);
 });
 
-tape("plot(…).scales expose symlog constant", test => {
+it("plot(…).scales expose symlog constant", () => {
   const x = Plot.dotX([]).plot({x: { type: "symlog", constant: 42 }}).scales.x;
-  test.equal(x.type, "symlog");
-  test.equal(x.constant, 42);
+  assert.strictEqual(x.type, "symlog");
+  assert.strictEqual(x.constant, 42);
 });
 
-tape("plot(…).scales expose align, paddingInner and paddingOuter", test => {
+it("plot(…).scales expose align, paddingInner and paddingOuter", () => {
   const x = Plot.cellX(["A", "B"]).plot({x: { paddingOuter: -0.2, align: 1 }}).scales.x;
-  test.equal(x.type, "band");
-  test.equal(x.align, 1);
-  test.equal(x.paddingInner, 0.1);
-  test.equal(x.paddingOuter, -0.2);
+  assert.strictEqual(x.type, "band");
+  assert.strictEqual(x.align, 1);
+  assert.strictEqual(x.paddingInner, 0.1);
+  assert.strictEqual(x.paddingOuter, -0.2);
 });
 
-tape("plot(…).scales expose unexpected scale options", test => {
+it("plot(…).scales expose unexpected scale options", () => {
   const x = Plot.dotX([]).plot({x: { lala: 42, width: 420 }}).scales.x;
-  test.equal(x.lala, 42);
-  test.equal(x.width, 420);
+  assert.strictEqual(x.lala, 42);
+  assert.strictEqual(x.width, 420);
 });
 
 function scaleOpt(x) {
