1a26fb5da7e236bd20db221491d106ded4e8d8ae
Robert Lesser <bclesser@gmail.com>
2021-08-05 08:57:18 -1000

    Adding US President data example

---
+++ test/plots/us-president-favorability-dots.js
@@ -0,0 +1,32 @@
+import * as Plot from "@observablehq/plot";
+import * as d3 from "d3";
+
+export default async function() {
+  const data = await d3.csv("data/us-president-favorability.csv", d3.autoType);
+  return Plot.plot({
+    inset: 20,
+    width: 900,
+    height: 600,
+    y: {
+      label: 'Net Favorability',
+      tickFormat: d => d+'%'
+    },
+    marks: [
+      Plot.ruleY([0]),
+      Plot.image(
+        data, 
+        {
+          x: 'First Inauguration Date',
+          y: d => 
+            d['Very Favorable %'] + 
+            d['Somewhat Favorable %'] - 
+            d['Very Unfavorable %'] - 
+            d['Somewhat Unfavorable %'],
+          href: 'Portrait URL',
+          size: 60,
+          title: 'Name'
+        }
+      )
+    ]
+  });
+}
