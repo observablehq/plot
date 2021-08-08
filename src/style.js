efd4f3ec51c6b9dd2624a36a357d85ecd6bb8bef
Mike Bostock <mbostock@gmail.com>
2021-08-08 09:56:37 -0700

    common style channels

--- src/style.js
+++ src/style.js
@@ -1,7 +1,59 @@
-import {string, number} from "./mark.js";
+import {string, number, maybeColor, maybeNumber, titleGroup} from "./mark.js";
 
 export const offset = typeof window !== "undefined" && window.devicePixelRatio > 1 ? 0 : 0.5;
 
+// TODO This works for Area, but Line has different defaults (primary fill vs. primary stroke).
+export function styles({
+  title,
+  fill,
+  fillOpacity,
+  stroke,
+  strokeWidth,
+  strokeOpacity,
+  strokeLinejoin,
+  strokeLinecap,
+  strokeMiterlimit,
+  strokeDasharray,
+  mixBlendMode,
+  shapeRendering
+} = {}) {
+  const [vstroke, cstroke] = maybeColor(stroke, "none");
+  const [vstrokeOpacity, cstrokeOpacity] = maybeNumber(strokeOpacity);
+  const [vfill, cfill] = maybeColor(fill, cstroke === "none" ? "currentColor" : "none");
+  const [vfillOpacity, cfillOpacity] = maybeNumber(fillOpacity);
+  if (strokeMiterlimit === undefined) strokeMiterlimit = cstroke === "none" ? undefined : 1;
+  return [
+    {
+      fill: impliedString(cfill, "currentColor"),
+      fillOpacity: impliedNumber(cfillOpacity, 1),
+      stroke: impliedString(cstroke, "none"),
+      strokeWidth: impliedNumber(strokeWidth, 1),
+      strokeOpacity: impliedNumber(cstrokeOpacity, 1),
+      strokeLinejoin: impliedString(strokeLinejoin, "miter"),
+      strokeLinecap: impliedString(strokeLinecap, "butt"),
+      strokeMiterlimit: impliedNumber(strokeMiterlimit, 4),
+      strokeDasharray: string(strokeDasharray),
+      mixBlendMode: impliedString(mixBlendMode, "normal"),
+      shapeRendering: impliedString(shapeRendering, "auto")
+    },
+    [
+      {name: "title", value: title, optional: true},
+      {name: "fill", value: vfill, scale: "color", optional: true},
+      {name: "fillOpacity", value: vfillOpacity, scale: "opacity", optional: true},
+      {name: "stroke", value: vstroke, scale: "color", optional: true},
+      {name: "strokeOpacity", value: vstrokeOpacity, scale: "opacity", optional: true}
+    ]
+  ];
+}
+
+export function findStyle(channels, ...names) {
+  for (const name of names) {
+    const channel = channels.find(d => d.name === name);
+    if (channel && channel.value != null) return channel.value;
+  }
+}
+
+// TODO remove me
 export function Style(mark, {
   fill,
   fillOpacity,
@@ -26,6 +78,15 @@ export function Style(mark, {
   mark.strokeDasharray = string(strokeDasharray);
   mark.mixBlendMode = impliedString(mixBlendMode, "normal");
   mark.shapeRendering = impliedString(shapeRendering, "auto");
+}
+
+// TODO This works for Area and Line, but Dot needs to be applied to individual elements.
+export function applyGroupedChannelStyles(selection, {title: L, fill: F, fillOpacity: FO, stroke: S, strokeOpacity: SO}) {
+  applyAttr(selection, "fill", F && (([i]) => F[i]));
+  applyAttr(selection, "fill-opacity", FO && (([i]) => FO[i]));
+  applyAttr(selection, "stroke", S && (([i]) => S[i]));
+  applyAttr(selection, "stroke-opacity", SO && (([i]) => SO[i]));
+  titleGroup(L)(selection);
 }
 
 export function applyIndirectStyles(selection, mark) {
