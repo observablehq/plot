eb9debaef15434741d88c1a42aadaa4df0dc9a76
Philippe Rivière <fil@rezo.net>
2021-08-09 12:39:50 +0200

    test plot for the empty option
    * uses the filter option of the bin transform
    * uses an explicit null if empty, or sum, accessor to create a broken line

---
+++ test/plots/availability.js
@@ -0,0 +1,47 @@
+import * as Plot from "@observablehq/plot";
+import * as d3 from "d3";
+
+export default async function() {
+  const data = await d3.csv("data/availability.csv", d3.autoType);
+  return Plot.plot({
+    marks: [
+      Plot.areaY(
+        data,
+        Plot.binX(
+          {
+            filter: null,
+            y: "sum"
+          },
+          {
+            x: "date",
+            y: "value",
+            sort: "date",
+            curve: "step",
+            empty: true,
+            thresholds: d3.utcDay,
+            fill: "#f2f2fe"
+          }
+        )
+      ),
+      Plot.line(
+        data,
+        Plot.binX(
+          {
+            filter: null,
+            y: d => d.length ? d3.sum(d) : null
+          },
+          {
+            x: "date",
+            y: "value",
+            sort: "date",
+            curve: "step",
+            empty: true,
+            thresholds: d3.utcDay
+          }
+        )
+      ),
+      Plot.ruleY([0])
+    ],
+    height: 180
+  });
+}
