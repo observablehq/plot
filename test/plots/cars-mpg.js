7bce12a15796a70130a3c559c967eeca3763729f
Philippe Rivière <fil@rezo.net>
2021-08-11 12:21:01 +0200

    This example plot computes the median of cars' economy (mpg), grouped by number of cylinders
    The bins are sorted by decreasing r, so that they are all visible.
    
    The example would benefit from stackR (#197).
    
    It could also benefit from a strategy to create missing values for the line, so that it's broken when there are no data. However, it won't work with an approach such as "return empty bins" (#495), because returning empty bins will not create the *z* values for each and every category, which would be necessary if we wanted to create broken lines. This shows that a generic foolproof solution to #351 will require much more than #495 (and #489 and #491 are not better in that regard).

---
+++ test/plots/cars-mpg.js
@@ -0,0 +1,35 @@
+import * as Plot from "@observablehq/plot";
+import * as d3 from "d3";
+
+export default async function() {
+  const data = await d3.csv("data/cars.csv", d3.autoType);
+
+  return Plot.plot({
+    x: {inset: 20, ticks: 10},
+    y: {grid: true},
+    color: {
+      type: "categorical",
+      legend: true // as soon as it's available
+    },
+    marks: [
+      Plot.ruleY([0], {filter: false}),
+      Plot.line(data,
+        Plot.binX({y: "mean"}, {
+          x: d => +d["year"],
+          y: "economy (mpg)",
+          stroke: "cylinders",
+          curve: "basis",
+          thresholds: 20
+        })
+      ),
+      Plot.dot(data,
+        Plot.reverse(Plot.sort("length", Plot.binY({ r: "count" }, {
+          x: "year",
+          y: "economy (mpg)",
+          fill: "cylinders",
+          thresholds: 20
+        }))
+      ))
+    ]
+  });
+}
