880b66437076b92f565166777e223295b464c33b
Mike Bostock <mbostock@gmail.com>
2021-08-11 08:11:59 -0700

    remove console.log

--- src/transforms/filter.js
+++ src/transforms/filter.js
@@ -8,7 +8,6 @@ export function filter(value, options) {
 export function filterTransform(value) {
   return (data, facets) => {
     const V = valueof(data, value);
-    console.log("filterTransform", {value, V});
     return {data, facets: facets.map(I => I.filter(i => V[i]))};
   };
 }
