4f8e93b92ab1c598c2bcbb12c2cb5b799ef3ed24
Philippe Rivière <fil@rezo.net>
2021-08-19 10:16:15 +0200

    first ladies example plot (demonstrates "group names by first tenure_start") based on Toph Tucker’s https://observablehq.com/@tophtucker/first-ladies-of-the-united-states

--- test/plots/index.js
+++ test/plots/index.js
@@ -31,6 +31,7 @@ export {default as empty} from "./empty.js";
 export {default as emptyX} from "./empty-x.js";
 export {default as figcaption} from "./figcaption.js";
 export {default as figcaptionHtml} from "./figcaption-html.js";
+export {default as firstLadies} from "./first-ladies.js";
 export {default as fruitSales} from "./fruit-sales.js";
 export {default as fruitSalesDate} from "./fruit-sales-date.js";
 export {default as gistempAnomaly} from "./gistemp-anomaly.js";
