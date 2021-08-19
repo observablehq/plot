dc27910588bf010d326cb29ead39339605bffb09
Mike Bostock <mbostock@gmail.com>
2021-08-19 15:58:32 -0700

    style

--- test/plots/first-ladies.js
+++ test/plots/first-ladies.js
@@ -3,31 +3,20 @@ import * as d3 from "d3";
 
 export default async function() {
   const data = await d3.csv("data/first-ladies.csv", d3.autoType);
-  const now = Date.UTC(2021, 7, 19);
+  const now = new Date("2021-07-19");
   return Plot.plot({
-    width: 940,
+    width: 960,
     marginRight: 120,
-    y: { axis: null },
+    x: {
+      axis: "top"
+    },
+    y: {
+      axis: null
+    },
     marks: [
-      Plot.barX(data, {
-        x1: "birth",
-        x2: d => d.death || now,
-        y: "name",
-        fill: "#ccc"
-      }),
-      Plot.barX(data, {
-        x1: "tenure_start",
-        x2: d => d.tenure_end || now,
-        y: "name",
-        sort: { y: { value: "x1", reduce: "min" } }
-      }),
-      Plot.text(data, {
-        x: d => d.death || now,
-        y: "name",
-        text: "name",
-        textAnchor: "start",
-        dx: 5
-      })
+      Plot.barX(data, {x1: "birth", x2: d => d.death ?? now, y: "name", fill: "#ccc"}),
+      Plot.barX(data, {x1: "tenure_start", x2: d => d.tenure_end ?? now, y: "name", sort: {y: "x1", reduce: "min"}}),
+      Plot.text(data, {x: d => d.death ?? now, y: "name", text: "name", textAnchor: "start", dx: 5})
     ]
   });
 }
