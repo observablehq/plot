983e83492ce85d76cf72b206b39e9a936c59cc23
Mike Bostock <mbostock@gmail.com>
2021-08-19 17:22:31 -0700

    area instead of line

--- test/plots/sf-covid-deaths.js
+++ test/plots/sf-covid-deaths.js
@@ -5,18 +5,18 @@ export default async function() {
   const cases = await d3.csv("data/sf-covid.csv", d3.autoType);
   return Plot.plot({
     marks: [
-      Plot.ruleY([0]),
-      Plot.lineY(cases, Plot.binX({
+      Plot.areaY(cases, Plot.binX({
         y: "sum",
         filter: null
       }, {
         x: "specimen_collection_date",
         y: "case_count",
         filter: d => d.case_disposition === "Death",
-        stroke: "transmission_category",
+        fill: "transmission_category",
         curve: "step",
         thresholds: d3.utcWeek
-      }))
+      })),
+      Plot.ruleY([0])
     ]
   });
 }
