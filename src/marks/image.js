5e0b3524f42baa1a671794b4f94d0c789e4cf427
Robert Lesser <bclesser@gmail.com>
2021-09-01 23:33:59 -0400

    Updated image and image-test to 0.2.0

--- src/marks/image.js
+++ src/marks/image.js
@@ -1,16 +1,11 @@
 import { create } from "d3";
 import { filter, positive } from "../defined.js";
-import { Mark, maybeNumber, maybeTuple, title } from "../mark.js";
-import {
-  Style,
-  applyDirectStyles,
-  applyIndirectStyles,
-  applyTransform,
-  applyAttr
-} from "../style.js";
+import { Mark, maybeNumber, maybeTuple } from "../mark.js";
+import {applyChannelStyles, applyDirectStyles, applyIndirectStyles, applyTransform, applyAttr} from "../style.js";
 
 export class Image extends Mark {
-  constructor(data, { x, y, href, r, title, ...options } = {}) {
+  constructor(data, options = {}) {
+    const {x, y, r, href} = options;
     const [vr, cr] = maybeNumber(r, 15);
     super(
       data,
@@ -18,45 +13,40 @@ export class Image extends Mark {
         { name: "x", value: x, scale: "x", optional: true },
         { name: "y", value: y, scale: "y", optional: true },
         {name: "r", value: vr, scale: "r", optional: true},
-        { name: 'href', value: href, optional: false },
-        { name: "title", value: title, optional: true }
+        { name: 'href', value: href, optional: false }
       ],
-      options
+      options,
+      {}
     );
     this.r = cr;
-    Style(this, {
-      ...options
-    });
   }
   render(
     I,
     { x, y },
-    { x: X, y: Y, href: H, r: R, title: L },
+    channels,
     { width, height, marginTop, marginRight, marginBottom, marginLeft }
   ) {
+    const {x: X, y: Y, r: R, href: H} = channels;
     let index = filter(I, X, Y, R, H);
     if (R) index = index.filter((i) => positive(R[i]));
     return create("svg:g")
-      .call(applyIndirectStyles, this)
-      .call(applyTransform, x, y, 0.5, 0.5)
-      .call((g) =>
-        g
-          .selectAll()
+        .call(applyIndirectStyles, this)
+        .call(applyTransform, x, y, 0.5, 0.5)
+        .call(g => g.selectAll()
           .data(index)
           .join("image")
-          .call(applyDirectStyles, this)
-          .attr("x", X ? (i) => X[i] : (marginLeft + width - marginRight) / 2)
-          .attr("y", Y ? (i) => Y[i] : (marginTop + height - marginBottom) / 2)
-          .attr("width", R ? (i) => R[i] * 2 : this.r * 2)
-          .attr("height", R ? (i) => R[i] * 2 : this.r * 2)
-          .call(applyAttr, "href", H && (i => H[i]))
-          .call(
-            applyAttr,
-            "transform",
-            i => `translate(${[-(R ? R[i] : this.r), -(R ? R[i] : this.r)]})`
-          )
-          .call(title(L))
-      )
+            .call(applyDirectStyles, this)
+            .attr("x", X ? (i) => X[i] : (marginLeft + width - marginRight) / 2)
+            .attr("y", Y ? (i) => Y[i] : (marginTop + height - marginBottom) / 2)
+            .attr("width", R ? (i) => R[i] * 2 : this.r * 2)
+            .attr("height", R ? (i) => R[i] * 2 : this.r * 2)
+            .call(applyAttr, "href", H && (i => H[i]))
+            .call(
+              applyAttr,
+              "transform",
+              i => `translate(${[-(R ? R[i] : this.r), -(R ? R[i] : this.r)]})`
+            )
+            .call(applyChannelStyles, channels))
       .node();
   }
 }
