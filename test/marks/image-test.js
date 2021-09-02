3af6a47bd5b5a68b2add5f3797d8f84bb1cadaca
Robert Lesser <bclesser@gmail.com>
2021-09-01 23:33:59 -0400

    Updated image and image-test to 0.2.0

--- test/marks/image-test.js
+++ test/marks/image-test.js
@@ -1,31 +1,31 @@
 import * as Plot from "@observablehq/plot";
-import tape from "tape-await";
+import assert from "assert";
 
-tape("image(undefined, {href}) has the expected defaults", test => {
+it("image(undefined, {href}) has the expected defaults", () => {
   const image = Plot.image(undefined, {href: ""});
-  test.strictEqual(image.data, undefined);
-  test.strictEqual(image.transform, undefined);
-  test.deepEqual(image.channels.map(c => c.name), ["x", "y", "href"]);
-  test.deepEqual(image.channels.map(c => Plot.valueof([[1, 2], [3, 4]], c.value)), [[1, 3], [2, 4], [undefined, undefined]]);
-  test.deepEqual(image.channels.map(c => c.scale), ["x", "y", undefined]);
-  test.strictEqual(image.r, 15);
+  assert.strictEqual(image.data, undefined);
+  assert.strictEqual(image.transform, undefined);
+  assert.deepStrictEqual(image.channels.map(c => c.name), ["x", "y", "href"]);
+  assert.deepStrictEqual(image.channels.map(c => Plot.valueof([[1, 2], [3, 4]], c.value)), [[1, 3], [2, 4], [undefined, undefined]]);
+  assert.deepStrictEqual(image.channels.map(c => c.scale), ["x", "y", undefined]);
+  assert.strictEqual(image.r, 15);
 });
 
-tape("image(data, {r, href}) allows r to be a constant amount", test => {
+it("image(data, {r, href}) allows r to be a constant amount", () => {
   const image = Plot.image(undefined, {r: 42, href: ""});
-  test.strictEqual(image.r, 42);
+  assert.strictEqual(image.r, 42);
 });
 
-tape("image(data, {r, href}) allows r to be a variable amount", test => {
+it("image(data, {r, href}) allows r to be a variable amount", () => {
   const image = Plot.image(undefined, {r: "x", href: ""});
-  test.strictEqual(image.r, undefined);
+  assert.strictEqual(image.r, undefined);
   const r = image.channels.find(c => c.name === "r");
-  test.strictEqual(r.value, "x");
+  assert.strictEqual(r.value, "x");
 });
 
-tape("image(data, {title, href}) specifies an optional title channel", test => {
+it("image(data, {title, href}) specifies an optional title channel", () => {
   const image = Plot.image(undefined, {title: "x", href: ""});
   const title = image.channels.find(c => c.name === "title");
-  test.strictEqual(title.value, "x");
-  test.strictEqual(title.scale, undefined);
+  assert.strictEqual(title.value, "x");
+  assert.strictEqual(title.scale, undefined);
 });
