02805620314bd7719435f0f7c66d776e816f6432
Mike Bostock <mbostock@gmail.com>
2021-09-26 13:34:25 -0700

    tweak footballCoverage example

--- test/plots/football-coverage.js
+++ test/plots/football-coverage.js
@@ -2,34 +2,23 @@ import * as Plot from "@observablehq/plot";
 import * as d3 from "d3";
 
 export default async function() {
-  const data = await d3.csv("data/football-coverage.csv", d3.autoType);
+  const football = await d3.csv("data/football-coverage.csv", d3.autoType);
   return Plot.plot({
-    facet: {
-      data,
-      x: "coverage"
-    },
     x: {
-      ticks: 0
+      axis: null
     },
     y: {
+      grid: true,
       domain: [0, 0.5],
-      ticks: d3.range(0, 1, 0.1),
       tickFormat: "%"
     },
-    fx: {
-      axis: "bottom"
+    facet: {
+      data: football,
+      x: "coverage"
     },
     marks: [
-      Plot.frame({ stroke: "#ccc" }),
-      Plot.ruleY(d3.range(0, 1, 0.1), { stroke: "#ccc" }),
-      Plot.dot(
-        data,
-        Plot.stackX({
-          offset: "silhouette",
-          y: (d) => +d.value.toFixed(2),
-          fill: "black"
-        })
-      )
+      Plot.frame(),
+      Plot.dot(football, Plot.stackX({offset: "center", y: d => +d.value.toFixed(2), fill: "black"}))
     ]
   });
 }
