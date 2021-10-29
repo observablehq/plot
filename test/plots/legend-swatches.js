9f8574788f722ce31827cd052fd9b110ebd82ac6
Philippe Rivière <fil@rezo.net>
2021-10-29 15:04:08 +0200

    remove redundant color tests

--- test/plots/legend-swatches.js
+++
@@ -1,6 +0,0 @@
-import * as Plot from "@observablehq/plot";
-
-export default async function() {
-  const chart = Plot.plot({color: {type: "categorical", domain: ["A", "B", "C"], label: "category" }});
-  return chart.legend("color");
-}
