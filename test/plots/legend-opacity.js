e834babdaf9f58c7ed25726d585fd1cf6b57166c
Philippe Rivière <fil@rezo.net>
2021-10-29 16:11:54 +0200

    remove radius and opacity legends for now

--- test/plots/legend-opacity.js
+++
@@ -1,8 +0,0 @@
-import * as Plot from "@observablehq/plot";
-
-export default async function() {
-  const chart = Plot.dotX([{o: 1}, {o: 10}], {
-    fillOpacity: "o"
-  }).plot({ opacity: {type: "log", label: "opaque", legend: true}});
-  return chart.legend("opacity");
-}
