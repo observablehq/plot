72e5278b858d376c4cab05464e7c673094253826
Mike Bostock <mbostock@gmail.com>
2021-11-08 08:54:47 -0800

    non-nullish, not truthy

--- src/legends.js
+++ src/legends.js
@@ -1,6 +1,6 @@
 import {legendColor} from "./legends/color.js";
 
 export function legend({color, ...options}) {
-  if (color) return legendColor({...color, ...options});
+  if (color != null) return legendColor({...color, ...options});
   throw new Error(`unsupported legend type`);
 }
