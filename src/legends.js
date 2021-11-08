12e0e47cc53bad37c8d850bd9428f2a91052816c
Mike Bostock <mbostock@gmail.com>
2021-11-08 08:51:59 -0800

    error on unknown legend type

--- src/legends.js
+++ src/legends.js
@@ -2,4 +2,5 @@ import {legendColor} from "./legends/color.js";
 
 export function legend({color, ...options}) {
   if (color) return legendColor({...color, ...options});
+  throw new Error(`unsupported legend type`);
 }
