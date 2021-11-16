224b0e2db8519bcda75e513684d5f5c98f3d958d
Philippe Rivière <fil@rezo.net>
2021-11-16 08:52:29 +0100

    Allow the color: {scheme, range} syntax for ordinal scales, to select a part of the domain of an interpolator. (#584)
    (Only continuous color schemes are authorized in that case.)

---
+++ test/plots/metro-unemployment-stroke.js
@@ -0,0 +1,16 @@
+import * as Plot from "@observablehq/plot";
+import * as d3 from "d3";
+
+export default async function() {
+  const data = await d3.csv("data/bls-metro-unemployment.csv", d3.autoType);
+  return Plot.plot({
+    marks: [
+      Plot.line(data, {x: "date", y: "unemployment", stroke: "division"}),
+      Plot.ruleY([0])
+    ],
+    color: {
+      scheme: "blues",
+      range: [0.4, 1]
+    }
+  });
+}
