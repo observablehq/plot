68abf6ed0e2430bb51f4aca1333a47748f47a020
Mike Bostock <mbostock@gmail.com>
2021-11-29 10:16:02 -0800

    tweak example

--- test/plots/wealth-britain-proportion-plot.js
+++ test/plots/wealth-britain-proportion-plot.js
@@ -5,20 +5,13 @@ export default async function() {
   const wide = await d3.csv("data/wealth-britain.csv", d3.autoType);
   const columns = wide.columns.slice(1);
   const data = columns.flatMap(type => wide.map(d => ({age: d.age, type, value: d[type]})));
-  const stack = {
-    x: "type",
-    y: "value",
-    z: "age",
-    order: d3.sort(wide, d => d.shareA).map(d => d.age)
-  };
+  const stack = options => Plot.stackY({}, {x: "type", y: "value", z: "age", ...options});
   return Plot.plot({
-    marginLeft: 50,
-    marginRight: 60,
     x: {
       domain: columns,
       axis: "top",
-      label: "",
-      tickFormat: d => d === "population" ? "Share of population" : "Share of national wealth",
+      label: null,
+      tickFormat: d => `Share of ${d}`,
       tickSize: 0,
       padding: 0 // see margins
     },
@@ -26,41 +19,45 @@ export default async function() {
       axis: null,
       reverse: true
     },
-    color: { scheme: "prgn", reverse: true },
+    color: {
+      scheme: "prgn",
+      reverse: true
+    },
+    marginLeft: 50,
+    marginRight: 60,
     marks: [
-      Plot.areaY(data, Plot.stackY({
-        ...stack,
+      Plot.areaY(data, stack({
         curve: "bump-x",
         fill: "age",
         stroke: "white"
       })),
       Plot.text(
-        data.filter(d => d.type === "population"),
-        Plot.stackY({
-          ...stack,
+        data,
+        stack({
+          filter: d => d.type === "population",
           text: d => `${d.value}%`,
           textAnchor: "end",
           dx: -6
         })
       ),
       Plot.text(
-        data.filter(d => d.type === "wealth"),
-        Plot.stackY({
-          ...stack,
+        data,
+        stack({
+          filter: d => d.type === "wealth",
           text: d => `${d.value}%`,
           textAnchor: "start",
           dx: +6
         })
       ),
       Plot.text(
-        data.filter(d => d.type === "population"),
-        Plot.stackY({
-          ...stack,
+        data,
+        stack({
+          filter: d => d.type === "population",
           text: "age",
           textAnchor: "start",
           fill: "white",
-          fontSize: 20,
-          dx: +18
+          fontWeight: "bold",
+          dx: +8
         })
       )
     ]
