b3e4bf9f20ef91c79ec488d5df8d6df7808a12f6
Philippe Rivière <fil@rezo.net>
2021-12-09 08:11:36 +0100

    Plot.normalize unit tests

---
+++ test/transforms/normalize-test.js
@@ -0,0 +1,48 @@
+import * as Plot from "@observablehq/plot";
+import assert from "assert";
+
+it("Plot.normalize first", () => {
+  testNormalize([1, 2, 4, 5], "first", [1, 2, 4, 5]);
+});
+
+it("Plot.normalize last", () => {
+  testNormalize([1, 2, 4, 5], "last", [0.2, 0.4, 0.8, 1]);
+});
+
+it("Plot.normalize mean", () => {
+  testNormalize([0, 4, 8, 4], "mean", [0, 1, 2, 1]);
+});
+
+it("Plot.normalize median", () => {
+  testNormalize([1, 2, 6, 6], "median", [0.25, 0.5, 1.5, 1.5]);
+});
+
+it("Plot.normalize min", () => {
+  testNormalize([10, 6, 2], "min", [5, 3, 1]);
+});
+
+it("Plot.normalize max", () => {
+  testNormalize([10, 6, 2], "max", [1, 0.6, 0.2]);
+});
+
+it("Plot.normalize sum", () => {
+  testNormalize([1, 1, 6, 2], "sum", [0.1, 0.1, 0.6, 0.2]);
+});
+
+it("Plot.normalize extent", () => {
+  testNormalize([1, 2, 3], "extent", [0, .5, 1]);
+});
+
+it("Plot.normalize deviation", () => {
+  testNormalize([1, 2, 3], "deviation", [-1, 0, 1]);
+});
+
+it("Plot.normalize deviation doesn’t crash on equal values", () => {
+  testNormalize([1, 1], "deviation", [0, 0]);
+});
+
+function testNormalize(data, basis, r) {
+  const mark = Plot.dot(data, Plot.normalizeY(basis, {y: data}));
+  const c = new Map(mark.initialize().channels);
+  assert.deepStrictEqual(c.get("y").value, r);
+}
