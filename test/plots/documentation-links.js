18941ed7c48c0a341d2ba24cbf1a3a6048339dfd
Mike Bostock <mbostock@gmail.com>
2022-01-02 12:21:37 -0800

    tweak test; add snapshot

--- test/plots/documentation-links.js
+++ test/plots/documentation-links.js
@@ -4,29 +4,31 @@ import * as d3 from "d3";
 export default async function() {
   const data = await d3.json("data/plot-documentation.json").then(d => d.listings);
   return Plot.plot({
-    padding: 0.05,
-    color: {
-      scheme: "PiYG",
-      unknown: "#ddd"
-    },
-    x: {domain: [0, 41], clamp: true, grid: true},
-    y: {tickFormat: i => data[i].title.replace(" / Observable Plot", "")},
     marginLeft: 140,
+    x: {
+      domain: [0, 41],
+      clamp: true,
+      grid: true
+    },
+    y: {
+      tickFormat: i => data[i].title.replace(" / Observable Plot", "")
+    },
     marks: [
       Plot.barX(data, {
         x: "likes",
         y: (d, i) => i,
-        href: d => `https://observablehq.com/@observablehq/${d["slug"]}`,
+        href: d => `https://observablehq.com/@observablehq/${d.slug}`,
         target: "_blank"
       }),
       Plot.textX(data, {
         x: "likes",
+        y: (d, i) => i,
         text: d => `${d.likes > 40 ? "⚡︎" : ""} ${d.likes}`,
         textAnchor: "end",
         fill: "white",
-        dx: -3,
-        y: (d, i) => i
-      })
+        dx: -3
+      }),
+      Plot.ruleX([0])
     ]
   });
 }
