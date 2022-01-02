29a4fe705546b19b5062deb896fd7a779d2bcf9a
Philippe Rivière <fil@rezo.net>
2022-01-02 21:22:07 +0100

    test plot for href (#647)
    * cheeky test plot for href
    
    * tweak test; add snapshot
    
    Co-authored-by: Mike Bostock <mbostock@gmail.com>

---
+++ test/plots/documentation-links.js
@@ -0,0 +1,34 @@
+import * as Plot from "@observablehq/plot";
+import * as d3 from "d3";
+
+export default async function() {
+  const data = await d3.json("data/plot-documentation.json").then(d => d.listings);
+  return Plot.plot({
+    marginLeft: 140,
+    x: {
+      domain: [0, 41],
+      clamp: true,
+      grid: true
+    },
+    y: {
+      tickFormat: i => data[i].title.replace(" / Observable Plot", "")
+    },
+    marks: [
+      Plot.barX(data, {
+        x: "likes",
+        y: (d, i) => i,
+        href: d => `https://observablehq.com/@observablehq/${d.slug}`,
+        target: "_blank"
+      }),
+      Plot.textX(data, {
+        x: "likes",
+        y: (d, i) => i,
+        text: d => `${d.likes > 40 ? "⚡︎" : ""} ${d.likes}`,
+        textAnchor: "end",
+        fill: "white",
+        dx: -3
+      }),
+      Plot.ruleX([0])
+    ]
+  });
+}
