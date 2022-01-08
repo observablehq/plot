8a6d0442d1d7f663e8507cf91234488bb4ea3656
Mike Bostock <mbostock@gmail.com>
2022-01-08 12:03:50 -0800

    arrow insets

--- src/marks/arrow.js
+++ src/marks/arrow.js
@@ -15,7 +15,18 @@ const defaults = {
 
 export class Arrow extends Mark {
   constructor(data, options = {}) {
-    const {x1, y1, x2, y2, bend = 0, headAngle = 60, headLength = 8} = options;
+    const {
+      x1,
+      y1,
+      x2,
+      y2,
+      bend = 0,
+      headAngle = 60,
+      headLength = 8,
+      inset = 0,
+      insetStart = inset,
+      insetEnd = inset
+    } = options;
     super(
       data,
       [
@@ -30,10 +41,12 @@ export class Arrow extends Mark {
     this.bend = bend === true ? 22.5 : Math.max(-90, Math.min(90, bend));
     this.headAngle = +headAngle;
     this.headLength = +headLength;
+    this.insetStart = +insetStart;
+    this.insetEnd = +insetEnd;
   }
   render(I, {x, y}, channels) {
     const {x1: X1, y1: Y1, x2: X2 = X1, y2: Y2 = Y1, SW} = channels;
-    const {dx, dy, strokeWidth, bend, headAngle, headLength} = this;
+    const {dx, dy, strokeWidth, bend, headAngle, headLength, insetStart, insetEnd} = this;
     const sw = SW ? i => SW[i] : () => strokeWidth;
 
     // When bending, the offset between the straight line between the two points
@@ -59,19 +72,36 @@ export class Arrow extends Mark {
           .join("path")
             .call(applyDirectStyles, this)
             .attr("d", i => {
-              const x1 = X1[i], y1 = Y1[i], x2 = X2[i], y2 = Y2[i];
-              const dx = x2 - x1, dy = y2 - y1;
-              const lineLength = Math.hypot(dx, dy);
-              const lineAngle = Math.atan2(dy, dx);
+              let x1 = X1[i], y1 = Y1[i], x2 = X2[i], y2 = Y2[i];
 
               // We don’t allow the wing length to be too large relative to the
               // length of the arrow. (Plot.vector allows arbitrarily large
               // wings, but that’s okay since vectors are usually small.)
+              const lineLength = Math.hypot(x2 - x1, y2 - y1);
               const headLength = Math.min(wingScale * sw(i), lineLength / 3);
+
+              // The radius of the circle that intersects with the two endpoints
+              // and has the specified bend angle.
+              const r = Math.hypot(lineLength / Math.tan(bendAngle), lineLength) / 2;
+
+              // Apply insets.
+              if (insetStart || insetEnd) {
+                if (r < 1e5) {
+                  const sign = Math.sign(bendAngle);
+                  const [cx, cy] = pointPointCenter([x1, y1], [x2, y2], r, sign);
+                  if (insetStart) ([x1, y1] = circleCircleIntersect([cx, cy, r], [x1, y1, insetStart], -sign));
+                  if (insetEnd) ([x2, y2] = circleCircleIntersect([cx, cy, r], [x2, y2, insetEnd], sign));
+                } else {
+                  const dx = x2 - x1, dy = y2 - y1, d = Math.hypot(dx, dy);
+                  if (insetStart) x1 += dx / d * insetStart, y1 += dy / d * insetStart;
+                  if (insetEnd) x2 -= dx / d * insetEnd, y2 -= dy / d * insetEnd;
+                }
+              }
 
               // The angle of the arrow as it approaches the endpoint, and the
               // angles of the adjacent wings. Here “left” refers to if the
               // arrow is pointing up.
+              const lineAngle = Math.atan2(y2 - y1, x2 - x1);
               const endAngle = lineAngle + bendAngle;
               const leftAngle = endAngle + wingAngle;
               const rightAngle = endAngle - wingAngle;
@@ -82,10 +112,6 @@ export class Arrow extends Mark {
               const x4 = x2 - headLength * Math.cos(rightAngle);
               const y4 = y2 - headLength * Math.sin(rightAngle);
 
-              // The radius of the circle that intersects with the two endpoints
-              // and has the specified bend angle.
-              const r = Math.hypot(lineLength / Math.tan(bendAngle), lineLength) / 2;
-
               // If the radius is very large (or even infinite, as when the bend
               // angle is zero), then render a straight line.
               return `M${x1},${y1}${r < 1e5 ? `A${r},${r} 0,0,${bendAngle > 0 ? 1 : 0} ` : `L`}${x2},${y2}M${x3},${y3}L${x2},${y2}L${x4},${y4}`;
@@ -93,6 +119,19 @@ export class Arrow extends Mark {
             .call(applyChannelStyles, this, channels))
       .node();
   }
+}
+
+function pointPointCenter([ax, ay], [bx, by], r, sign = 1) {
+  const dx = bx - ax, dy = by - ay, d = Math.hypot(dx, dy);
+  const k = sign * Math.sqrt(r * r - d * d / 4) / d;
+  return [(ax + bx) / 2 - dy * k, (ay + by) / 2 + dx * k];
+}
+
+function circleCircleIntersect([ax, ay, ar], [bx, by, br], sign = 1) {
+  const dx = bx - ax, dy = by - ay, d = Math.hypot(dx, dy);
+  const x = (dx * dx + dy * dy - br * br + ar * ar) / (2 * d);
+  const y = sign * Math.sign(ay) * Math.sqrt(ar * ar - x * x);
+  return [ax + (dx * x + dy * y) / d, ay + (dy * x - dx * y) / d];
 }
 
 export function arrow(data, {x, x1, x2, y, y1, y2, ...options} = {}) {
