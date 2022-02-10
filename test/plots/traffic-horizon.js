232f45e43104bb4d3755a1e8237e9b8239119ed7
Mike Bostock <mbostock@gmail.com>
2022-02-10 11:52:43 -0800

    tweak horizon example (#753)
    * tweak horizon example
    
    * use offset, not index

--- test/plots/traffic-horizon.js
+++ test/plots/traffic-horizon.js
@@ -5,31 +5,34 @@ export default async function() {
   const data = d3.sort(await d3.csv("data/traffic.csv", d3.autoType), d => d.date);
   const bands = 7;
   const step = +(d3.max(data, d => d.value) / bands).toPrecision(2);
-  const format = d3.format(",");
-  const ticks = d3.range(bands).map(d => ({
-    low: d * step,
-    label: `< ${format((d + 1) * step)}`
-  }));
+  const ticks = d3.range(bands).map(i => i * step);
   return Plot.plot({
-    x: {axis: "top"},
-    y: {axis: null, domain: [0, step]},
+    width: 960,
+    height: 1100,
+    x: {
+      axis: "top"
+    },
+    y: {
+      axis: null,
+      domain: [0, step]
+    },
     color: {
+      type: "ordinal",
       scheme: "blues",
-      range: [1 / (bands + 1), 1 - 1 / (bands + 1)],
+      tickFormat: (f => t => `<${f(t + step)}`)(d3.format(",")),
       legend: true
     },
-    fy: { axis: null, domain: new Set(data.map((d) => d.name)) },
-    facet: { data, y: "name" },
+    fy: {
+      axis: null,
+      domain: data.map((d) => d.name) // respect input order
+    },
+    facet: {
+      data,
+      y: "name"
+    },
     marks: [
-      ticks.map(({low, label}) => Plot.areaY(data, {
-        x: "date",
-        y: d => d.value - low,
-        fill: () => label,
-        clip: true
-      })),
+      ticks.map(t => Plot.areaY(data, {x: "date", y: d => d.value - t, fill: () => t, clip: true})),
       Plot.text(data, Plot.selectFirst({text: "name", frameAnchor: "left"}))
-    ],
-    height: 1100,
-    width: 960
+    ]
   });
 }
