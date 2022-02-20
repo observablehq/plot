b44a874752d306d9084fe0dff0798c279c7e3db7
Mike Bostock <mbostock@gmail.com>
2022-02-20 08:55:55 -0800

    x and y reducers (#771)

--- test/plots/athletes-bins-colors.js
+++ test/plots/athletes-bins-colors.js
@@ -5,7 +5,7 @@ export default async function() {
   const athletes = await d3.csv("data/athletes.csv", d3.autoType);
   return Plot.plot({
     marks: [
-      Plot.rectY(athletes, Plot.binX({fill: "x1", y: "count"}, {x: "weight"})),
+      Plot.rectY(athletes, Plot.binX({fill: "x", y: "count"}, {x: "weight"})),
       Plot.ruleY([0])
     ]
   });
