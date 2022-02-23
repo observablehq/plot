9679c6fd2f8b5a2adceaa04eda0a631acdcbb97a
Philippe Rivière <fil@rezo.net>
2022-02-23 20:25:11 +0100

    default options: Plot.boxX(values)

--- src/marks/box.js
+++ src/marks/box.js
@@ -1,6 +1,6 @@
 import {min, max, quantile} from "d3";
 import {marks} from "../plot.js";
-import {groupX, groupY} from "../transforms/group.js";
+import {groupX, groupY, groupZ} from "../transforms/group.js";
 import {map} from "../transforms/map.js";
 import {barX, barY} from "./bar.js";
 import {dot} from "./dot.js";
@@ -19,10 +19,11 @@ export function boxX(data, {
   strokeWidth = 2,
   ...options
 } = {}) {
+  const group = y != null ? groupY : groupZ;
   return marks(
-    ruleY(data, groupY({x1: iqr1, x2: iqr2}, {x, y, stroke, strokeOpacity, ...options})),
-    barX(data, groupY({x1: "p25", x2: "p75"}, {x, y, fill, fillOpacity, ...options})),
-    tickX(data, groupY({x: "p50"}, {x, y, stroke, strokeOpacity, strokeWidth, ...options})),
+    ruleY(data, group({x1: iqr1, x2: iqr2}, {x, y, stroke, strokeOpacity, ...options})),
+    barX(data, group({x1: "p25", x2: "p75"}, {x, y, fill, fillOpacity, ...options})),
+    tickX(data, group({x: "p50"}, {x, y, stroke, strokeOpacity, strokeWidth, ...options})),
     dot(data, map({x: oqr}, {x, y, z: y, stroke, strokeOpacity, ...options}))
   );
 }
@@ -39,10 +40,11 @@ export function boxY(data, {
   strokeWidth = 2,
   ...options
 } = {}) {
+  const group = x != null ? groupX : groupZ;
   return marks(
-    ruleX(data, groupX({y1: iqr1, y2: iqr2}, {x, y, stroke, strokeOpacity, ...options})),
-    barY(data, groupX({y1: "p25", y2: "p75"}, {x, y, fill, fillOpacity, ...options})),
-    tickY(data, groupX({y: "p50"}, {x, y, stroke, strokeOpacity, strokeWidth, ...options})),
+    ruleX(data, group({y1: iqr1, y2: iqr2}, {x, y, stroke, strokeOpacity, ...options})),
+    barY(data, group({y1: "p25", y2: "p75"}, {x, y, fill, fillOpacity, ...options})),
+    tickY(data, group({y: "p50"}, {x, y, stroke, strokeOpacity, strokeWidth, ...options})),
     dot(data, map({y: oqr}, {x, y, z: x, stroke, strokeOpacity, ...options}))
   );
 }
