5c073b1571a6414f558b4e23827ec26ea649bde5
Mike Bostock <mbostock@gmail.com>
2022-03-11 08:15:13 -0800

    simpler hexagon

--- src/symbols.js
+++ src/symbols.js
@@ -1,17 +1,17 @@
 import {symbolAsterisk, symbolDiamond2, symbolPlus, symbolSquare2, symbolTriangle2, symbolX as symbolTimes} from "d3";
 import {symbolCircle, symbolCross, symbolDiamond, symbolSquare, symbolStar, symbolTriangle, symbolWye} from "d3";
 
-const t = Math.sqrt(3) / 2; // TODO decide on radius definition
+const w = 2 / Math.sqrt(3);
 
 const symbolHexagon = {
   draw(context, size) {
-    const s = Math.sqrt(size / Math.PI) * 2 / Math.sqrt(3), hs = s / 2, ts = s * t;
-    context.moveTo(0, s);
-    context.lineTo(ts, hs);
-    context.lineTo(ts, -hs);
-    context.lineTo(0, -s);
-    context.lineTo(-ts, -hs);
-    context.lineTo(-ts, hs);
+    const s = Math.sqrt(size / Math.PI), t = s * w, h = t / 2;
+    context.moveTo(0, t);
+    context.lineTo(s, h);
+    context.lineTo(s, -h);
+    context.lineTo(0, -t);
+    context.lineTo(-s, -h);
+    context.lineTo(-s, h);
     context.closePath();
   }
 };
