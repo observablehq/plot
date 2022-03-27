bddf2749ad81d45bda475cd2d1c221c00dd24178
Philippe Rivière <fil@rezo.net>
2022-03-27 10:27:27 +0200

    a more generic "remap"

--- test/plots/darker-dodge.js
+++ test/plots/darker-dodge.js
@@ -2,45 +2,29 @@ import * as Plot from "@observablehq/plot";
 import * as d3 from "d3";
 import {composeInitialize} from "../../src/transforms/basic.js";
 
-const darker = (darker = 0.7, options = {}) => {
-  if (typeof darker === "object") {
-    options = darker;
-    darker = "darker" in options ? options.darker : 0.7;
-  }
-
-  function darken(data, facets, channels, scales) {
-    const F = channels.fill && new Array(channels.fill.value.length);
-    const S = channels.stroke && new Array(channels.stroke.value.length);
-    if (F) {
-      for (const facet of facets) {
-        for (const i of facet) {
-          let v = channels.fill.value[i];
-          if (channels.fill.scale === "color") v = scales.color(v);
-          F[i] = d3.rgb(v).darker(darker).formatHex();
+function remap(outputs = {}, options) {
+  return composeInitialize(options, (data, facets, channels, scales) => {
+    const newChannels = {};
+    for (const [key, map] of Object.entries(outputs)) {
+      const input = channels[key];
+      if (input == null) throw new Error(`missing channel: ${key}`);
+      const V = Array.from(input.value);
+      if (input.scale != null) {
+        const scale = scales[input.scale];
+        if (scale != null) {
+          for (let i = 0; i < V.length; ++i) V[i] = scale(V[i]);
         }
       }
-    }
-    if (S) {
-      for (const facet of facets) {
-        for (const i of facet) {
-          let v = channels.stroke.value[i];
-          if (channels.stroke.scale === "color") v = scales.color(v);
-          S[i] = d3.rgb(v).darker(darker).formatHex();
-        }
-      }
+      for (let i = 0; i < V.length; ++i) V[i] = map(V[i]);
+      newChannels[key] = {value: V};
     }
     return {
       data,
       facets,
-      channels: {
-        ...(F && { fill: { value: F } }),
-        ...(S && { stroke: { value: S } })
-      }
+      channels: newChannels
     };
-  }
-
-  return composeInitialize(options, darken);
-};
+  });
+}
 
 // In the following, darker and Plot.dodgeY are interchangeable
 export default async function() {
@@ -50,7 +34,12 @@ export default async function() {
     marks: [
       Plot.dotX(
         Array.from({ length: 150 }, d3.randomLogNormal.source(d3.randomLcg(42))()),
-        Plot.dodgeY("middle", darker({ x: (d) => d, fill: (d) => d }))
+        Plot.dodgeY("middle", remap({
+          fill: v => d3.rgb(v).darker(0.7).formatHex()
+        }, {
+          x: (d) => d,
+          fill: (d) => d
+        }))
       )
     ],
     height: 170
