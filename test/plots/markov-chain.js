7dea0b690de586d9e0ef64e625e8662d37f48c06
Philippe Rivière <fil@rezo.net>
2022-03-29 09:46:30 +0200

    remove arrows with a non-positive length fixes inset bug closes #832

--- test/plots/markov-chain.js
+++ test/plots/markov-chain.js
@@ -4,14 +4,15 @@ import * as d3 from "d3";
 export default async function() {
   const M = [[0.3, 0.2, 0.5], [0.1, 0.7, 0.2], [0.1, 0.1, 0.8]];
   const centers = d3.range(M.length).map(i => d3.pointRadial((2 - i) * 2 * Math.PI / M.length, 100));
+  const factors = M.flatMap((row, i) => row.map((value, j) => ({i, j, value})));
   return Plot.plot({
     width: 400,
     height: 380,
-    inset: 100,
+    inset: 60,
     axis: null,
     marks: [
       Plot.dot(centers, { r: 40 }),
-      Plot.arrow(M.flatMap((row, i) => row.map((value, j) => ({i, j, value}))), {
+      Plot.arrow(factors, {
         x1: ({i}) => centers[i][0],
         y1: ({i}) => centers[i][1],
         x2: ({j}) => centers[j][0],
@@ -20,7 +21,12 @@ export default async function() {
         bend: true,
         inset: 55
       }),
-      Plot.text(centers, {text: ["A", "B", "C"]})
+      Plot.text(centers, {text: ["A", "B", "C"], dy: 55}),
+      Plot.text(factors, {
+        x: ({i, j}) => (centers[i][0] + centers[j][0]) / 2 + (centers[i][1] - centers[j][1]) * 0.16,
+        y: ({i, j}) => (centers[i][1] + centers[j][1]) / 2 - (centers[i][0] - centers[j][0]) * 0.16,
+        text: "value"
+      })
     ]
   });
 }
