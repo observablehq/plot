3eeb42b71adde1af6132ab48713df35fdc0d2b96
Philippe Rivière <fil@rezo.net>
2022-04-19 09:52:19 +0200

    all shorthand tests

---
+++ test/plots/shorthand-groupBarY.js
@@ -0,0 +1,6 @@
+import * as Plot from "@observablehq/plot";
+
+export default async function() {
+  const gene = "AAAAGAGTGAAGATGCTGGAGACGAGTGAAGCATTCACTTTAGGGAAAGCGAGGCAAGAGCGTTTCAGAAGACGAAACCTGGTAGGTGCACTCACCACAG";
+  return Plot.barY(gene, Plot.groupX()).plot();
+}
