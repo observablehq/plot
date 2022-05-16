b2c57371d3f6698a93fc4eeb2176e8ba5fa95f46
Mike Bostock <mbostock@gmail.com>
2022-05-16 10:58:19 -0700

    sketch of .d.ts

---
+++ src/options.d.ts
@@ -0,0 +1,11 @@
+export function string(x: null): null;
+export function string(x: undefined): undefined;
+export function string(x: any): string;
+
+export function number(x: null): null;
+export function number(x: undefined): undefined;
+export function number(x: any): number;
+
+export function boolean(x: null): null;
+export function boolean(x: undefined): undefined;
+export function boolean(x: any): boolean;
