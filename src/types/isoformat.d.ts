e997d9dfdfc3c5141de4ff57f05370efdd81c741
Philippe Rivière <fil@rezo.net>
2022-05-26 16:10:27 +0200

    move isoformat.d.ts to src/types/

--- src/types/isoformat.d.ts
+++ src/types/isoformat.d.ts
@@ -2,4 +2,5 @@
 declare module 'isoformat' {
     export function format(value: any, fallback: string): string;
     export function format(value: any, fallback: any): any;
+    export function parse(value: string): Date;
 }
