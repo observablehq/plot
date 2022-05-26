30da5cb6f2dac7d93035c07236c6b47cec664015
Michael Cooper <mythmon@gmail.com>
2022-05-26 08:14:37 -0700

    Use object for initial cache key in memoize again

--- src/memoize.ts
+++ src/memoize.ts
@@ -1,8 +1,8 @@
 export function memoize1<R extends Array<unknown>, T>(compute: (...rest: R) => T): (...rest: R) => T {
   let cacheValue: T;
-  let cacheKeys: R | null = null;
+  let cacheKeys: R | {length?: undefined} = {};
   return (...keys: R) => {
-    if (cacheKeys?.length !== keys.length || cacheKeys.some((k, i) => k !== keys[i])) {
+    if (cacheKeys.length !== keys.length || cacheKeys.some((k, i) => k !== keys[i])) {
       cacheKeys = keys;
       cacheValue = compute(...keys);
     }
