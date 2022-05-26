383eec9b305b887a4c3e142782868cffcf9f5bba
Philippe Rivière <fil@rezo.net>
2022-05-26 15:46:04 +0200

    make github happier?

--- src/isoformat.d.ts
+++ src/isoformat.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module 'isoformat' {
     export function format(value: any, fallback: string): string;
     export function format(value: any, fallback: any): any;
