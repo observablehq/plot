cda1bdac6ff4bb68aeeeebe54650bc6a9aaaeea0
Mike Bostock <mbostock@gmail.com>
2022-05-27 22:19:54 -0700

    tweak tests

--- test/plots/penguin-facet-dodge-symbol.js
+++ test/plots/penguin-facet-dodge-symbol.js
@@ -4,13 +4,13 @@ import * as d3 from "d3";
 export default async function() {
   const penguins = await d3.csv("data/penguins.csv", d3.autoType);
   return Plot.plot({
-    height: 450,
-    width: 300,
-    marginRight: 60,
-    y: {grid: true, nice: true},
-    symbol: {legend: true},
+    grid: true,
+    nice: true,
+    symbol: {
+      legend: true
+    },
     marks: [
-      Plot.dot(penguins, Plot.dodgeX("left", {y: "body_mass_g", symbol: "species", stroke: "species", dx: 2}))
+      Plot.dot(penguins, Plot.dodgeX("left", {y: "body_mass_g", symbol: "species", stroke: "species"}))
     ]
   });
 }
