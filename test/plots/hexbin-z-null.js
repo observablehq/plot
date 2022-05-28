d4a286b8b90d2a6077bca369ed715530618f7419
Mike Bostock <mbostock@gmail.com>
2022-05-27 20:36:31 -0700

    tweak tests

--- test/plots/hexbin-z-null.js
+++ test/plots/hexbin-z-null.js
@@ -4,20 +4,17 @@ import * as d3 from "d3";
 export default async function() {
   const penguins = await d3.csv("data/penguins.csv", d3.autoType);
   return Plot.plot({
-    x: {inset: 10},
-    y: {inset: 10},
+    inset: 10,
     marks: [
-      Plot.frame(),
       Plot.hexgrid(),
-      Plot.dot(penguins, Plot.hexbin({r: "count"}, {
-        x: "culmen_depth_mm",
-        y: "culmen_length_mm",
-        stroke: "species",
-        fill: "island",
-        fillOpacity: 0.5,
-        z: null,
-        symbol: "dot" // TODO Plot.circle
-      }))
+      Plot.frame(),
+      Plot.circle(
+        penguins,
+        Plot.hexbin(
+          {r: "count", stroke: "mode", fill: "mode"},
+          {x: "culmen_depth_mm", y: "culmen_length_mm", z: null, fill: "island", stroke: "species"}
+        )
+      )
     ]
   });
 }
