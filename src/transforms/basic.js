6f0f10d36864c14b1a7819ee84c653d807f4157b
Mike Bostock <mbostock@gmail.com>
2022-05-28 08:27:29 -0700

    preserve this with composed transforms

--- src/transforms/basic.js
+++ src/transforms/basic.js
@@ -28,9 +28,9 @@ export function basic({
 function composeTransform(t1, t2) {
   if (t1 == null) return t2 === null ? undefined : t2;
   if (t2 == null) return t1 === null ? undefined : t1;
-  return (data, facets) => {
-    ({data, facets} = t1(data, facets));
-    return t2(arrayify(data), facets);
+  return function(data, facets) {
+    ({data, facets} = t1.call(this, data, facets));
+    return t2.call(this, arrayify(data), facets);
   };
 }
 
