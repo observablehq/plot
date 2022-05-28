d0129307c672336578a0d3dc5e3c51f1a4baeeb1
Mike Bostock <mbostock@gmail.com>
2022-05-27 20:38:02 -0700

    tweak tests

--- test/plots/hexbin-z.js
+++ test/plots/hexbin-z.js
@@ -4,13 +4,14 @@ import * as d3 from "d3";
 export default async function() {
   const penguins = await d3.csv("data/penguins.csv", d3.autoType);
   return Plot.plot({
-    x: {inset: 10},
-    y: {inset: 10},
-    color: {legend: true},
+    inset: 10,
+    color: {
+      legend: true
+    },
     marks: [
-      Plot.frame(),
       Plot.hexgrid(),
-      Plot.dot(penguins, Plot.hexbin({r: "count"}, {x: "culmen_length_mm", y: "body_mass_g", stroke: "species", strokeOpacity: 0.8})) // TOD remove strokeOpacity
+      Plot.frame(),
+      Plot.dot(penguins, Plot.hexbin({r: "count"}, {x: "culmen_length_mm", y: "body_mass_g", stroke: "species"}))
     ]
   });
 }
