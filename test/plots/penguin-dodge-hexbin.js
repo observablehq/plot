7c4fc40c528407c8c6832d28d009cb0c912e5d48
Mike Bostock <mbostock@gmail.com>
2022-05-27 20:40:38 -0700

    tweak tests

--- test/plots/penguin-dodge-hexbin.js
+++ test/plots/penguin-dodge-hexbin.js
@@ -1,7 +1,7 @@
 import * as Plot from "@observablehq/plot";
 import * as d3 from "d3";
 
-// test channel transform composition
+// Test channel transform composition.
 export default async function() {
   const penguins = await d3.csv("data/penguins.csv", d3.autoType);
   return Plot.plot({
@@ -9,6 +9,9 @@ export default async function() {
     x: {
       grid: true,
       inset: 7
+    },
+    color: {
+      legend: true
     },
     facet: {
       data: penguins,
@@ -20,7 +23,6 @@ export default async function() {
       Plot.frame(),
       Plot.dot(penguins, Plot.dodgeY("bottom", {x: "body_mass_g", stroke: "red", r: 3})),
       Plot.dot(penguins, Plot.hexbin({}, Plot.dodgeY("bottom", {x: "body_mass_g", fill: "black", r: 3, binWidth: 7})))
-    ],
-    color: {legend: true}
+    ]
   });
 }
