5f1ac861546c5b427103de90405dae544336b9aa
Mike Bostock <mbostock@gmail.com>
2022-06-01 08:26:45 -0700

    tweak style

--- src/plot.js
+++ src/plot.js
@@ -116,6 +116,7 @@ export function plot(options = {}) {
     Object.assign(scaleDescriptors, newScaleDescriptors);
     Object.assign(scales, newScales);
   }
+
   autoScaleLabels(channelsByScale, scaleDescriptors, axes, dimensions, options);
 
   // Compute value objects, applying scales as needed.
