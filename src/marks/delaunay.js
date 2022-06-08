a7f110fa5ded2ac99d38e8aa652abf54bcc57ec8
Mike Bostock <mbostock@gmail.com>
2022-06-08 10:56:17 -0700

    a few fixes

--- src/marks/delaunay.js
+++ src/marks/delaunay.js
@@ -28,13 +28,13 @@ const hullDefaults = {
 
 export class DelaunayLink extends Mark {
   constructor(data, options = {}) {
-    const {x, y, curve, tension} = options;
+    const {x, y, z, stroke, curve, tension} = options;
     super(
       data,
       [
         {name: "x", value: x, scale: "x"},
         {name: "y", value: y, scale: "y"},
-        {name: "z", value: maybeZ(options), optional: true}
+        {name: "z", value: maybeZ({z, stroke}), optional: true}
       ],
       options,
       linkDefaults
@@ -52,12 +52,11 @@ export class DelaunayLink extends Mark {
       const newIndex = [];
       const newChannels = {};
       for (const k in channels) newChannels[k] = [];
-  
       const X1 = [];
       const X2 = [];
       const Y1 = [];
       const Y2 = [];
-  
+
       function link(ti, tj) {
         ti = index[ti];
         tj = index[tj];
@@ -68,7 +67,7 @@ export class DelaunayLink extends Mark {
         Y2[i] = Y[tj];
         for (const k in channels) newChannels[k].push(channels[k][tj]);
       }
-  
+
       const {halfedges, hull, triangles} = Delaunay.from(index, i => X[i], i => Y[i]);
       for (let i = 0; i < halfedges.length; ++i) { // inner edges
         const j = halfedges[i];
@@ -77,22 +76,23 @@ export class DelaunayLink extends Mark {
       for (let i = 0; i < hull.length; ++i) { // convex hull
         link(hull[i], hull[(i + 1) % hull.length]);
       }
+
       select(this)
-      .selectAll()
-      .data(newIndex)
-      .join("path")
-        .call(applyDirectStyles, mark)
-        .attr("d", (i) => {
-          const p = path();
-          const c = curve(p);
-          c.lineStart();
-          c.point(X1[i], Y1[i]);
-          c.point(X2[i], Y2[i]);
-          c.lineEnd();
-          return p;
-        })
-        .call(applyChannelStyles, mark, newChannels)
-        .call(applyMarkers, mark, newChannels);
+        .selectAll()
+        .data(newIndex)
+        .join("path")
+          .call(applyDirectStyles, mark)
+          .attr("d", i => {
+            const p = path();
+            const c = curve(p);
+            c.lineStart();
+            c.point(X1[i], Y1[i]);
+            c.point(X2[i], Y2[i]);
+            c.lineEnd();
+            return p;
+          })
+          .call(applyChannelStyles, mark, newChannels)
+          .call(applyMarkers, mark, newChannels);
     }
 
     return create("svg:g")
@@ -107,13 +107,13 @@ export class DelaunayLink extends Mark {
 
 export class DelaunayMesh extends Mark {
   constructor(data, options = {}, defaults = meshDefaults) {
-    const {x, y} = options;
+    const {x, y, z, stroke} = options;
     super(
       data,
       [
         {name: "x", value: x, scale: "x"},
         {name: "y", value: y, scale: "y"},
-        {name: "z", value: maybeZ(options), optional: true}
+        {name: "z", value: maybeZ({z, stroke}), optional: true}
       ],
       options,
       defaults
@@ -126,22 +126,19 @@ export class DelaunayMesh extends Mark {
   render(index, {x, y}, {x: X, y: Y, z: Z, ...channels}, dimensions) {
     const {dx, dy} = this;
     const mark = this;
-  function mesh(render) {
-      return function (index) {
+    function mesh(render) {
+      return function(index) {
         const delaunay = Delaunay.from(index, i => X[i], i => Y[i]);
-        const newChannels = {};
-        for (const k in channels) newChannels[k] = {[index[0]]: channels[k][index[0]]};
         select(this).append("path")
           .datum(index[0])
           .call(applyDirectStyles, mark)
-          .call(applyTransform, x, y, offset + dx, offset + dy)
           .attr("d", render(delaunay, dimensions))
-          .call(applyChannelStyles, mark, newChannels)
-          .call(applyMarkers, mark, newChannels);
+          .call(applyChannelStyles, mark, channels);
       };
     }
     return create("svg:g")
         .call(applyIndirectStyles, this, dimensions)
+        .call(applyTransform, x, y, offset + dx, offset + dy)
         .call(Z
           ? g => g.selectAll().data(group(index, i => Z[i]).values()).enter().append("g").each(mesh(this._render))
           : g => g.datum(index).each(mesh(this._render)))
