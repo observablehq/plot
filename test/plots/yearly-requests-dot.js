3832868b923aac2492e8311d3456f23ac2fcaed9
Philippe Rivière <fil@rezo.net>
2022-06-13 20:49:24 +0200

    fix date in test (#937)
    * fix date in test
    
    Co-authored-by: Mike Bostock <mbostock@gmail.com>

--- test/plots/yearly-requests-dot.js
+++ test/plots/yearly-requests-dot.js
@@ -3,9 +3,9 @@ import * as Plot from "@observablehq/plot";
 
 export default async function() {
   const requests = [
-    [new Date("2002-1-01"), 9],
-    [new Date("2003-1-01"), 17],
-    [new Date("2005-1-01"), 5]
+    [new Date("2002-01-01"), 9],
+    [new Date("2003-01-01"), 17],
+    [new Date("2005-01-01"), 5]
   ];
   return Plot.plot({
     label: null,
