563ce906e6b4e25d69c389d00488d1b8086d25b9
Philippe Rivière <fil@rezo.net>
2022-06-24 22:38:49 +0200

    density contours stroke & fill (#948)
    * density contours as an initializer: first pass
    
    * density by z, carrying styles and channels (no reducer, only "first")
    
    * fill: "density"
    
    * density weight
    
    * consistent thresholds across facets and series
    
    note: we have to go around this bug in d3-contour: https://github.com/d3/d3-contour/pull/57
    
    * allow initializer composition; move initializer to the class; clean up
    
    * density weight example
    
    * error if x or y is undefined
    
    (in the future we might route those to 1-dimensional transforms—KDE)
    
    * document
    
    * reduce img
    
    * * don't apply the scales if we are already in pixel space (e.g. when composing with the hexbin transform)
    * avoids a crash when there is no contour
    
    * 1d density contours with frameAnchor
    
    * adopt d3@7.4.5 for https://github.com/d3/d3-contour/releases/tag/v3.0.2
    
    (not entirely sure if it's good practice to have all the yarn.lock changes in the PR, beyond those that are relevant)
    
    * replace example image
    
    * mike’s edits
    
    * fix image dimensions
    
    * isDensity
    
    * distinct
    
    * tweak tests
    
    Co-authored-by: Mike Bostock <mbostock@gmail.com>

---
+++ test/plots/faithful-density-1d.js
@@ -0,0 +1,15 @@
+import * as Plot from "@observablehq/plot";
+import * as d3 from "d3";
+
+export default async function() {
+  const faithful = await d3.tsv("data/faithful.tsv", d3.autoType);
+  return Plot.plot({
+    height: 100,
+    inset: 20,
+    marks: [
+      Plot.density(faithful, {x: "waiting", stroke: "steelblue", strokeWidth: 0.25, bandwidth: 10}),
+      Plot.density(faithful, {x: "waiting", stroke: "steelblue", thresholds: 4, bandwidth: 10}),
+      Plot.dot(faithful, {x: "waiting", fill: "currentColor", r: 1.5})
+    ]
+  });
+}
---
+++ test/plots/seattle-precipitation-density.js
@@ -0,0 +1,12 @@
+import * as Plot from "@observablehq/plot";
+import * as d3 from "d3";
+
+export default async function() {
+  const data = await d3.csv("data/seattle-weather.csv", d3.autoType);
+  return Plot.plot({
+    marks: [
+      Plot.density(data, {x: "temp_min", y: "wind", weight: "precipitation"}),
+      Plot.dot(data, {x: "temp_min", y: "wind", r: "precipitation", fill: "steelblue", fillOpacity: 0.5})
+    ]
+  });
+}
