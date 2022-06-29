66d01da6e91616f023a75f9e4f71810c548b226b
Mike Bostock <mbostock@gmail.com>
2022-06-29 09:09:51 -0400

    fix formatting

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,8 @@ import glob from "glob";
 // Vite will automatically try to resolve to .ts files when an imported .js file
 // doesn't exist, but only if the importer is .ts. In order to fall back on .ts
 // for all.js imports that don't exist, we provide a customResolver below that
-// will do this based on the js and ts files we have in src/.
-// Once all of the files in src/ are converted to TypeScript we can remove
-// this customLoader.
+// will do this based on the js and ts files we have in src/. Once all of the
+// files in src/ are converted to TypeScript we can remove this customLoader.
 const typescriptPaths = new Set(glob.sync(`${path.resolve("./src")}/**/*.ts`));
 const javascriptPaths = new Set(glob.sync(`${path.resolve("./src")}/**/*.js`));
 
