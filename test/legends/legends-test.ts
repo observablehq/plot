e024d1bcb92a690434c5dcc68c67d130c8b68f03
Mike Bostock <mbostock@gmail.com>
2022-07-04 07:06:01 -0400

    Update test/legends/legends-test.ts
    Co-authored-by: Philippe Rivière <fil@rezo.net>

--- test/legends/legends-test.ts
+++ test/legends/legends-test.ts
@@ -5,10 +5,6 @@ it(`Plot.legend({color: {type: "identity"}}) returns undefined`, () => {
   assert.strictEqual(Plot.legend({color: {type: "identity"}}), undefined);
 });
 
-it(`Plot.legend({color: {type: "identity"}}) returns undefined`, () => {
-  assert.strictEqual(Plot.legend({color: {type: "identity"}}), undefined);
-});
-
 it(`Plot.legend({legend: "swatches", color: {type: "<not-ordinal>"}}) throws an error`, () => {
   assert.throws(() => Plot.legend({legend: "swatches", color: {type: "linear"}}), /swatches legend requires ordinal color scale \(not linear\)/);
   assert.throws(() => Plot.legend({legend: "swatches", color: {type: "diverging"}}), /swatches legend requires ordinal color scale \(not diverging\)/);
