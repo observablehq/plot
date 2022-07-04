68f2f86c92182ce25be18131a56d521ede22b966
Mike Bostock <mbostock@gmail.com>
2022-07-04 07:07:31 -0400

    swatches legend requires ordinal scale (#971)
    * swatches legend requires ordinal scale
    
    * Update test/legends/legends-test.ts
    
    Co-authored-by: Philippe Rivière <fil@rezo.net>
    
    Co-authored-by: Philippe Rivière <fil@rezo.net>

--- test/legends/legends-test.ts
+++ test/legends/legends-test.ts
@@ -1,9 +1,13 @@
 import * as Plot from "@observablehq/plot";
 import * as assert from "assert";
 
-it("Plot.legend({color: {type:'identity'}}) returns undefined", () => {
-  const l = Plot.legend({color: {type: "identity"}});
-  assert.strictEqual(l, undefined);
+it(`Plot.legend({color: {type: "identity"}}) returns undefined`, () => {
+  assert.strictEqual(Plot.legend({color: {type: "identity"}}), undefined);
+});
+
+it(`Plot.legend({legend: "swatches", color: {type: "<not-ordinal>"}}) throws an error`, () => {
+  assert.throws(() => Plot.legend({legend: "swatches", color: {type: "linear"}}), /swatches legend requires ordinal color scale \(not linear\)/);
+  assert.throws(() => Plot.legend({legend: "swatches", color: {type: "diverging"}}), /swatches legend requires ordinal color scale \(not diverging\)/);
 });
 
 it("Plot.legend({}) throws an error", () => {
