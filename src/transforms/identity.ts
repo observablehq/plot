c0a7a527980828f122b4786a0e31cd473beedd81
Philippe Rivière <fil@rezo.net>
2022-07-20 16:57:56 +0200

    transforms/identity.ts

--- src/transforms/identity.ts
+++ src/transforms/identity.ts
@@ -1,13 +1,14 @@
+import type {MarkOptions} from "../common.js";
 import {identity} from "../options.js";
 
-export function maybeIdentityX(options = {}) {
+export function maybeIdentityX(options: MarkOptions = {}) {
   const {x, x1, x2} = options;
   return x1 === undefined && x2 === undefined && x === undefined
     ? {...options, x: identity}
     : options;
 }
 
-export function maybeIdentityY(options = {}) {
+export function maybeIdentityY(options: MarkOptions = {}) {
   const {y, y1, y2} = options;
   return y1 === undefined && y2 === undefined && y === undefined
     ? {...options, y: identity}
