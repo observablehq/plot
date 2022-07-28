be0ac86c1408538a48edc02fd8934d13111c37b1
Philippe Rivière <fil@rezo.net>
2022-07-28 14:13:16 +0200

    An arguably better test/example plot.

--- test/plots/facet-warning.js
+++ test/plots/facet-warning.js
@@ -2,19 +2,21 @@ import * as Plot from "@observablehq/plot";
 import * as d3 from "d3";
 
 export default async function () {
-  const AAPL = await d3.csv("data/aapl.csv", d3.autoType);
+  const anscombe = await d3.csv("data/anscombe.csv", d3.autoType);
   return Plot.plot({
+    grid: true,
+    inset: 10,
+    width: 960,
+    height: 240,
     facet: {
-      data: AAPL.filter((d) => d.Close > 0),
-      y: (d) => d3.utcFormat("%Y")(+d.Date)
-    },
-    y: {
-      grid: true
+      data: anscombe.filter((_, i) => i % 2),
+      x: "series"
     },
     marks: [
-      Plot.line(
-        AAPL.filter((d) => d.Close > 0),
-        {x: "Date", y: "Close", strokeWidth: 1}
+      Plot.frame(),
+      Plot.dot(
+        anscombe.filter((_, i) => i % 2),
+        {x: "x", y: "y" /* , facet: true */}
       )
     ]
   });
