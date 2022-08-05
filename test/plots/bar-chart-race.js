fd3a3f6d0c13cd2a073ad933b7bd7a3267cbee71
Philippe Rivière <fil@rezo.net>
2022-08-05 12:10:32 +0200

    improve bar chart race

--- test/plots/bar-chart-race.js
+++ test/plots/bar-chart-race.js
@@ -4,56 +4,79 @@ import * as d3 from "d3";
 export default async function() {
   const brands = await d3.csv("data/category-brands.csv", d3.autoType);
   return Plot.plot({
+    width: 980,
+    height: 15 * 40,
     marginTop: 20,
     marginLeft: 10,
+    marginBottom: 10,
     x: { axis: null, domain: [0, 1] },
     y: { domain: d3.range(0, 15), axis: null },
+    color: {
+      scheme: "tableau10",
+      domain: d3.groupSort(
+        brands,
+        (v) => d3.mean(v, (d) => d.value),
+        (d) => d.category
+      )
+    },
     time: {
       delay: 1000,
-      duration: 10000
+      duration: 20000
     },
     marks: [
-      Plot.tickX(
-        brands,
-        ticks({
-          x: "value",
-          time: "date",
-          stroke: "rgba(0,0,0,0.1)"
-        })
-      ),
       Plot.barX(
         brands,
-        Plot.map(
-          {
-            x: (values) => {
-              const m = d3.max(values);
-              return values.map((d) => d / m);
-            },
-            y: "rank",
-            sort: "rank"
-          },
-          {
+        Plot.normalizeX(
+          Plot.mapY("rank", {
+            fillOpacity: 0.6,
+            mixBlendMode: "multiply",
             x: "value",
             y: (d) => -d.value,
-            sort: (d) => -d.value,
             fill: "category",
             time: "date",
             key: "name",
             z: null
-          }
+          })
         )
       ),
+  
+      // ticks
+      ((data, options) => {
+        options = Plot.mapY("rank", { ...options, title: options.x });
+        const b = Plot.tickX(data, options);
+        b.render = (
+          index,
+          { x },
+          { title: T },
+          { height, marginTop, marginBottom }
+        ) =>
+          d3
+            .create("svg:g")
+            .attr("transform", "translate(0, 20)")
+            .call(
+              d3
+                .axisTop(
+                  d3.scaleLinear([0, d3.max(index, (i) => T[i])], x.range())
+                )
+                .ticks(8)
+                .tickSize(-height + marginTop + marginBottom)
+            )
+            .call((g) => {
+              g.select(".domain").remove();
+              g.attr("stroke-width", 0.75).attr("font-size", 8);
+              g.selectAll("line").attr("stroke", "white");
+            })
+            .node();
+        return b;
+      })(brands, {
+        x: "value",
+        time: "date",
+        key: "name",
+        y: (d) => -d.value,
+        z: null
+      }),
       Plot.tickX([0]),
-      Plot.text(
-        brands,
-        ticks({
-          x: "value",
-          time: "date",
-          frameAnchor: "top",
-          dy: -20,
-          fontSize: 8
-        })
-      ),
+  
       Plot.text(
         brands,
         Plot.selectMaxX({
@@ -70,85 +93,46 @@ export default async function() {
           strokeWidth: 30
         })
       ),
-      Plot.text(
-        brands,
-        Plot.selectMaxX({
-          x: (d) => d.value * 1e-9 + 1,
-          text: "value",
-          frameAnchor: "top-right",
-          time: "date",
-          fill: "black",
-          stroke: "white",
-          strokeWidth: 7,
-          dy: -20
-        })
-      ),
+  
       Plot.textX(
         brands,
-        Plot.map(
-          {
-            x: (values) => {
-              const m = d3.max(values);
-              return values.map((d) => d / m);
-            },
-            y: "rank",
-            sort: "rank"
-          },
-          {
+        Plot.normalizeX(
+          Plot.mapY("rank", {
             x: "value",
-            sort: (d) => -d.value,
             y: (d) => -d.value,
             text: "name",
             textAnchor: "end",
+            fontSize: 12,
             dx: -4,
+            dy: -7,
             time: "date",
             key: "name",
-            fill: "white",
+            fill: "currentColor",
+            fontWeight: "bold",
             z: null
-          }
+          })
+        )
+      ),
+  
+      Plot.textX(
+        brands,
+        Plot.normalizeX(
+          Plot.mapY("rank", {
+            x: "value",
+            sort: (d) => -d.value,
+            y: (d) => -d.value,
+            text: "value",
+            textAnchor: "end",
+            dx: -4,
+            dy: 8,
+            time: "date",
+            key: "name",
+            fill: "currentColor",
+            z: null
+          })
         )
       )
     ]
   });
-
-  function ticks({ x, text, ticks = 8, ...options }) {
-    const [X, setX] = Plot.column(x);
-    const [T, setT] = Plot.column(text);
-    const [O, setO] = Plot.column("opacity");
-    const textFormat = d3.format(","); // string, no interpolation
-  
-    const transform = (data, facets) => {
-      const newFacets = [];
-      const V = Plot.valueof(data, x);
-      const T = setT && setT([]);
-      const X = setX([]);
-      const O = setO([]);
-      for (const facet of facets) {
-        const newFacet = [];
-        const M = d3.max(facet, (i) => V[i]);
-        const mainTicks = d3.ticks(0, M, ticks).slice(0, -1);
-        const secondaryTicks = d3.difference(
-          d3.ticks(0, M * 1.3, 2 * ticks),
-          mainTicks
-        );
-        for (const value of [...mainTicks, ...secondaryTicks]) {
-          newFacet.push(X.length);
-          T && T.push(textFormat(value));
-          O && O.push(mainTicks.includes(value) ? 1 : 0);
-          X.push(value / M);
-        }
-        newFacets.push(newFacet);
-      }
-      return { data: X, facets: newFacets };
-    };
-    return {
-      transform,
-      x: X,
-      ...(T && { key: T }),
-      ...(T && { text: T }),
-      ...(O && { fillOpacity: O, strokeOpacity: O }),
-      ...options
-    };
-  }
 }
 
