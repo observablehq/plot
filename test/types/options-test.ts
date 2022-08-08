54d70681b51cc9f58178c15482af0b033d375716
Philippe Rivière <fil@rezo.net>
2022-08-08 16:44:32 +0200

    ts tooling and tests

---
+++ test/types/options-test.ts
@@ -0,0 +1,241 @@
+import {expectError, expectType} from "tsd";
+import type {Data, Row, Value, ValueArray} from "../../src/data.js";
+import {Accessor, arrayify, ColorAccessor, map, TransformMethod} from "../../src/options.js";
+
+import {
+  constant,
+  field,
+  labelof,
+  maybeColorChannel,
+  maybeKeyword,
+  maybeNumberChannel,
+  percentile,
+  valueof
+} from "../../src/options.js";
+
+// valueof
+// _____________________________________________________________________
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const numberTransform: TransformMethod = {
+  transform: (data) => (data ? Array.from(data, (d) => (d ? +d + 1 : 0)) : [])
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const nullTransform: TransformMethod = {
+  transform: () => [1, 2, 3]
+};
+
+expectType<number[]>(valueof({length: 10}, (_, i) => i));
+expectType<ValueArray>(valueof([1, 2, 3], numberTransform));
+expectType<ValueArray>(valueof([1, 2, 3], nullTransform));
+expectType<ValueArray>(valueof({length: 10}, nullTransform));
+expectType<string[]>(valueof([{one: "one", two: "two"}], "one"));
+expectType<number[]>(valueof([{one: 1, two: 1}], "one"));
+expectType<Uint8Array>(valueof([{one: 1, two: 1}], "one", Uint8Array));
+expectType<number[]>(valueof(Float32Array.of(1, 2, 3), (d) => d));
+expectType<Date[]>(valueof({length: 10}, new Date()));
+expectType<undefined>(valueof(undefined, (_, i) => i + 10));
+expectType<undefined>(valueof(undefined, nullTransform));
+expectType<null>(valueof(null, nullTransform));
+expectType<number[]>(valueof({length: 10}, 1));
+expectType<boolean[]>(valueof({length: 10}, true));
+expectType<Uint8Array>(valueof({length: 10}, true, Uint8Array));
+valueof([{one: "one", two: "two"}], (d) => {
+  expectType<{one: string; two: string}>(d);
+  return d.one;
+});
+valueof([true, false], (d) => {
+  expectType<boolean>(d);
+  return d;
+});
+expectType<Float32Array>(valueof([1, 2, 3], (d) => d * 2, Float32Array));
+expectType<Float64Array>(valueof([1, 2, 3], (d) => d * 2, Float64Array));
+expectType<ValueArray>(valueof([1, 2, 3], (d) => d * 2, Array));
+expectType<string[]>(valueof({length: 10}, (_, i) => `My String #${i}`));
+
+// @ts-expect-error: red is not one of the keys
+valueof([{one: "one", two: "two"}], "red");
+// @ts-expect-error: red is not one of the keys
+valueof(null, "red");
+
+// field
+// _____________________________________________________________________
+
+expectType<Value>(field("key")({key: "value"}));
+expectType<Value>(field("foo")({key: "value"}));
+
+// identity
+// _____________________________________________________________________
+
+// constant
+// _____________________________________________________________________
+
+expectType<"foo">(constant("foo")());
+expectType<1>(constant(1)());
+expectType<true>(constant(true)());
+expectType<false>(constant(false)());
+expectType<null>(constant(null)());
+
+// percentile
+// _____________________________________________________________________
+
+expectType<number | undefined>(percentile("p99")([1, 2, 3, 4, 5], (i) => i));
+
+// @ts-expect-error: the accessor function is required by percentile'e return type.
+expectError(percentile("p99")([1, 2, 3, 4, 5]));
+
+// maybeColorChannel
+// _____________________________________________________________________
+
+expectType<[Accessor<string> | undefined, string | null | undefined]>(maybeColorChannel("red"));
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+expectType<[ColorAccessor<any> | undefined, string | null | undefined]>(maybeColorChannel(1));
+
+// maybeNumberChannel
+// _____________________________________________________________________
+
+expectType<[Accessor<number, Value> | null | undefined, undefined] | [undefined, number | null]>(maybeNumberChannel(1));
+
+// maybeKeyword
+// _____________________________________________________________________
+
+expectType<undefined>(maybeKeyword(null, "foo", ["bar"]));
+expectType<undefined>(maybeKeyword(undefined, "foo", ["bar"]));
+expectType<string | undefined>(maybeKeyword("bar", "foo", ["bar"]));
+
+// keyword
+// _____________________________________________________________________
+
+// arrayify
+// _____________________________________________________________________
+
+expectType<null>(arrayify(null));
+expectType<undefined>(arrayify(undefined));
+expectType<null>(arrayify(null, Float32Array));
+expectType<undefined>(arrayify(undefined, Float64Array));
+expectType<number[]>(arrayify([1, 2, 3]));
+expectType<number[]>(arrayify([1, 2, 3], undefined));
+expectType<Float32Array>(arrayify([1, 2, 3], Float32Array));
+expectType<Float64Array>(arrayify(new Float64Array([1, 2, 3]), Float64Array));
+expectType<Float32Array>(arrayify(new Float64Array([1, 2, 3]), Float32Array));
+
+// map
+// _____________________________________________________________________
+
+expectType<string[]>(map({length: 10}, (_, i) => `Item #${i}`));
+expectType<string[]>(map({length: 10}, (_, i) => `Item #${i}`, Array));
+expectType<string[]>(map([1, 2, 3], (_, i) => `Item #${i}`));
+expectType<number[]>(map([1, 2, 3], (d) => d ** 2));
+expectType<number[]>(map([1, 2, 3], (d) => d ** 2));
+expectType<number[]>(map([1, 2, 3], (d) => d ** 2, undefined));
+expectType<Float32Array>(map([1, 2, 3], (d) => d ** 2, Float32Array));
+expectType<Float64Array>(map([1, 2, 3], (d) => d ** 2, Float64Array));
+expectType<Float64Array>(map([{one: 2}], (d) => d.one ** 2, Float64Array));
+expectType<Float64Array>(map([true, false], (d) => (d ? 1 : 0), Float64Array));
+expectType<Value[]>(map([{one: 2}] as Data<Row>, field("one")));
+expectType<Float64Array>(map([{one: 2}] as Data<Row>, field("one"), Float64Array));
+
+// slice
+// _____________________________________________________________________
+
+// maybeZero
+// _____________________________________________________________________
+
+// maybeTuple
+// _____________________________________________________________________
+
+// maybeZ
+// _____________________________________________________________________
+
+// range
+// _____________________________________________________________________
+
+// where
+// _____________________________________________________________________
+
+// take
+// _____________________________________________________________________
+
+// keyof
+// _____________________________________________________________________
+
+// maybeInput
+// _____________________________________________________________________
+
+// column
+// _____________________________________________________________________
+
+// maybeColumn
+// _____________________________________________________________________
+
+// labelof
+// _____________________________________________________________________
+
+expectType<string>(labelof("some label"));
+expectType<string>(labelof(undefined, "fallback"));
+expectType<string>(labelof(null, "fallback"));
+expectType<undefined>(labelof(undefined));
+expectType<undefined>(labelof(null));
+expectType<string>(labelof({label: "my label"}, "fallback"));
+expectType<string>(labelof({label: "my label"}));
+
+// mid
+// _____________________________________________________________________
+
+// maybeValue
+// _____________________________________________________________________
+
+// numberChannel
+// _____________________________________________________________________
+
+// isIterable
+// _____________________________________________________________________
+
+// isTextual
+// _____________________________________________________________________
+
+// isOrdinal
+// _____________________________________________________________________
+
+// isTemporalString
+// _____________________________________________________________________
+
+// isNumericString
+// _____________________________________________________________________
+
+// isNumeric
+// _____________________________________________________________________
+
+// isFirst
+// _____________________________________________________________________
+
+// isEvery
+// _____________________________________________________________________
+
+// isColor
+// _____________________________________________________________________
+
+// isNoneish
+// _____________________________________________________________________
+
+// isNone
+// _____________________________________________________________________
+
+// isRound
+// _____________________________________________________________________
+
+// maybeFrameAnchor
+// _____________________________________________________________________
+
+// order
+// _____________________________________________________________________
+
+// inherit
+// _____________________________________________________________________
+
+// Named
+// _____________________________________________________________________
+
+// maybeNamed
+// _____________________________________________________________________
