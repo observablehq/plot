5e370621c2c50aa52ac88df9fd1da657fb49bc08
Philippe Rivière <fil@rezo.net>
2022-08-10 11:55:26 +0200

    format commit (just to get CI to work)

--- test/plots/driving-animation.js
+++ test/plots/driving-animation.js
@@ -1,13 +1,18 @@
 import * as Plot from "@observablehq/plot";
 import * as d3 from "d3";
 
-export default async function() {
+export default async function () {
   const driving = await d3.csv("data/driving.csv", d3.autoType);
   const chart = Plot.plot({
     inset: 10,
     grid: true,
     marks: [
-      Plot.line(driving, {x: "miles", y: "gas", time: "year", timeFilter: "lte"})
+      Plot.line(driving, {
+        x: "miles",
+        y: "gas",
+        time: "year",
+        timeFilter: "lte"
+      })
     ]
   });
 
@@ -15,7 +20,9 @@ export default async function() {
   chart.snapshots = [2009.3];
 
   // animate on click
-  d3.select(chart).on("click", () => chart.paused ? chart.play() : chart.pause());
+  d3.select(chart).on("click", () =>
+    chart.paused ? chart.play() : chart.pause()
+  );
 
   return chart;
 }
