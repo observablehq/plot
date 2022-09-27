2305151f75612ea7c6e131e153bb2522f0e4d6f7
Philippe Rivière <fil@rezo.net>
2022-09-27 12:26:43 +0200

    autoHeight factors in the auto and manual margins. See https://observablehq.com/@observablehq/plot-band-scale-bug-1045
    fixes #1045

--- src/dimensions.js
+++ src/dimensions.js
@@ -9,7 +9,6 @@ export function Dimensions(
   {
     projection,
     width = 640,
-    height = autoHeight(scales, geometry || isProjection(projection)),
     facet: {
       margin: facetMargin,
       marginTop: facetMarginTop = facetMargin !== undefined ? facetMargin : fxAxis === "top" ? 30 : 0,
@@ -17,19 +16,22 @@ export function Dimensions(
       marginBottom: facetMarginBottom = facetMargin !== undefined ? facetMargin : fxAxis === "bottom" ? 30 : 0,
       marginLeft: facetMarginLeft = facetMargin !== undefined ? facetMargin : fyAxis === "left" ? 40 : 0
     } = {},
+    marginTopAuto = Math.max((xAxis === "top" ? 30 : 0) + facetMarginTop, yAxis || fyAxis ? 20 : 0.5 - offset),
+    marginBottomAuto = Math.max((xAxis === "bottom" ? 30 : 0) + facetMarginBottom, yAxis || fyAxis ? 20 : 0.5 + offset),
     margin,
-    marginTop = margin !== undefined
-      ? margin
-      : Math.max((xAxis === "top" ? 30 : 0) + facetMarginTop, yAxis || fyAxis ? 20 : 0.5 - offset),
+    marginTop = margin !== undefined ? margin : marginTopAuto,
     marginRight = margin !== undefined
       ? margin
       : Math.max((yAxis === "right" ? 40 : 0) + facetMarginRight, xAxis || fxAxis ? 20 : 0.5 + offset),
-    marginBottom = margin !== undefined
-      ? margin
-      : Math.max((xAxis === "bottom" ? 30 : 0) + facetMarginBottom, yAxis || fyAxis ? 20 : 0.5 + offset),
+    marginBottom = margin !== undefined ? margin : marginBottomAuto,
     marginLeft = margin !== undefined
       ? margin
-      : Math.max((yAxis === "left" ? 40 : 0) + facetMarginLeft, xAxis || fxAxis ? 20 : 0.5 - offset)
+      : Math.max((yAxis === "left" ? 40 : 0) + facetMarginLeft, xAxis || fxAxis ? 20 : 0.5 - offset),
+    height = autoHeight(
+      scales,
+      geometry || isProjection(projection),
+      marginTop - marginTopAuto + marginBottom - marginBottomAuto
+    )
   } = {}
 ) {
   return {
@@ -46,8 +48,8 @@ export function Dimensions(
   };
 }
 
-function autoHeight({y, fy, fx}, geometry) {
+function autoHeight({y, fy, fx}, geometry, add) {
   const nfy = fy ? fy.scale.domain().length : 1;
   const ny = y ? (isOrdinalScale(y) ? y.scale.domain().length : Math.max(7, 17 / nfy)) : geometry ? 17 : 1;
-  return !!(y || fy || geometry) * Math.max(1, Math.min(60, ny * nfy)) * 20 + !!fx * 30 + 60;
+  return !!(y || fy || geometry) * Math.max(1, Math.min(60, ny * nfy)) * 20 + !!fx * 30 + 60 + Math.max(0, add);
 }
