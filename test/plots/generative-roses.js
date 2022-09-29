859d038c7d1b50364e07182ddca26d5d1a2f5707
Philippe Rivière <fil@rezo.net>
2022-09-29 16:28:29 +0200

    tests for select & facet reindex

---
+++ test/plots/generative-roses.js
@@ -0,0 +1,40 @@
+import * as Plot from "@observablehq/plot";
+import * as d3 from "d3";
+
+// Generate roses from a cumulative sum of vectors with various angles. The
+// twist is that each facet selects a subset of these angles to ignore (with
+// facet: "exclude").
+export default async function () {
+  const data = d3.range(0, 48, 0.7);
+  const mapped = Plot.mapY(
+    "cumsum",
+    Plot.mapX("cumsum", {
+      facet: "exclude",
+      x: Math.sin,
+      y: Math.cos
+    })
+  );
+  return Plot.plot({
+    facet: {
+      data,
+      x: (d, i) => (i % 8) % 3,
+      y: (d, i) => Math.floor((i % 8) / 3),
+      marginRight: 80
+    },
+    axis: null,
+    marks: [
+      Plot.line(data, {...mapped, curve: "natural"}),
+      ["First", "Last", "MaxX", "MinX", "MaxY", "MinY"].map((p) =>
+        Plot.dot(
+          data,
+          Plot[`select${p}`]({
+            ...mapped,
+            fill: () => p,
+            title: () => p,
+            r: 6
+          })
+        )
+      )
+    ]
+  });
+}
