10de98d775df9a487eff820dbab37e15b15c7c01
Philippe Rivière <fil@rezo.net>
2022-09-29 09:39:45 +0200

    since the sum (strict window) ignores the first 6 days, feels nicer to ignore them from the domain as well.

--- test/plots/seattle-precipitation-sum.js
+++ test/plots/seattle-precipitation-sum.js
@@ -6,6 +6,8 @@ export default async function () {
   const y = Plot.window({k: 7, strict: true, reduce: "sum", anchor: "end"});
   const text = Plot.window({k: 7, strict: true, reduce: (V) => Math.round(d3.sum(V)), anchor: "end"});
   return Plot.plot({
+    x: {domain: d3.extent(weather.slice(6), (d) => d.date)},
+    y: {insetTop: 10},
     marks: [
       Plot.rectY(weather, Plot.map({y}, {x: "date", y: "precipitation", interval: d3.utcDay})),
       Plot.textY(
