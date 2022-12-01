3c6da701290da444fd2d2082f5c3ddca0836f473
Mike Bostock <mbostock@gmail.com>
2022-12-01 09:51:52 -0800

    clip projectionFitAntarctica

--- test/plots/projection-fit-antarctica.js
+++ test/plots/projection-fit-antarctica.js
@@ -4,7 +4,6 @@ import {feature} from "topojson-client";
 
 export default async function () {
   const world = await d3.json("data/countries-50m.json");
-  const land = feature(world, world.objects.land);
   const domain = feature(
     world,
     world.objects.countries.geometries.find((d) => d.properties.name === "Antarctica")
@@ -14,6 +13,6 @@ export default async function () {
     height: 600,
     inset: 30,
     projection: {type: "azimuthal-equidistant", rotate: [0, 90], domain},
-    marks: [Plot.geo(land, {fill: "#666"}), Plot.graticule(), Plot.geo(domain, {fill: "black"}), Plot.frame()]
+    marks: [Plot.graticule({clip: "frame"}), Plot.geo(domain, {clip: "frame", fill: "currentColor"}), Plot.frame({})]
   });
 }
