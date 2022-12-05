88dadd4b8e18e9c4899922559264029f2cfd1993
Philippe Rivière <fil@rezo.net>
2022-12-05 21:51:40 +0100

    Line handles the "sphere" curve to follow great circles on spherical projections.

--- src/marks/line.js
+++ src/marks/line.js
@@ -26,18 +26,23 @@ const defaults = {
 export class Line extends Mark {
   constructor(data, options = {}) {
     const {x, y, z, curve, tension} = options;
+
+    // When the line mark is used with a "sphere" curve and a projection
+    // (supposedly spherical), keep x and y as numbers.
+    const sphere = curve === "sphere";
     super(
       data,
       {
-        x: {value: x, scale: "x"},
-        y: {value: y, scale: "y"},
+        x: {value: x, scale: sphere ? undefined : "x"},
+        y: {value: y, scale: sphere ? undefined : "y"},
         z: {value: maybeZ(options), optional: true}
       },
       options,
       defaults
     );
     this.z = z;
-    this.curve = Curve(curve, tension);
+    if (sphere) this.sphere = true;
+    else this.curve = Curve(curve, tension);
     markers(this, options);
   }
   filter(index) {
@@ -45,6 +50,25 @@ export class Line extends Mark {
   }
   render(index, scales, channels, dimensions, context) {
     const {x: X, y: Y} = channels;
+
+    let shape;
+    if (this.sphere) {
+      const {projection} = context;
+      if (projection === undefined) throw new Error("the sphere curve requires a projection");
+      const path = geoPath(projection);
+      shape = (I) =>
+        path({
+          type: "LineString",
+          coordinates: Array.from(I, (i) => [+X[i], +Y[i]]).filter(([x, y]) => !isNaN(x + y))
+        });
+    } else {
+      shape = shapeLine()
+        .curve(this.curve)
+        .defined((i) => i >= 0)
+        .x((i) => X[i])
+        .y((i) => Y[i]);
+    }
+
     return create("svg:g", context)
       .call(applyIndirectStyles, this, scales, dimensions, context)
       .call(applyTransform, this, scales)
@@ -57,14 +81,7 @@ export class Line extends Mark {
           .call(applyDirectStyles, this)
           .call(applyGroupedChannelStyles, this, channels)
           .call(applyGroupedMarkers, this, channels)
-          .attr(
-            "d",
-            shapeLine()
-              .curve(this.curve)
-              .defined((i) => i >= 0)
-              .x((i) => X[i])
-              .y((i) => Y[i])
-          )
+          .attr("d", shape)
       )
       .node();
   }
@@ -72,11 +89,9 @@ export class Line extends Mark {
 
 /** @jsdoc line */
 export function line(data, options = {}) {
-  let {x, y, curve, ...remainingOptions} = options;
+  let {x, y, ...remainingOptions} = options;
   [x, y] = maybeTuple(x, y);
-  return curve === "geodesic"
-    ? new LineGeodesic(data, {...remainingOptions, x, y})
-    : new Line(data, {...remainingOptions, curve, x, y});
+  return new Line(data, {...remainingOptions, x, y});
 }
 
 /** @jsdoc lineX */
@@ -89,51 +104,4 @@ export function lineX(data, options = {}) {
 export function lineY(data, options = {}) {
   const {x = indexOf, y = identity, ...remainingOptions} = options;
   return new Line(data, maybeDenseIntervalX({...remainingOptions, x, y}));
-}
-
-export class LineGeodesic extends Mark {
-  constructor(data, options = {}) {
-    const {x, y, z} = options;
-    super(
-      data,
-      {
-        x: {value: x},
-        y: {value: y},
-        z: {value: maybeZ(options), optional: true}
-      },
-      options,
-      defaults
-    );
-    this.z = z;
-    markers(this, options);
-  }
-  filter(index) {
-    return index;
-  }
-  render(index, scales, channels, dimensions, context) {
-    const {x: X, y: Y} = channels;
-    const {projection} = context;
-    if (projection === undefined) throw new Error("A projection is needed for geodesic curves");
-    const path = geoPath(projection);
-    return create("svg:g", context)
-      .call(applyIndirectStyles, this, scales, dimensions, context)
-      .call(applyTransform, this, scales)
-      .call((g) =>
-        g
-          .selectAll()
-          .data(groupIndex(index, [X, Y], this, channels))
-          .enter()
-          .append("path")
-          .call(applyDirectStyles, this)
-          .call(applyGroupedChannelStyles, this, channels)
-          .call(applyGroupedMarkers, this, channels)
-          .attr("d", (I) =>
-            path({
-              type: "LineString",
-              coordinates: Array.from(I, (i) => [+X[i], +Y[i]]).filter(([x, y]) => !isNaN(x + y))
-            })
-          )
-      )
-      .node();
-  }
 }
