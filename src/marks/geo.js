6d4e17632ec6192f51eeb4fbadd363e91e34a566
Philippe Rivière <fil@rezo.net>
2022-12-05 14:41:22 +0100

    the dot or geo mark is empty if given a constant negative r
    0 is accepted, as it doesn't result in an error for dots, and may be useful for geo marks.
    
    fixes #1145

--- src/marks/geo.js
+++ src/marks/geo.js
@@ -1,6 +1,6 @@
 import {geoGraticule10, geoPath} from "d3";
 import {create} from "../context.js";
-import {positive} from "../defined.js";
+import {negative, positive} from "../defined.js";
 import {identity, maybeNumberChannel} from "../options.js";
 import {Mark} from "../plot.js";
 import {applyChannelStyles, applyDirectStyles, applyIndirectStyles, applyTransform} from "../style.js";
@@ -34,7 +34,8 @@ export class Geo extends Mark {
     const {geometry: G, r: R} = channels;
     const path = geoPath(context.projection);
     const {r} = this;
-    if (r !== undefined) path.pointRadius(r);
+    if (negative(r)) index = [];
+    else if (r !== undefined) path.pointRadius(r);
     return create("svg:g", context)
       .call(applyIndirectStyles, this, scales, dimensions, context)
       .call(applyTransform, this, scales)
