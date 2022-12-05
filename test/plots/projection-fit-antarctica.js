228c5f5e39c75eece115ebdad919a15383e6aa7e
Philippe Rivière <fil@rezo.net>
2022-12-05 11:48:13 +0100

    projection.domain accepts an iterable
    closes #1148

--- test/plots/projection-fit-antarctica.js
+++ test/plots/projection-fit-antarctica.js
@@ -4,7 +4,7 @@ import {feature} from "topojson-client";
 
 export default async function () {
   const world = await d3.json("data/countries-50m.json");
-  const domain = feature(
+  const ata = feature(
     world,
     world.objects.countries.geometries.find((d) => d.properties.name === "Antarctica")
   );
@@ -12,7 +12,7 @@ export default async function () {
     width: 600,
     height: 600,
     inset: 30,
-    projection: {type: "azimuthal-equidistant", rotate: [0, 90], domain},
-    marks: [Plot.graticule({clip: "frame"}), Plot.geo(domain, {clip: "frame", fill: "currentColor"}), Plot.frame({})]
+    projection: {type: "azimuthal-equidistant", rotate: [0, 90], domain: [ata]},
+    marks: [Plot.graticule({clip: "frame"}), Plot.geo(ata, {clip: "frame", fill: "currentColor"}), Plot.frame({})]
   });
 }
