03a84b89098029c31e11be01a656c56409770930
Philippe Rivière <fil@rezo.net>
2022-12-20 19:27:05 +0100

    geoCentroid

--- test/plots/us-presidential-election-map-2020.js
+++ test/plots/us-presidential-election-map-2020.js
@@ -7,23 +7,25 @@ export default async function () {
     d3.json("data/us-counties-10m.json").then((us) => [feature(us, us.objects.counties), mesh(us, us.objects.states)]),
     d3.csv("data/us-presidential-election-2020.csv")
   ]);
-  const centroids = new Map(counties.features.map((d) => [d.id, d3.geoCentroid(d)]));
+  const geom = new Map(counties.features.map((d) => [d.id, d]));
   return Plot.plot({
     width: 960,
     height: 600,
     projection: "albers-usa",
     marks: [
       Plot.geo(statemesh),
-      Plot.vector(elections, {
-        filter: (d) => d.votes > 0,
-        anchor: "start",
-        x: (d) => centroids.get(d.fips)?.[0],
-        y: (d) => centroids.get(d.fips)?.[1],
-        sort: (d) => Math.abs(+d.results_trumpd - +d.results_bidenj),
-        stroke: (d) => (+d.results_trumpd > +d.results_bidenj ? "red" : "blue"),
-        length: (d) => Math.sqrt(Math.abs(+d.margin2020 * +d.votes)),
-        rotate: (d) => (+d.results_bidenj < +d.results_trumpd ? 60 : -60)
-      })
+      Plot.vector(
+        elections,
+        Plot.geoCentroid({
+          geometry: ({fips}) => geom.get(fips),
+          filter: (d) => d.votes > 0,
+          anchor: "start",
+          sort: (d) => Math.abs(+d.results_trumpd - +d.results_bidenj),
+          stroke: (d) => (+d.results_trumpd > +d.results_bidenj ? "red" : "blue"),
+          length: (d) => Math.sqrt(Math.abs(+d.margin2020 * +d.votes)),
+          rotate: (d) => (+d.results_bidenj < +d.results_trumpd ? 60 : -60)
+        })
+      )
     ]
   });
 }
