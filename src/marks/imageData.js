f213f1c11f585c4ddab48f2b4e00b423d0f607dc
Mike Bostock <mbostock@gmail.com>
2022-12-21 16:39:24 -0800

    fillOpacity

--- src/marks/imageData.js
+++ src/marks/imageData.js
@@ -12,12 +12,12 @@ const defaults = {
 // TODO
 // - an image data channel?
 // - verify that fill is width * height array? allow function?
-// - allow or disallow fill-opacity, opacity channels?
+// - allow opacity channels?
 // - faceting?
 // - optimize application of scale as RGB
 export class ImageData extends Mark {
   constructor(options = {}) {
-    let {x1, y1, x2, y2, width, height, offset = 0, stride = 1, fill, imageRendering = "pixelated"} = options;
+    let {x1, y1, x2, y2, width, height, offset = 0, stride = 1, imageRendering = "pixelated"} = options;
     if (x1 == null) throw new Error("missing x1");
     if (y1 == null) throw new Error("missing y1");
     if (x2 == null) throw new Error("missing x2");
@@ -28,8 +28,7 @@ export class ImageData extends Mark {
       null,
       {
         x: {value: [x1, x2], scale: "x"},
-        y: {value: [y1, y2], scale: "y"},
-        fill: {value: fill, scale: "color"}
+        y: {value: [y1, y2], scale: "y"}
       },
       options,
       defaults
@@ -41,27 +40,44 @@ export class ImageData extends Mark {
     this.imageRendering = string(imageRendering);
   }
   render(index, scales, channels, dimensions, context) {
-    const {x: X, y: Y, fill: F} = channels;
+    const {x: X, y: Y, fill: F, fillOpacity: FO} = channels;
     const [x1, x2] = X;
     const [y1, y2] = Y;
     const {document} = context;
-    const {width, height, offset, stride, imageRendering, fillOpacity} = this;
+    const {width, height, offset, stride, imageRendering} = this;
     const canvas = document.createElement("canvas");
     canvas.width = width;
     canvas.height = height;
     const context2d = canvas.getContext("2d");
-    if (fillOpacity != null) context2d.globalAlpha = fillOpacity;
     const image = context2d.createImageData(width, height);
     const imageData = image.data;
-    for (let y = 0, i = offset, j = 0; y < height; ++y) {
-      for (let x = 0; x < width; ++x, i += stride, j += 4) {
-        const f = F[i];
-        if (f == null) continue; // skip missing or invalid data
-        const {r, g, b} = rgb(f); // TODO hint to scale to generate rgb instead of string
-        imageData[j + 0] = r;
-        imageData[j + 1] = g;
-        imageData[j + 2] = b;
-        imageData[j + 3] = 255; // TODO fill opacity?
+    if (F) {
+      for (let y = 0, i = offset, j = 0; y < height; ++y) {
+        for (let x = 0; x < width; ++x, i += stride, j += 4) {
+          const f = F[i];
+          if (f == null) continue; // skip missing or invalid data
+          const {r, g, b} = rgb(f); // TODO hint to scale to generate rgb instead of string
+          imageData[j + 0] = r;
+          imageData[j + 1] = g;
+          imageData[j + 2] = b;
+        }
+      }
+    }
+    if (FO) {
+      for (let y = 0, i = offset, j = 0; y < height; ++y) {
+        for (let x = 0; x < width; ++x, i += stride, j += 4) {
+          const o = FO[i];
+          if (o == null) continue; // skip missing or invalid data
+          imageData[j + 3] = o * 255;
+        }
+      }
+    } else {
+      const {fillOpacity = 1} = this;
+      const o = fillOpacity * 255;
+      for (let y = 0, j = 0; y < height; ++y) {
+        for (let x = 0; x < width; ++x, j += 4) {
+          imageData[j + 3] = o;
+        }
       }
     }
     context2d.putImageData(image, 0, 0);
