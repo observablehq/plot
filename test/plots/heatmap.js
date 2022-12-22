ce258ba8ad88902261eb90c7c3f34941769539ae
Mike Bostock <mbostock@gmail.com>
2022-12-22 09:13:39 -0800

    fill as f(x, y)

--- test/plots/heatmap.js
+++ test/plots/heatmap.js
@@ -3,20 +3,10 @@ import * as Plot from "@observablehq/plot";
 export default async function () {
   const width = 580 >> 2;
   const height = 370 >> 2;
-  const scale = (4 * Math.PI) / width; // units per sample
+  const k = (4 * Math.PI) / width; // units per sample
   return Plot.plot({
     marks: [
-      Plot.imageData({
-        x2: width * scale,
-        y2: height * scale,
-        width,
-        height,
-        fill: Float64Array.from({length: width * height}, (_, i) => {
-          const x = (i % width) * scale;
-          const y = Math.floor(i / width) * scale;
-          return Math.sin(x) * Math.cos(y);
-        })
-      }),
+      Plot.imageData({x2: width * k, y2: height * k, width, height, fill: (x, y) => Math.sin(x) * Math.cos(y)}),
       Plot.frame()
     ]
   });
