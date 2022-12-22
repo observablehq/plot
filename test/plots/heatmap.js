1c295029b157d2d54afcbdacdeb30eadef933a91
Mike Bostock <mbostock@gmail.com>
2022-12-22 09:21:04 -0800

    PreTtiER

--- test/plots/heatmap.js
+++ test/plots/heatmap.js
@@ -6,7 +6,14 @@ export default async function () {
   const k = (4 * Math.PI) / width; // units per sample
   return Plot.plot({
     marks: [
-      Plot.imageData({imageRendering: "pixelated", x2: width * k, y2: height * k, width, height, fill: (x, y) => Math.sin(x) * Math.cos(y)}),
+      Plot.imageData({
+        imageRendering: "pixelated",
+        x2: width * k,
+        y2: height * k,
+        width,
+        height,
+        fill: (x, y) => Math.sin(x) * Math.cos(y)
+      }),
       Plot.frame()
     ]
   });
