e36bf48fda409b016160e3a0028425f7a0080e92
Mike Bostock <mbostock@gmail.com>
2022-12-23 15:12:19 -0800

    deferred channels

--- src/marks/axis.js
+++ src/marks/axis.js
@@ -1,6 +1,7 @@
 import {inferFontVariant} from "../axes.js";
-import {range} from "../options.js";
+import {range, valueof} from "../options.js";
 import {marks} from "../plot.js";
+import {position, registry as scaleRegistry} from "../scales/index.js";
 import {initializer} from "../transforms/basic.js";
 import {ruleX, ruleY} from "./rule.js";
 import {textX, textY} from "./text.js";
@@ -18,26 +19,27 @@ export function axisY({
   return marks(
     ruleY(
       [],
-      initializer({stroke, strokeOpacity, ...options}, function (data, facets, channels, {y}, dimensions) {
-        const Y = y.ticks(ticks);
+      initializer({stroke, strokeOpacity, ...options}, function (data, facets, channels, scales, dimensions) {
+        const {y} = scales;
+        data = y.ticks(ticks);
+        facets = [range(data)];
         this.insetLeft = -6;
         this.insetRight = dimensions.width - dimensions.marginLeft - dimensions.marginRight;
-        return {data: Y, facets: [range(Y)], channels: {y: {value: Y.map(y)}}};
+        return {data, facets, channels: {y: {value: data.map(y)}, ...rechannel(this, data)}};
       })
     ),
     textY(
       [],
-      initializer({fill, fillOpacity, ...options}, function (data, facets, channels, {y}) {
-        const Y = y.ticks(ticks);
+      initializer({fill, fillOpacity, ...options}, function (data, facets, channels, scales) {
+        const {y} = scales;
+        data = y.ticks(ticks);
+        facets = [range(data)];
         this.fontVariant = inferFontVariant(y);
         this.frameAnchor = "left";
         this.textAnchor = "end";
+        this.channels.text.value = y.tickFormat(ticks);
         this.dx = -9;
-        return {
-          data: Y,
-          facets: [range(Y)],
-          channels: {y: {value: Y.map(y)}, text: {value: Y.map(y.tickFormat(ticks))}}
-        };
+        return {data, facets, channels: {y: {value: data.map(y)}, ...rechannel(this, data)}};
       })
     )
   );
@@ -56,27 +58,36 @@ export function axisX({
   return marks(
     ruleX(
       [],
-      initializer({stroke, strokeOpacity, ...options}, function (data, facets, channels, {x}, dimensions) {
-        const X = x.ticks(ticks);
+      initializer({stroke, strokeOpacity, ...options}, function (data, facets, channels, scales, dimensions) {
+        const {x} = scales;
+        data = x.ticks(ticks);
+        facets = [range(data)];
         this.insetBottom = -6;
         this.insetTop = dimensions.height - dimensions.marginTop - dimensions.marginBottom;
-        return {data: X, facets: [range(X)], channels: {x: {value: X.map(x)}}};
+        return {data, facets, channels: {x: {value: data.map(x)}, ...rechannel(this, data)}};
       })
     ),
     textX(
       [],
-      initializer({fill, fillOpacity, ...options}, function (data, facets, channels, {x}) {
-        const X = x.ticks(ticks);
+      initializer({fill, fillOpacity, ...options}, function (data, facets, channels, scales) {
+        const {x} = scales;
+        data = x.ticks(ticks);
+        facets = [range(data)];
         this.fontVariant = inferFontVariant(x);
         this.frameAnchor = "bottom";
         this.lineAnchor = "top";
         this.dy = 9;
-        return {
-          data: X,
-          facets: [range(X)],
-          channels: {x: {value: X.map(x)}, text: {value: X.map(x.tickFormat(ticks))}}
-        };
+        this.channels.text.value = x.tickFormat(ticks);
+        return {data, facets, channels: {x: {value: data.map(x)}, ...rechannel(this, data)}};
       })
     )
+  );
+}
+
+function rechannel(mark, data) {
+  return Object.fromEntries(
+    Object.entries(mark.channels)
+      .filter(([, {scale}]) => scaleRegistry.get(scale) !== position)
+      .map(([name, channel]) => [name, {...channel, value: valueof(data, channel.value)}])
   );
 }
