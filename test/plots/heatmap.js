47f11c58a7f39cf5c5f327499352969abf28fd0c
Mike Bostock <mbostock@gmail.com>
2022-12-23 10:30:21 -0800

    Goldstein–Price

--- test/plots/heatmap.js
+++ test/plots/heatmap.js
@@ -3,6 +3,9 @@ import * as d3 from "d3";
 
 export default async function () {
   return Plot.plot({
+    color: {
+      type: "diverging"
+    },
     marks: [
       Plot.raster({
         fill: (x, y) => x * y * Math.sin(x) * Math.cos(y),
@@ -28,6 +31,9 @@ export async function heatmapArray() {
   const x = (f => i => f.invert(i % width + 0.5))(d3.scaleLinear([x1, x2], [0, width])); // prettier-ignore
   const y = (f => i => f.invert(Math.floor(i / width) + 0.5))(d3.scaleLinear([y2, y1], [height, 0])); // prettier-ignore
   return Plot.plot({
+    color: {
+      type: "diverging"
+    },
     marks: [
       Plot.raster(d3.range(width * height), {
         fill: ((f) => (_, i) => f(x(i), y(i)))((x, y) => x * y * Math.sin(x) * Math.cos(y)), // prettier-ignore
@@ -40,6 +46,30 @@ export async function heatmapArray() {
         width,
         height
       }),
+      Plot.frame()
+    ]
+  });
+}
+
+export async function heatmapLog() {
+  return Plot.plot({
+    height: 630,
+    x: {ticks: 10, tickFormat: "+f"},
+    y: {ticks: 10, tickFormat: "+f"},
+    color: {type: "log", scheme: "magma"},
+    marks: [
+      Plot.raster({
+        fill: (x, y) =>
+          (1 + (x + y + 1) ** 2 * (19 - 14 * x + 3 * x ** 2 - 14 * y + 6 * x * y + 3 * y ** 2)) *
+          (30 + (2 * x - 3 * y) ** 2 * (18 - 32 * x + 12 * x * x + 48 * y - 36 * x * y + 27 * y ** 2)),
+        x1: -2,
+        y1: -2.5,
+        x2: 2,
+        y2: 1.5,
+        pixelRatio: 4
+      }),
+      Plot.ruleX([0], {strokeOpacity: 0.2}),
+      Plot.ruleY([0], {strokeOpacity: 0.2}),
       Plot.frame()
     ]
   });
