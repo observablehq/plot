83826bb0a59e74370d6e242548557d38a4e54a66
Mike Bostock <mbostock@gmail.com>
2022-12-22 18:52:03 -0800

    raster

--- test/plots/heatmap.js
+++ test/plots/heatmap.js
@@ -2,11 +2,19 @@ import * as Plot from "@observablehq/plot";
 
 export default async function () {
   const width = 580 >> 2;
-  const height = 370 >> 2;
+  const height = 350 >> 2;
   const k = (4 * Math.PI) / width; // units per sample
   return Plot.plot({
     marks: [
-      Plot.imageData({x2: width * k, y2: height * k, width, height, fill: (x, y) => Math.sin(x) * Math.cos(y)}),
+      Plot.raster({
+        fill: (x, y) => x * y * Math.sin(x) * Math.cos(y),
+        x1: 0,
+        y1: 0,
+        x2: width * k,
+        y2: height * k,
+        width,
+        height
+      }),
       Plot.frame()
     ]
   });
