c44c29d63f62dde76050e0dd1ce9078c5fd70b8a
Mike Bostock <mbostock@gmail.com>
2022-12-23 21:00:38 -0800

    DRY tick

--- src/marks/axis.js
+++ src/marks/axis.js
@@ -21,7 +21,6 @@ export function axisY({
   frameAnchor = "left",
   lineAnchor = "middle",
   textAnchor = "end",
-  ticks,
   tickSize = 6,
   tickPadding = 3,
   dx = 0,
@@ -34,7 +33,6 @@ export function axisY({
           stroke: grid === true ? stroke : grid,
           strokeOpacity: gridOpacity,
           strokeWidth,
-          ticks,
           dx,
           x1: x ?? null,
           x2: null,
@@ -42,62 +40,32 @@ export function axisY({
         })
       : null,
     tickSize !== 0 && !isNoneish(stroke)
-      ? vectorY(
-          [],
-          initializer(
-            {
-              stroke,
-              strokeOpacity,
-              strokeWidth,
-              frameAnchor,
-              x,
-              ...options,
-              dx: +dx - offset,
-              anchor: "start",
-              length: tickSize,
-              shape: shapeTickY,
-              strokeLinejoin: null,
-              strokeLinecap: null
-            },
-            function (data, facets, channels, scales) {
-              const {x, y} = scales;
-              data = y.ticks(ticks);
-              facets = [range(data)];
-              return {
-                data,
-                facets,
-                channels: {
-                  y: {value: data.map(y)},
-                  ...(channels.x && {x: {value: map(valueof(data, this.channels.x.value), x)}}),
-                  ...rechannel(this, data)
-                }
-              };
-            }
-          )
-        )
+      ? tick(vectorY, "y", {
+          stroke,
+          strokeOpacity,
+          strokeWidth,
+          frameAnchor,
+          x,
+          ...options,
+          dx: +dx - offset,
+          anchor: "start",
+          length: tickSize,
+          shape: shapeTickY,
+          strokeLinejoin: null,
+          strokeLinecap: null
+        })
       : null,
     !isNoneish(fill)
-      ? textY(
-          [],
-          initializer(
-            {fill, fillOpacity, frameAnchor, lineAnchor, textAnchor, x, ...options, dx: +dx - tickSize - tickPadding},
-            function (data, facets, channels, scales) {
-              const {x, y} = scales;
-              data = y.ticks(ticks);
-              facets = [range(data)];
-              this.fontVariant = inferFontVariant(y);
-              this.channels.text.value = y.tickFormat(ticks);
-              return {
-                data,
-                facets,
-                channels: {
-                  y: {value: data.map(y)},
-                  ...(channels.x && {x: {value: map(valueof(data, this.channels.x.value), x)}}),
-                  ...rechannel(this, data)
-                }
-              };
-            }
-          )
+      ? tick(
+          textY,
+          "y",
+          {fill, fillOpacity, frameAnchor, lineAnchor, textAnchor, x, ...options, dx: +dx - tickSize - tickPadding},
+          function (scales) {
+            const {y} = scales;
+            const {ticks} = options;
+            this.fontVariant = inferFontVariant(y);
+            this.channels.text.value = y.tickFormat(ticks);
+          }
         )
       : null
   );
@@ -116,7 +84,6 @@ export function axisX({
   frameAnchor = "bottom",
   lineAnchor = "top",
   textAnchor = "middle",
-  ticks,
   tickSize = 6,
   tickPadding = 3,
   dy = 0,
@@ -129,7 +96,6 @@ export function axisX({
           stroke: grid === true ? stroke : grid,
           strokeOpacity: gridOpacity,
           strokeWidth,
-          ticks,
           dy,
           y1: null,
           y2: y ?? null,
@@ -137,62 +103,32 @@ export function axisX({
         })
       : null,
     tickSize !== 0 && !isNoneish(stroke)
-      ? vectorX(
-          [],
-          initializer(
-            {
-              stroke,
-              strokeOpacity,
-              strokeWidth,
-              frameAnchor,
-              y,
-              ...options,
-              dy: +dy - offset,
-              anchor: "start",
-              length: tickSize,
-              shape: shapeTickX,
-              strokeLinejoin: null,
-              strokeLinecap: null
-            },
-            function (data, facets, channels, scales) {
-              const {x, y} = scales;
-              data = x.ticks(ticks);
-              facets = [range(data)];
-              return {
-                data,
-                facets,
-                channels: {
-                  x: {value: data.map(x)},
-                  ...(channels.y && {y: {value: map(valueof(data, this.channels.y.value), y)}}),
-                  ...rechannel(this, data)
-                }
-              };
-            }
-          )
-        )
+      ? tick(vectorX, "x", {
+          stroke,
+          strokeOpacity,
+          strokeWidth,
+          frameAnchor,
+          y,
+          ...options,
+          dy: +dy - offset,
+          anchor: "start",
+          length: tickSize,
+          shape: shapeTickX,
+          strokeLinejoin: null,
+          strokeLinecap: null
+        })
       : null,
     !isNoneish(fill)
-      ? textX(
-          [],
-          initializer(
-            {fill, fillOpacity, frameAnchor, lineAnchor, textAnchor, y, ...options, dy: +dy + +tickSize + +tickPadding},
-            function (data, facets, channels, scales) {
-              const {x, y} = scales;
-              data = x.ticks(ticks);
-              facets = [range(data)];
-              this.fontVariant = inferFontVariant(x);
-              this.channels.text.value = x.tickFormat(ticks);
-              return {
-                data,
-                facets,
-                channels: {
-                  x: {value: data.map(x)},
-                  ...(channels.y && {y: {value: map(valueof(data, this.channels.y.value), y)}}),
-                  ...rechannel(this, data)
-                }
-              };
-            }
-          )
+      ? tick(
+          textX,
+          "x",
+          {fill, fillOpacity, frameAnchor, lineAnchor, textAnchor, y, ...options, dy: +dy + +tickSize + +tickPadding},
+          function (scales) {
+            const {x} = scales;
+            const {ticks} = options;
+            this.fontVariant = inferFontVariant(x);
+            this.channels.text.value = x.tickFormat(ticks);
+          }
         )
       : null
   );
@@ -204,79 +140,47 @@ export function gridY({
   stroke = color,
   strokeOpacity = opacity,
   strokeWidth = 1,
-  ticks,
   ...options
 } = {}) {
-  return ruleY(
-    [],
-    initializer(
-      {
-        stroke,
-        strokeOpacity,
-        strokeWidth,
-        ...options
-      },
-      function (data, facets, channels, scales) {
-        const {x, y} = scales;
-        data = y.ticks(ticks);
-        facets = [range(data)];
-        return {
-          data,
-          facets,
-          channels: {
-            y: {value: data.map(y)},
-            ...(channels.x1 && {x1: {value: map(valueof(data, this.channels.x1.value), x)}}),
-            ...(channels.x2 && {x2: {value: map(valueof(data, this.channels.x2.value), x)}}),
-            ...rechannel(this, data)
-          }
-        };
-      }
-    )
-  );
+  return tick(ruleY, "y", {stroke, strokeOpacity, strokeWidth, ...options});
 }
 
 export function gridX({
   color = "currentColor",
-  opacity = 0.11,
+  opacity = 0.1,
   stroke = color,
   strokeOpacity = opacity,
   strokeWidth = 1,
-  ticks,
   ...options
 } = {}) {
-  return ruleX(
-    [],
-    initializer(
-      {
-        stroke,
-        strokeOpacity,
-        strokeWidth,
-        ...options
-      },
-      function (data, facets, channels, scales) {
-        const {x, y} = scales;
-        data = x.ticks(ticks);
-        facets = [range(data)];
-        return {
-          data,
-          facets,
-          channels: {
-            x: {value: data.map(x)},
-            ...(channels.y1 && {y1: {value: map(valueof(data, this.channels.y1.value), y)}}),
-            ...(channels.y2 && {y2: {value: map(valueof(data, this.channels.y2.value), y)}}),
-            ...rechannel(this, data)
-          }
-        };
-      }
-    )
-  );
+  return tick(ruleX, "x", {stroke, strokeOpacity, strokeWidth, ...options});
 }
 
-function rechannel(mark, data) {
-  return Object.fromEntries(
-    Object.entries(mark.channels)
-      .filter(([, {scale}]) => scaleRegistry.get(scale) !== position)
-      .map(([name, channel]) => [name, {...channel, value: valueof(data, channel.value)}])
+function tick(mark, k, options, initialize) {
+  return mark(
+    [],
+    initializer(options, function (data, facets, channels, scales) {
+      initialize?.call(this, scales);
+      const {[k]: scale} = scales;
+      const {ticks} = options;
+      data = scale.ticks(ticks);
+      facets = [range(data)];
+      return {
+        data,
+        facets,
+        channels: Object.fromEntries(
+          Object.entries(this.channels).map(([name, channel]) => {
+            const {scale} = channel;
+            channel = {...channel, value: valueof(data, channel.value)};
+            if (scaleRegistry.get(scale) === position) {
+              channel.scale = null;
+              channel.value = map(channel.value, scales[scale]);
+            }
+            return [name, channel];
+          })
+        )
+      };
+    })
   );
 }
 
