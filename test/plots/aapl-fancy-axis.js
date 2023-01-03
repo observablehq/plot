d4915e44ce6d67aa465be4b7618cfee44f7d9c53
Mike Bostock <mbostock@gmail.com>
2023-01-02 17:27:59 -0800

    text stroke options

--- test/plots/aapl-fancy-axis.js
+++ test/plots/aapl-fancy-axis.js
@@ -5,9 +5,9 @@ export default async function () {
   const AAPL = await d3.csv("data/aapl.csv", d3.autoType);
   return Plot.plot({
     marks: [
-      Plot.axisY({grid: true, x: (y) => AAPL.find((d) => d.Close >= y)?.Date, insetLeft: -6}),
       Plot.ruleY([0]),
-      Plot.line(AAPL, {x: "Date", y: "Close"})
+      Plot.line(AAPL, {x: "Date", y: "Close"}),
+      Plot.axisY({grid: true, x: (y) => AAPL.find((d) => d.Close >= y)?.Date, insetLeft: -6, textStroke: "white"})
     ]
   });
 }
