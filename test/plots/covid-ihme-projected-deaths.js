8f21d242c4f9990ade977d8c36afadb354af1882
Mike Bostock <mbostock@gmail.com>
2023-01-04 10:31:15 -0800

    fancy multi-line time ticks

--- test/plots/covid-ihme-projected-deaths.js
+++ test/plots/covid-ihme-projected-deaths.js
@@ -14,6 +14,13 @@ export default async function () {
       tickFormat: ",~f"
     },
     marks: [
+      Plot.axisX({
+        grid: true,
+        tickFormat: (
+          (fm, fd) => (x, i) =>
+            i === 0 || d3.utcDay.count(d3.utcMonth(x), x) < 7 ? `${fd(x)}\n${fm(x)}` : fd(x)
+        )(d3.utcFormat("%B"), d3.utcFormat("%d"))
+      }),
       Plot.areaY(data, {
         x: "date",
         y1: (d) => Math.max(1, d.lower), // avoid zero
