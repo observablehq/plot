8a66f3ae059e4cd7e48017e8228ac65b7a5e8e15
Mike Bostock <mbostock@gmail.com>
2023-03-09 09:58:29 -0800

    more better transform types

--- src/transforms/basic.d.ts
+++ src/transforms/basic.d.ts
@@ -1,21 +1,13 @@
 import type {MarkOptions, MarkTransform} from "../mark.js";
 
-export interface TransformOptions {
-  filter?: MarkOptions["filter"];
-  reverse?: MarkOptions["reverse"];
-  sort?: MarkOptions["sort"];
-  transform?: MarkOptions["transform"];
-  initializer?: MarkOptions["initializer"];
-}
+export function transform<T extends MarkOptions>(options: T, transform: MarkTransform): T;
 
-export function transform<T extends TransformOptions>(options: T, transform: MarkTransform): T;
+export function initializer<T extends MarkOptions>(options: T, initializer: any): T;
 
-export function initializer<T extends TransformOptions>(options: T, initializer: any): T;
+export function filter<T extends MarkOptions>(test: (d: any, i: number) => boolean, options: T): T;
 
-export function filter<T extends TransformOptions>(test: (d: any, i: number) => boolean, options: T): T;
+export function reverse<T extends MarkOptions>(options: T): T;
 
-export function reverse<T extends TransformOptions>(options: T): T;
+export function shuffle<T extends MarkOptions>(options: T): T;
 
-export function shuffle<T extends TransformOptions>(options: T): T;
-
-export function sort<T extends TransformOptions>(order: MarkOptions["sort"], options: T): T;
+export function sort<T extends MarkOptions>(order: MarkOptions["sort"], options: T): T;
--- src/transforms/group.d.ts
+++ src/transforms/group.d.ts
@@ -1,351 +1,50 @@
-// TODO
+import type {ChannelValueSpec} from "../channel.js";
 
-export function groupZ(
-  outputs: any,
-  options: any
-):
-  | {
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      x: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      x1: any;
-      x2: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y1: any;
-      y2: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y: any;
-      x: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y1: any;
-      y2: any;
-      x: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y: any;
-      x1: any;
-      x2: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y1: any;
-      y2: any;
-      x1: any;
-      x2: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    };
+export type ReducerName =
+  | "first"
+  | "last"
+  | "count"
+  | "distinct"
+  | "sum"
+  | "proportion"
+  | "proportion-facet"
+  | "deviation"
+  | "min"
+  | "min-index"
+  | "max"
+  | "max-index"
+  | "mean"
+  | "median"
+  | "variance"
+  | "mode"
+  | `p${number}${number}` // percentile
+  | "x"
+  | "x1"
+  | "x2"
+  | "y"
+  | "y1"
+  | "y2";
 
-export function groupX(
-  outputs?: {
-    y: string;
-  },
-  options?: {}
-):
-  | {
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      x: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      x1: any;
-      x2: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y1: any;
-      y2: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y: any;
-      x: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y1: any;
-      y2: any;
-      x: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y: any;
-      x1: any;
-      x2: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y1: any;
-      y2: any;
-      x1: any;
-      x2: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    };
+export type ReducerFunction = (values: any[], extent: [min: any, max: any]) => any;
 
-export function groupY(
-  outputs?: {
-    x: string;
-  },
-  options?: {}
-):
-  | {
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      x: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      x1: any;
-      x2: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y1: any;
-      y2: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y: any;
-      x: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y1: any;
-      y2: any;
-      x: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y: any;
-      x1: any;
-      x2: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y1: any;
-      y2: any;
-      x1: any;
-      x2: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    };
+export interface ReducerImplementation {
+  reduce(index: number[], values: any[], extent: [min: any, max: any]): any;
+}
 
-export function group(
-  outputs?: {
-    fill: string;
-  },
-  options?: {}
-):
-  | {
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      x: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      x1: any;
-      x2: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y1: any;
-      y2: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y: any;
-      x: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y1: any;
-      y2: any;
-      x: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y: any;
-      x1: any;
-      x2: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    }
-  | {
-      y1: any;
-      y2: any;
-      x1: any;
-      x2: any;
-      transform: any;
-      sort?: any;
-      stroke?: any;
-      fill?: any;
-      z?: any;
-    };
+export type Reducer = ReducerName | ReducerFunction | ReducerImplementation;
+
+export type Reducers = {
+  [key: string]: Reducer;
+};
+
+export type Outputs<T> = {
+  [key in keyof T]: ChannelValueSpec;
+};
+
+export function groupZ<A, B extends Reducers>(outputs: B, options: A): A & Outputs<B>;
+
+export function groupX<A, B extends Reducers>(outputs: B, options: A): A & Outputs<B>;
+
+export function groupY<A, B extends Reducers>(outputs: B, options: A): A & Outputs<B>;
+
+export function group<A, B extends Reducers>(outputs: B, options: A): A & Outputs<B>;
