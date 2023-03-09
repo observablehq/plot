cb21e4ea1141ba5b6fdb29524f5a35c203a0c62b
Philippe Rivière <fil@rezo.net>
2023-03-09 08:30:13 +0100

    the quantile scale supports the unknown option (#1321)

---
+++ test/plots/penguin-quantile-unknown.js
@@ -0,0 +1,12 @@
+import * as Plot from "@observablehq/plot";
+import * as d3 from "d3";
+
+export async function penguinQuantileUnknown() {
+  const sample = (await d3.csv("data/penguins.csv", d3.autoType)).map((d, i) => ({
+    ...d,
+    body_mass_g: i % 7 === 0 ? NaN : d.body_mass_g
+  }));
+  return Plot.tickX(sample, {x: "culmen_length_mm", stroke: "body_mass_g"}).plot({
+    color: {type: "quantile", n: 5, scheme: "blues", unknown: "red", legend: true}
+  });
+}
