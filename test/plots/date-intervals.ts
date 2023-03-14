0a57694c7ed37a7ec93c607ae8a12825fe191907
Philippe Rivière <fil@rezo.net>
2023-03-14 20:14:39 +0100

    as const 🪄

--- test/plots/date-intervals.ts
+++ test/plots/date-intervals.ts
@@ -6,12 +6,14 @@ export async function intervalWeekMonth() {
   return Plot.plot({
     marginLeft: 80,
     color: {type: "linear", scheme: "blues"},
-    marks: [
-      {label: "month", interval: "month" as Plot.TimeIntervalName},
-      {label: "d3.utcMonth", interval: d3.utcMonth as Plot.IntervalImplementation<Date>},
-      {label: "week", interval: "week" as Plot.TimeIntervalName},
-      {label: "d3.utcWeek", interval: d3.utcWeek as Plot.IntervalImplementation<Date>}
-    ].map(({label, interval}: {label: string; interval: Plot.Interval<Date>}) =>
+    marks: (
+      [
+        {label: "month", interval: "month"},
+        {label: "d3.utcMonth", interval: d3.utcMonth},
+        {label: "week", interval: "week"},
+        {label: "d3.utcWeek", interval: d3.utcWeek}
+      ] as const
+    ).map(({label, interval}) =>
       Plot.barX(data, {
         x: "date",
         filter: (d) => d.industry === "Construction",
