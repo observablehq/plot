4b42726ae720e4a86f1d914bb52a04d5ce7965aa
Mike Bostock <mbostock@gmail.com>
2023-03-15 14:51:11 -0700

    more documentation

--- src/context.d.ts
+++ src/context.d.ts
@@ -1,6 +1,17 @@
 import type {GeoStreamWrapper} from "d3";
 
+/**
+ * Additional rendering context provided to marks and initializers.
+ */
 export interface Context {
+  /**
+   * The current document. Defaults to window.document, but may be overridden
+   * via plot options as when rendering plots in a headless environment.
+   */
   document: Document;
+
+  /**
+   * The current projection, if any.
+   */
   projection?: GeoStreamWrapper;
 }
