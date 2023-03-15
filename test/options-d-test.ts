fbdaa5ee06c46177df4a6601553bf861f6ac23cf
Philippe Rivière <fil@rezo.net>
2023-03-15 08:59:30 +0100

    remove "valid input data" type tests

--- test/options-d-test.ts
+++ test/options-d-test.ts
@@ -1,13 +1,5 @@
 import {valueof} from "../src/options.js";
 
-// Valid input data
-valueof([1, 2, 3], (d) => d, Float32Array);
-valueof(["1", 2, 3], (d) => d);
-valueof(["1", 2, 3], (d) => d, Array);
-valueof(["1", 2, 3], (d) => d, Float64Array);
-valueof(["1", 2, 3], (d) => +d, Float32Array);
-valueof(new Set(["1", 2, 3]), (d) => +d, Float32Array);
-
 // A function is not a valid input data
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
