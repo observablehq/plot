7d7a23a556a3ffa4b9cf4697e8a1aeeee85b910a
Philippe Rivière <fil@rezo.net>
2023-03-17 12:14:24 +0100

    documentation for the tree mark and the treeLink, treeNode transforms

--- src/marks/tree.d.ts
+++ src/marks/tree.d.ts
@@ -6,10 +6,32 @@ import type {TextOptions} from "./text.js";
 
 // TODO tree channels, e.g., "node:name" | "node:path" | "node:internal"?
 export interface TreeOptions extends DotOptions, LinkOptions, TextOptions, TreeTransformOptions {
+  /**
+   * Should the node be represented by a dot. Defaults to true unless a **marker** is specified.
+   */
   dot?: boolean;
+  /**
+   * A **stroke** color to pass to the text mark, to limit label occlusion. Defaults to white.
+   */
   textStroke?: MarkOptions["stroke"];
 }
 
+/**
+ * Transforms a tabular dataset into a hierarchy according to the given **path** input channel, which is typically a slash-separated string; then executes a tree layout algorithm to compute **x** and **y** output channels; these channels can then be fed to other marks to construct a node-link diagram.
+ *
+ * The following options control how the tabular data is organized into a hierarchy:
+ *
+ * * **path** - a channel specifying each node’s hierarchy location; defaults to identity
+ * * **delimiter** - the path separator; defaults to forward slash (/)
+ *
+ * The following options control how the node-link diagram is laid out:
+ *
+ * * **treeLayout** - a tree layout algorithm; defaults to [d3.tree](https://github.com/d3/d3-hierarchy/blob/main/README.md#tree)
+ * * **treeAnchor** - a tree layout orientation, either *left* or *right*; defaults to *left*
+ * * **treeSort** - a node comparator, or null to preserve input order
+ * * **treeSeparation** - a node separation function, or null for uniform separation
+ *
+ */
 export function tree(data?: Data, options?: TreeOptions): CompoundMark;
 
 export function cluster(data?: Data, options?: TreeOptions): CompoundMark;
--- test/plots/greek-gods.ts
+++ test/plots/greek-gods.ts
@@ -18,3 +18,24 @@ Chaos Tartarus`
     marks: [Plot.tree(gods)]
   });
 }
+
+export async function greekGodsExplicit() {
+  const gods = `Chaos Gaia Mountains
+Chaos Gaia Pontus
+Chaos Gaia Uranus
+Chaos Eros
+Chaos Erebus
+Chaos Tartarus`.split("\n");
+  return Plot.plot({
+    axis: null,
+    insetLeft: 10,
+    insetTop: 20,
+    insetBottom: 20,
+    insetRight: 120,
+    marks: [
+      Plot.link(gods, Plot.treeLink({stroke: "node:internal", delimiter: " "})),
+      Plot.dot(gods, Plot.treeNode({fill: "node:internal", delimiter: " "})),
+      Plot.text(gods, Plot.treeNode({text: "node:name", stroke: "white", fill: "currentColor", dx: 6, delimiter: " "}))
+    ]
+  });
+}
