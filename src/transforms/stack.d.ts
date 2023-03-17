398f7cc33b1376464f638bac99956cf0c55946eb
Philippe Rivière <fil@rezo.net>
2023-03-17 15:55:11 +0100

    stackOptions

--- src/transforms/stack.d.ts
+++ src/transforms/stack.d.ts
@@ -21,9 +21,53 @@ export type StackOrder =
   | any[]; // explicit ordinal values
 
 export interface StackOptions {
+  /**
+   * After all values have been stacked from zero, an optional **offset** can be
+   * applied to translate or scale the stacks:
+   *
+   * - null (default) - a zero baseline
+   * - *normalize* (or *expand*) - rescale each stack to fill [0, 1]
+   * - *center* (or *silhouette*) - align the centers of all stacks
+   * - *wiggle* - translate stacks to minimize apparent movement
+   * - a function to be passed a nested index, and start, end, and *z* values
+   *
+   * If a given stack has zero total value, the *expand* offset will not adjust
+   * the stack’s position. Both the *center* and *wiggle* offsets ensure that
+   * the lowest element across stacks starts at zero for better default axes.
+   * The *wiggle* offset is recommended for streamgraphs, and if used, changes
+   * the default order to *inside-out*.
+   */
   offset?: StackOffset | null;
+
+  /**
+   * The order in which stacks are layered:
+   *
+   * - null (default) - input order
+   * - *value* - ascending value order (or descending with **reverse**)
+   * - *sum* - order series by their total value
+   * - *appearance* - order series by the position of their maximum value
+   * - *inside-out* - order the earliest-appearing series on the inside
+   *   (recommended for the *wiggle* offset)
+   * - a named field or function of data - order data by priority
+   * - an array enumerating all the *z* values in the desired order
+   *
+   * The **reverse** option reverses the effective order. For the *value* order,
+   * Plot.stackY uses the *y* value while Plot.stackX uses the *x* value. For
+   * the *appearance* order, Plot.stackY uses the *x* position of the maximum
+   * *y* value while Plot.stackX uses the *y* position of the maximum *x* value.
+   */
   order?: StackOrder | null;
+
+  /**
+   * If true, reverse the effective order of the stacks.
+   */
   reverse?: boolean;
+
+  /**
+   * The *z* channel defines the series of each value in the stack. Used when
+   * the *order* is sum, appearance, inside-out, or an explicit array of z
+   * values.
+   */
   z?: ChannelValue;
 }
 
