3366f06bee60052a9ff72e9e3b7198606f3d61b0
Philippe Rivière <fil@rezo.net>
2023-03-20 13:20:23 +0100

    document centroid

--- src/transforms/centroid.d.ts
+++ src/transforms/centroid.d.ts
@@ -5,6 +5,18 @@ export interface CentroidOptions {
   geometry?: ChannelValue;
 }
 
+/**
+ * The centroid initializer derives **x** and **y** channels representing the
+ * planar (projected) centroids for the given GeoJSON geometry. If the
+ * **geometry** option is not specified, the mark’s data is assumed to be
+ * GeoJSON objects. See also Plot.geoCentroid.
+ */
 export function centroid<T>(options?: T & CentroidOptions): Initialized<T>;
 
+/**
+ * The geoCentroid transform derives **x** and **y** channels representing the
+ * spherical centroids for the given GeoJSON geometry. If the **geometry**
+ * option is not specified, the mark’s data is assumed to be GeoJSON objects.
+ * See also Plot.centroid.
+ */
 export function geoCentroid<T>(options?: T & CentroidOptions): T & {x: ChannelTransform; y: ChannelTransform};
