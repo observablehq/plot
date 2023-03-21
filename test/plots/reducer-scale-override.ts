520ecff33d497c3d4d75c7a414e31af3c4d00af9
Mike Bostock <mbostock@gmail.com>
2023-03-21 11:35:34 -0700

    DRY test

--- test/plots/reducer-scale-override.ts
+++ test/plots/reducer-scale-override.ts
@@ -1,9 +1,8 @@
 import * as Plot from "@observablehq/plot";
 import * as d3 from "d3";
 
-export async function reducerScaleOverrideFunction() {
+async function reducerScaleOverride(reduce: Plot.Reducer) {
   const penguins = await d3.csv<any>("data/penguins.csv", d3.autoType);
-  const reduce: Plot.ReducerFunction = (values) => d3.mode(values);
   return Plot.barY(
     penguins,
     Plot.groupX(
@@ -11,28 +10,16 @@ export async function reducerScaleOverrideFunction() {
       {x: "species", fill: (d) => (d.island === "Biscoe" ? "orange" : "green"), fy: "sex"}
     )
   ).plot();
+}
+
+export async function reducerScaleOverrideFunction() {
+  return reducerScaleOverride((values) => d3.mode(values));
 }
 
 export async function reducerScaleOverrideImplementation() {
-  const penguins = await d3.csv<any>("data/penguins.csv", d3.autoType);
-  const reduce: Plot.ReducerImplementation = {reduce: (index, values) => d3.mode(index, (i) => values[i])};
-  return Plot.barY(
-    penguins,
-    Plot.groupX(
-      {y: "count", fill: {reduce, scale: true}},
-      {x: "species", fill: (d) => (d.island === "Biscoe" ? "orange" : "green"), fy: "sex"}
-    )
-  ).plot();
+  return reducerScaleOverride({reduce: (index, values) => d3.mode(index, (i) => values[i])});
 }
 
 export async function reducerScaleOverrideName() {
-  const penguins = await d3.csv<any>("data/penguins.csv", d3.autoType);
-  const reduce: Plot.ReducerName = "mode";
-  return Plot.barY(
-    penguins,
-    Plot.groupX(
-      {y: "count", fill: {reduce, scale: true}},
-      {x: "species", fill: (d) => (d.island === "Biscoe" ? "orange" : "green"), fy: "sex"}
-    )
-  ).plot();
+  return reducerScaleOverride("mode");
 }
