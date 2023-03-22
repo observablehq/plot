6759ce912a791389c6bbbfc8e786e07c651a0d8e
Philippe Rivière <fil@rezo.net>
2023-03-22 10:48:44 +0100

    document basic transforms, initializers, valueof…
    Removes the FilterFunction type

--- src/options.d.ts
+++ src/options.d.ts
@@ -1,8 +1,45 @@
 import type {ChannelTransform, ChannelValue} from "./channel.js";
 import type {Data} from "./mark.js";
 
+/**
+ * Given an iterable *data* and some *value* accessor (such as a field name),
+ * returns an array (a column) of the specified *type* with the corresponding
+ * value of each element of the data.
+ *
+ * If *type* is specified, it must be Array or a similar class that implements
+ * the
+ * [Array.from](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from)
+ * interface such as a typed array.
+ *
+ * Plot.valueof is not guaranteed to return a new array. When a transform method
+ * is used, or when the given *value* is an array that is compatible with the
+ * requested *type*, the array may be returned as-is without making a copy.
+ */
 export function valueof(data: Data | null, value: ChannelValue | null, type?: any): any[] | null;
 
+/**
+ * This helper for constructing derived columns returns a [*column*,
+ * *setColumn*] array. The *column* object implements *column*.transform,
+ * returning whatever value was most recently passed to *setColumn*. If
+ * *setColumn* is not called, then *column*.transform returns undefined. If a
+ * *source* is specified, then *column*.label exposes the given *source*’s
+ * label, if any: if *source* is a string as when representing a named field of
+ * data, then *column*.label is *source*; otherwise *column*.label propagates
+ * *source*.label. This allows derived columns to propagate a human-readable
+ * axis or legend label.
+ *
+ * Plot.column is typically used by options transforms to define new channels;
+ * the associated columns are populated (derived) when the **transform** option
+ * function is invoked. See Plot.**transform**.
+ */
 export function column(source?: any): [ChannelTransform, <T>(value: T) => T];
 
+/**
+ * This helper returns a source array as-is, avoiding an extra copy when
+ * defining a channel as being equal to the data:
+ *
+ * ```js
+ * Plot.raster(await readValues(), {width: 300, height: 200, fill: Plot.identity})
+ * ```
+ */
 export const identity: ChannelTransform;
--- src/transforms/basic.d.ts
+++ src/transforms/basic.d.ts
@@ -3,8 +3,22 @@ import type {Context} from "../context.js";
 import type {Dimensions} from "../dimensions.js";
 import type {ScaleFunctions} from "../scales.js";
 
+/**
+ * A data transform receives the data and an array of facets (indices into the
+ * data), and returns possibly new data and facets. Can be used in conjunction
+ * with Plot.**column** to affect channels.
+ */
 export type TransformFunction = (data: any[], facets: number[][]) => {data?: any[]; facets?: number[][]};
 
+/**
+ * Initializers can be used to transform and derive new channels prior to
+ * rendering. Unlike transforms which operate in abstract data space,
+ * initializers can operate in screen space such as pixel coordinates and
+ * colors. For example, initializers can modify a marks’ positions to avoid
+ * occlusion. Initializers are invoked *after* the initial scales are
+ * constructed and can modify the channels or derive new channels; these in turn
+ * may (or may not, as desired) be passed to scales.
+ */
 export type InitializerFunction = (
   data: any[],
   facets: number[][],
@@ -18,22 +32,60 @@ export type InitializerFunction = (
   channels?: Channels;
 };
 
-export type FilterFunction = (d: any, i: number) => boolean;
-
 export type CompareFunction = (a: any, b: any) => number;
 
 export type Transformed<T> = T & {transform: TransformFunction};
 
 export type Initialized<T> = T & {initializer: InitializerFunction};
 
+/**
+ * Given an *options* object that may specify some basic transforms (*filter*,
+ * *sort*, or *reverse*) or a custom *transform* function, composes those
+ * transforms if any with the given *transform* function, returning a new
+ * *options* object. If a custom *transform* function is present on the given
+ * *options*, any basic transforms are ignored. Any additional input *options*
+ * are passed through in the returned *options* object. This method facilitates
+ * applying the basic transforms prior to applying the given custom *transform*
+ * and is used internally by Plot’s built-in transforms.
+ */
 export function transform<T>(options: T, transform: TransformFunction): Transformed<T>;
 
+/**
+ * This helper composes the *initializer* function with any other transforms
+ * present in the *options*, and returns a new *options* object. The initializer
+ * function is called after the scales have been computed, and receives as
+ * inputs the (possibly transformed) array of *data*, the *facets* index of
+ * elements of this array that belong to each facet, the input *channels* (as an
+ * object of named channels), the *scales*, and the *dimensions*. The mark
+ * itself is the *this* context. The initializer function must return an object
+ * with *data*, *facets*, and new *channels*. Any new channels are merged with
+ * existing channels, replacing channels of the same name. If an initializer
+ * desires a channel that is not supported by the downstream mark, additional
+ * channels can be declared using the mark **channels** option.
+ */
 export function initializer<T>(options: T, initializer: InitializerFunction): Initialized<T>;
 
-export function filter<T>(test: FilterFunction, options?: T): Transformed<T>;
+/**
+ * Filters the data given the specified *test*. The test can be given as an
+ * accessor function (which receives the datum and index), or as a channel value
+ * definition such as a field name; truthy values are retained.
+ *
+ * ```js
+ * Plot.filter(d => d.body_mass_g > 3000, options) // show data whose body mass is greater than 3kg
+ * ```
+ */
+export function filter<T>(test: ChannelValue, options?: T): Transformed<T>;
 
+/**
+ * Reverses the order of the data. Often used in conjunction with Plot.**sort**.
+ */
 export function reverse<T>(options?: T): Transformed<T>;
 
+/**
+ * Shuffles the data randomly. If a *seed* option is specified, a linear
+ * congruential generator with the given seed is used to generate random numbers
+ * deterministically; otherwise, Math.random is used.
+ */
 export function shuffle<T>(options?: T): Transformed<T>;
 
 export interface SortOrderOptions {
@@ -44,4 +96,12 @@ export interface SortOrderOptions {
 
 export type SortOrder = CompareFunction | ChannelValue | SortOrderOptions;
 
+/**
+ * Sorts the data by the specified *order*, which can be an accessor function, a
+ * comparator function, or a channel value definition such as a field name.
+ *
+ * ```js
+ * Plot.sort("body_mass_g", options) // show data in ascending body mass order
+ * ```
+ */
 export function sort<T>(order: SortOrder, options?: T): Transformed<T>;
