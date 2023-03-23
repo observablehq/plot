579f9ae717de817889734877ae984733c2403fd0
Mike Bostock <mbostock@gmail.com>
2023-03-23 09:53:37 -0700

    edits

--- src/transforms/normalize.d.ts
+++ src/transforms/normalize.d.ts
@@ -14,24 +14,27 @@ export type NormalizeBasisName =
   | "extent"
   | ReducerPercentile;
 
-export type NormalizeBasisFunction = (index: number[], values: any[]) => any;
+export type NormalizeBasisFunction = (index: number[], values: any[]) => number;
 
+/** How to normalize series values. */
 export type NormalizeBasis = NormalizeBasisName | NormalizeBasisFunction;
 
+/** Options for the normalize transform. */
 export interface NormalizeOptions {
   basis?: NormalizeBasis;
 }
 
 /**
- * Normalize series values relative to the given basis. For example, if the
- * series values are [*x₀*, *x₁*, *x₂*, …] and the *first* basis is used, the
- * mapped series values would be [*x₀* / *x₀*, *x₁* / *x₀*, *x₂* / *x₀*, …] as
- * in an index chart. The **basis** option specifies how to normalize the series
- * values.
+ * Groups data into series using the first channel of *z*, *fill*, or *stroke*
+ * (if any), then derives new *x*, *x1*, and *x2* channels for each
+ * corresponding input channel by normalizing to the given *basis*. For example,
+ * if the series values are [*x₀*, *x₁*, *x₂*, …] and the *first* basis is used,
+ * the derived series values would be [*x₀* / *x₀*, *x₁* / *x₀*, *x₂* / *x₀*, …]
+ * as in an index chart.
  *
- * The following basis methods are supported:
+ * The **basis** option specifies how to normalize series values. It can be:
  *
- * * *first* - the first value, as in an index chart; the default
+ * * *first* (default) - the first value, as in an index chart
  * * *last* - the last value
  * * *min* - the minimum value
  * * *max* - the maximum value
@@ -40,22 +43,23 @@ export interface NormalizeOptions {
  * * *pXX* - the percentile value, where XX is a number in [00,99]
  * * *sum* - the sum of values
  * * *extent* - the minimum is mapped to zero, and the maximum to one
- * * *deviation* - each value is transformed by subtracting the mean and then dividing by the standard deviation
- * * a function to be passed an array of values, returning the desired basis
+ * * *deviation* - subtract the mean, then divide by the standard deviation
+ * * a function to be passed an array of series values, returning a number
  */
 export function normalizeX<T>(options?: T & NormalizeOptions): Transformed<T>;
 export function normalizeX<T>(basis?: NormalizeBasis, options?: T): Transformed<T>;
 
 /**
- * Normalize series values relative to the given basis. For example, if the
- * series values are [*y₀*, *y₁*, *y₂*, …] and the *first* basis is used, the
- * mapped series values would be [*y₀* / *y₀*, *y₁* / *y₀*, *y₂* / *y₀*, …] as
- * in an index chart. The **basis** option specifies how to normalize the series
- * values.
+ * Groups data into series using the first channel of *z*, *fill*, or *stroke*
+ * (if any), then derives new *y*, *y1*, and *y2* channels for each
+ * corresponding input channel by normalizing to the given *basis*. For example,
+ * if the series values are [*y₀*, *y₁*, *y₂*, …] and the *first* basis is used,
+ * the derived series values would be [*y₀* / *y₀*, *y₁* / *y₀*, *y₂* / *y₀*, …]
+ * as in an index chart.
  *
- * The following basis methods are supported:
+ * The **basis** option specifies how to normalize series values. It can be:
  *
- * * *first* - the first value, as in an index chart; the default
+ * * *first* (default) - the first value, as in an index chart
  * * *last* - the last value
  * * *min* - the minimum value
  * * *max* - the maximum value
@@ -64,18 +68,24 @@ export function normalizeX<T>(basis?: NormalizeBasis, options?: T): Transformed<
  * * *pXX* - the percentile value, where XX is a number in [00,99]
  * * *sum* - the sum of values
  * * *extent* - the minimum is mapped to zero, and the maximum to one
- * * *deviation* - each value is transformed by subtracting the mean and then dividing by the standard deviation
- * * a function to be passed an array of values, returning the desired basis
+ * * *deviation* - subtract the mean, then divide by the standard deviation
+ * * a function to be passed an array of series values, returning a number
  */
 export function normalizeY<T>(options?: T & NormalizeOptions): Transformed<T>;
 export function normalizeY<T>(basis?: NormalizeBasis, options?: T): Transformed<T>;
 
 /**
- * Returns a normalize map method for the given *basis*, suitable for use with Plot.map.
+ * Given a normalize *basis*, returns a corresponding map implementation for use
+ * with the map transform, allowing the normalization of arbitrary channels
+ * instead of only *x* and *y*. For example, to normalize the *title* channel:
  *
- * The following basis methods are supported:
+ * ```js
+ * Plot.map({title: Plot.normalize("first")}, {x: "Date", title: "Close", stroke: "Symbol"})
+ * ```
  *
- * * *first* - the first value, as in an index chart; the default
+ * The **basis** option specifies how to normalize series values. It can be:
+ *
+ * * *first* (default) - the first value, as in an index chart
  * * *last* - the last value
  * * *min* - the minimum value
  * * *max* - the maximum value
@@ -84,11 +94,7 @@ export function normalizeY<T>(basis?: NormalizeBasis, options?: T): Transformed<
  * * *pXX* - the percentile value, where XX is a number in [00,99]
  * * *sum* - the sum of values
  * * *extent* - the minimum is mapped to zero, and the maximum to one
- * * *deviation* - each value is transformed by subtracting the mean and then dividing by the standard deviation
- * * a function to be passed an array of values, returning the desired basis
- *
- * ```js
- * Plot.map({y: Plot.normalize("first")}, {x: "Date", y: "Close", stroke: "Symbol"})
- * ```
+ * * *deviation* - subtract the mean, then divide by the standard deviation
+ * * a function to be passed an array of series values, returning a number
  */
 export function normalize(basis: NormalizeBasis): Map;
