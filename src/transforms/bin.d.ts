86c2d7dd04a0c305f6a986380ba50bd6d70b1907
Mike Bostock <mbostock@gmail.com>
2023-03-24 08:09:22 -0700

    document group and bin

--- src/transforms/bin.d.ts
+++ src/transforms/bin.d.ts
@@ -10,9 +10,40 @@ export type ThresholdsFunction = (values: any[], min: any, max: any) => any[];
 export type Thresholds = ThresholdsName | ThresholdsFunction | RangeInterval;
 
 export interface BinOptions {
+  /**
+   * Whether the distribution is cumulative (use -1 for the [complementary
+   * cumulative](https://en.wikipedia.org/wiki/Cumulative_distribution_function#Complementary_cumulative_distribution_function_.28tail_distribution.29))
+   */
   cumulative?: boolean | number;
+  /**
+   * The domain, or a function that receives the values and returns the
+   * domain. Values outside the domain will be omitted.
+   */
   domain?: ((values: any[]) => [min: any, max: any]) | [min: any, max: any];
+  /**
+   * The **thresholds** value may be specified as:
+   *
+   * * *auto* (default) - Scott’s rule, capped at 200
+   * * *freedman-diaconis* - the [Freedman–Diaconis
+   *   rule](https://en.wikipedia.org/wiki/Freedman–Diaconis_rule)
+   * * *scott* - [Scott’s normal reference
+   *   rule](https://en.wikipedia.org/wiki/Histogram#Scott.27s_normal_reference_rule)
+   * * *sturges* - [Sturges’
+   *   formula](https://en.wikipedia.org/wiki/Histogram#Sturges.27_formula)
+   * * a count (hint) representing the desired number of bins
+   * * an array of *n* threshold values for *n* - 1 bins
+   * * an interval or time interval (see also **interval**)
+   * * a function that returns an array, count, or time interval
+   */
   thresholds?: Thresholds;
+  /**
+   * An alternative way of specifying the bins thresholds. It may be either an
+   * interval (object with a floor method), a time interval such as *day*, or a
+   * number. If a number *n*, threshold values are consecutive multiples of *n*
+   * that span the domain. When the thresholds are specified as an interval, and
+   * the default **domain** is used, the domain will automatically be extended
+   * to start and end to align with the interval.
+   */
   interval?: RangeInterval;
 }
 
@@ -35,17 +66,198 @@ export interface BinReducerImplementation {
 }
 
 export interface BinOutputOptions extends BinOptions {
+  /**
+   * The data reducer; defaults to the array of values that belong to the bin in
+   * input order.
+   */
   data?: BinReducer | null;
+  /**
+   * The filter reducer, defaults to a check on empty bins. Use null to return
+   * all bins, for example to impute sum=0 for a line chart.
+   */
   filter?: BinReducer | null;
+  /**
+   * The order in which the bins are generated, specified as an aggregation
+   * method (defaults to ascending).
+   */
   sort?: BinReducer | null;
+  /**
+   * Reverse the order in which the bins are generated.
+   */
   reverse?: boolean;
 }
 
 /** How to reduce binned channel values. */
 export type BinOutputs = ChannelReducers<BinReducer> & BinOutputOptions;
 
+/**
+ * Aggregates continuous data—quantitative or temporal values such as
+ * temperatures or times—into discrete bins and then computes summary statistics
+ * for each bin such as a count or sum. The binX transform is often used in
+ * conjunction with the rectY mark, to make histograms.
+ *
+ * ```
+ * Plot.rectY(penguins, Plot.binX({y: "count"}, {x: "culmen_length_mm"}))
+ * ```
+ *
+ * The following aggregation methods are supported:
+ *
+ * * *first* - the first value, in input order
+ * * *last* - the last value, in input order
+ * * *count* - the number of elements (frequency)
+ * * *distinct* - the number of distinct values
+ * * *sum* - the sum of values
+ * * *proportion* - the sum proportional to the overall total (weighted
+ *   frequency)
+ * * *proportion-facet* - the sum proportional to the facet total
+ * * *min* - the minimum value
+ * * *min-index* - the zero-based index of the minimum value
+ * * *max* - the maximum value
+ * * *max-index* - the zero-based index of the maximum value
+ * * *mean* - the mean value (average)
+ * * *median* - the median value
+ * * *mode* - the value with the most occurrences
+ * * *pXX* - the percentile value, where XX is a number in [00,99]
+ * * *deviation* - the standard deviation
+ * * *variance* - the variance per [Welford’s
+ *   algorithm](https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Welford's_online_algorithm)
+ * * *x* - the middle of the bin’s *x* extent (when binning on *x*)
+ * * *x1* - the lower bound of the bin’s *x* extent (when binning on *x*)
+ * * *x2* - the upper bound of the bin’s *x* extent (when binning on *x*)
+ * * *y* - the middle of the bin’s *y* extent (when binning on *y*)
+ * * *y1* - the lower bound of the bin’s *y* extent (when binning on *y*)
+ * * *y2* - the upper bound of the bin’s *y* extent (when binning on *y*)
+ * * a function to be passed the array of values for each bin and the extent of
+ *   the bin
+ * * an object with a *reduce* method, and optionally a *scope*
+ *
+ * Most aggregation methods require binding the output channel to an input
+ * channel; for example, if you want the **y** output channel to be a *sum* (not
+ * merely a count), there should be a corresponding **y** input channel
+ * specifying which values to sum. If there is not, *sum* will be equivalent to
+ * *count*.
+ *
+ * To control how *x* is divided into bins, the following options are supported:
+ *
+ * * **thresholds** - the threshold values; see below
+ * * **interval** - an alternative method of specifying thresholds
+ * * **domain** - values outside the domain will be omitted
+ * * **cumulative** - if positive, each bin will contain all lesser bins
+ */
 export function binX<T>(outputs?: BinOutputs, options?: T & BinOptions): Transformed<T>;
 
+/**
+ * Aggregates continuous data—quantitative or temporal values such as
+ * temperatures or times—into discrete bins and then computes summary statistics
+ * for each bin such as a count or sum. The binY transform is often used in
+ * conjunction with the rectX mark, to make vertical histograms.
+ *
+ * ```
+ * Plot.rectX(penguins, Plot.binY({x: "count"}, {y: "culmen_length_mm"}))
+ * ```
+ *
+ * The following aggregation methods are supported:
+ *
+ * * *first* - the first value, in input order
+ * * *last* - the last value, in input order
+ * * *count* - the number of elements (frequency)
+ * * *distinct* - the number of distinct values
+ * * *sum* - the sum of values
+ * * *proportion* - the sum proportional to the overall total (weighted
+ *   frequency)
+ * * *proportion-facet* - the sum proportional to the facet total
+ * * *min* - the minimum value
+ * * *min-index* - the zero-based index of the minimum value
+ * * *max* - the maximum value
+ * * *max-index* - the zero-based index of the maximum value
+ * * *mean* - the mean value (average)
+ * * *median* - the median value
+ * * *mode* - the value with the most occurrences
+ * * *pXX* - the percentile value, where XX is a number in [00,99]
+ * * *deviation* - the standard deviation
+ * * *variance* - the variance per [Welford’s
+ *   algorithm](https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Welford's_online_algorithm)
+ * * *x* - the middle of the bin’s *x* extent (when binning on *x*)
+ * * *x1* - the lower bound of the bin’s *x* extent (when binning on *x*)
+ * * *x2* - the upper bound of the bin’s *x* extent (when binning on *x*)
+ * * *y* - the middle of the bin’s *y* extent (when binning on *y*)
+ * * *y1* - the lower bound of the bin’s *y* extent (when binning on *y*)
+ * * *y2* - the upper bound of the bin’s *y* extent (when binning on *y*)
+ * * a function to be passed the array of values for each bin and the extent of
+ *   the bin
+ * * an object with a *reduce* method, and optionally a *scope*
+ *
+ * Most aggregation methods require binding the output channel to an input
+ * channel; for example, if you want the **y** output channel to be a *sum* (not
+ * merely a count), there should be a corresponding **y** input channel
+ * specifying which values to sum. If there is not, *sum* will be equivalent to
+ * *count*.
+ *
+ * To control how *y* is divided into bins, the following options are supported:
+ *
+ * * **thresholds** - the threshold values; see below
+ * * **interval** - an alternative method of specifying thresholds
+ * * **domain** - values outside the domain will be omitted
+ * * **cumulative** - if positive, each bin will contain all lesser bins
+ */
 export function binY<T>(outputs?: BinOutputs, options?: T & BinOptions): Transformed<T>;
 
+/**
+ * Aggregates continuous data—quantitative or temporal values such as
+ * temperatures or times—into discrete *x* and *y* bins and then computes
+ * summary statistics for each bin such as a count or sum. The bin transform is
+ * often used in conjunction with the rect mark, to make heatmaps.
+ *
+ * ```
+ * Plot.rect(penguins, Plot.bin({fill: "count"}, {x: "culmen_depth_mm", y: "culmen_length_mm"}))
+ * ```
+ *
+ * The following aggregation methods are supported:
+ *
+ * * *first* - the first value, in input order
+ * * *last* - the last value, in input order
+ * * *count* - the number of elements (frequency)
+ * * *distinct* - the number of distinct values
+ * * *sum* - the sum of values
+ * * *proportion* - the sum proportional to the overall total (weighted
+ *   frequency)
+ * * *proportion-facet* - the sum proportional to the facet total
+ * * *min* - the minimum value
+ * * *min-index* - the zero-based index of the minimum value
+ * * *max* - the maximum value
+ * * *max-index* - the zero-based index of the maximum value
+ * * *mean* - the mean value (average)
+ * * *median* - the median value
+ * * *mode* - the value with the most occurrences
+ * * *pXX* - the percentile value, where XX is a number in [00,99]
+ * * *deviation* - the standard deviation
+ * * *variance* - the variance per [Welford’s
+ *   algorithm](https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Welford's_online_algorithm)
+ * * *x* - the middle of the bin’s *x* extent (when binning on *x*)
+ * * *x1* - the lower bound of the bin’s *x* extent (when binning on *x*)
+ * * *x2* - the upper bound of the bin’s *x* extent (when binning on *x*)
+ * * *y* - the middle of the bin’s *y* extent (when binning on *y*)
+ * * *y1* - the lower bound of the bin’s *y* extent (when binning on *y*)
+ * * *y2* - the upper bound of the bin’s *y* extent (when binning on *y*)
+ * * a function to be passed the array of values for each bin and the extent of
+ *   the bin
+ * * an object with a *reduce* method, and optionally a *scope*
+ *
+ * Most aggregation methods require binding the output channel to an input
+ * channel; for example, if you want the **fill** output channel to be a *sum*
+ * (not merely a count), there should be a corresponding **fill** input channel
+ * specifying which values to sum. If there is not, *sum* will be equivalent to
+ * *count*.
+ *
+ * To control how *x* and *y* are divided into bins, the following options are supported:
+ *
+ * * **thresholds** - the threshold values; see below
+ * * **interval** - an alternative method of specifying thresholds
+ * * **domain** - values outside the domain will be omitted
+ * * **cumulative** - if positive, each bin will contain all lesser bins
+ *
+ * To pass separate binning options for *x* and *y*, the **x** and **y** input
+ * channels can be specified as an object with the options above and a **value**
+ * option to specify the input channel values. (🌶 NOT TYPED.)
+ */
 export function bin<T>(outputs?: BinOutputs, options?: T & BinOptions): Transformed<T>;
--- src/transforms/group.d.ts
+++ src/transforms/group.d.ts
@@ -3,18 +3,247 @@ import type {Reducer} from "../reducer.js";
 import type {Transformed} from "./basic.js";
 
 export interface GroupOutputOptions {
+  /**
+   * The data reducer; defaults to the subset of data corresponding to the
+   * group in input order.
+   */
   data?: Reducer | null;
+  /**
+   * The filter reducer, defaults to a check on empty groups. Use null to return
+   * all groups, for example to impute sum=0 for a line chart.
+   */
   filter?: Reducer | null;
+  /**
+   * The order in which the groups are generated.
+   */
   sort?: Reducer | null;
+  /**
+   * Reverse the order in which the groups are generated.
+   */
   reverse?: boolean;
 }
 
 export type GroupOutputs = ChannelReducers & GroupOutputOptions;
 
+/**
+ * Aggregates ordinal or categorical data—such as names—into groups and then
+ * computes summary statistics for each group such as a count or sum. Groups are
+ * computed on the first channel of *z*, *fill*, or *stroke*, if any. If none of
+ * *z*, *fill*, or *stroke* are channels, then all data (within each facet) is
+ * placed into a single group.
+ *
+ * ```js
+ * Plot.groupZ({x: "proportion"}, {fill: "species"})
+ * ```
+ *
+ * The following aggregation methods are supported:
+ *
+ * * *first* - the first value, in input order
+ * * *last* - the last value, in input order
+ * * *count* - the number of elements (frequency)
+ * * *sum* - the sum of values
+ * * *proportion* - the sum proportional to the overall total (weighted
+ *   frequency)
+ * * *proportion-facet* - the sum proportional to the facet total
+ * * *min* - the minimum value
+ * * *min-index* - the zero-based index of the minimum value
+ * * *max* - the maximum value
+ * * *max-index* - the zero-based index of the maximum value
+ * * *mean* - the mean value (average)
+ * * *median* - the median value
+ * * *mode* - the value with the most occurrences
+ * * *pXX* - the percentile value, where XX is a number in [00,99]
+ * * *deviation* - the standard deviation
+ * * *variance* - the variance per [Welford’s
+ *   algorithm](https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Welford's_online_algorithm)
+ * * a function - passed the array of values for each group
+ * * an object with a *reduce* method, an optionally a *scope*
+ *
+ * Most aggregation methods require binding the output channel to an input
+ * channel; for example, if you want the **r** output channel to be a *sum* (not
+ * merely a count), there should be a corresponding **r** input channel
+ * specifying which values to sum.
+ *
+ * You can control whether a channel is computed before or after grouping. If a
+ * channel is declared only in *options* (and it is not a special group-eligible
+ * channel such as *x*, *y*, *z*, *fill*, or *stroke*), it will be computed
+ * after grouping and be passed the grouped data: each datum is the array of
+ * input data corresponding to the current group.
+ *
+ * The default reducer for the **title** channel returns a summary list of the
+ * top 5 values with the corresponding number of occurrences.
+ *
+ * See also **groupX**, **groupY**, and **group** if you need to group by *x*,
+ * *y*, or both.
+ */
 export function groupZ<T>(outputs?: GroupOutputs, options?: T): Transformed<T>;
 
+/**
+ * Aggregates ordinal or categorical data—such as names—into groups and then
+ * computes summary statistics for each group such as a count or sum. Typically
+ * used with the **barY** mark for a categorical histogram. Groups are computed
+ * on *x* and the first channel of *z*, *fill*, or *stroke*, if any.
+ *
+ * ```js
+ * Plot.groupX({y: "sum"}, {x: "species", y: "body_mass_g"})
+ * ```
+ *
+ * The following aggregation methods are supported:
+ *
+ * * *first* - the first value, in input order
+ * * *last* - the last value, in input order
+ * * *count* - the number of elements (frequency)
+ * * *sum* - the sum of values
+ * * *proportion* - the sum proportional to the overall total (weighted
+ *   frequency)
+ * * *proportion-facet* - the sum proportional to the facet total
+ * * *min* - the minimum value
+ * * *min-index* - the zero-based index of the minimum value
+ * * *max* - the maximum value
+ * * *max-index* - the zero-based index of the maximum value
+ * * *mean* - the mean value (average)
+ * * *median* - the median value
+ * * *mode* - the value with the most occurrences
+ * * *pXX* - the percentile value, where XX is a number in [00,99]
+ * * *deviation* - the standard deviation
+ * * *variance* - the variance per [Welford’s
+ *   algorithm](https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Welford's_online_algorithm)
+ * * a function - passed the array of values for each group
+ * * an object with a *reduce* method, an optionally a *scope*
+ *
+ * Most aggregation methods require binding the output channel to an input
+ * channel; for example, if you want the **r** output channel to be a *sum* (not
+ * merely a count), there should be a corresponding **r** input channel
+ * specifying which values to sum.
+ *
+ * You can control whether a channel is computed before or after grouping. If a
+ * channel is declared only in *options* (and it is not a special group-eligible
+ * channel such as *x*, *y*, *z*, *fill*, or *stroke*), it will be computed
+ * after grouping and be passed the grouped data: each datum is the array of
+ * input data corresponding to the current group.
+ *
+ * The default reducer for the **title** channel returns a summary list of the
+ * top 5 values with the corresponding number of occurrences.
+ *
+ * The outputs may also include *filter* and *sort* options (with *reverse*) to
+ * specify which groups are generated. Use filter: null to generate empty
+ * groups, for example to impute sum=0 for a line chart. The *sort* option can
+ * also target the domain of an associated scale such as *x*, *fx* or *fy*.
+ *
+ * See also **groupZ**, **groupY**, and **group** if you need to group by
+ * series, *y*, or both *x* and *y*.
+ */
 export function groupX<T>(outputs?: GroupOutputs, options?: T): Transformed<T>;
 
+/**
+ * Aggregates ordinal or categorical data—such as names—into groups and then
+ * computes summary statistics for each group such as a count or sum. Typically
+ * used with the **barX** mark for a categorical histogram. Groups are computed
+ * on *y* and the first channel of *z*, *fill*, or *stroke*, if any.
+ *
+ * ```js
+ * Plot.groupY({x: "sum"}, {y: "species", x: "body_mass_g"})
+ * ```
+ *
+ * The following aggregation methods are supported:
+ *
+ * * *first* - the first value, in input order
+ * * *last* - the last value, in input order
+ * * *count* - the number of elements (frequency)
+ * * *sum* - the sum of values
+ * * *proportion* - the sum proportional to the overall total (weighted
+ *   frequency)
+ * * *proportion-facet* - the sum proportional to the facet total
+ * * *min* - the minimum value
+ * * *min-index* - the zero-based index of the minimum value
+ * * *max* - the maximum value
+ * * *max-index* - the zero-based index of the maximum value
+ * * *mean* - the mean value (average)
+ * * *median* - the median value
+ * * *mode* - the value with the most occurrences
+ * * *pXX* - the percentile value, where XX is a number in [00,99]
+ * * *deviation* - the standard deviation
+ * * *variance* - the variance per [Welford’s
+ *   algorithm](https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Welford's_online_algorithm)
+ * * a function - passed the array of values for each group
+ * * an object with a *reduce* method, an optionally a *scope*
+ *
+ * Most aggregation methods require binding the output channel to an input
+ * channel; for example, if you want the **r** output channel to be a *sum* (not
+ * merely a count), there should be a corresponding **r** input channel
+ * specifying which values to sum.
+ *
+ * You can control whether a channel is computed before or after grouping. If a
+ * channel is declared only in *options* (and it is not a special group-eligible
+ * channel such as *x*, *y*, *z*, *fill*, or *stroke*), it will be computed
+ * after grouping and be passed the grouped data: each datum is the array of
+ * input data corresponding to the current group.
+ *
+ * The default reducer for the **title** channel returns a summary list of the
+ * top 5 values with the corresponding number of occurrences.
+ *
+ * The outputs may also include *filter* and *sort* options (with *reverse*) to
+ * specify which groups are generated. Use filter: null to generate empty
+ * groups, for example to impute sum=0 for a line chart. The *sort* option can
+ * also target the domain of an associated scale such as *y*, *fx* or *fy*.
+ *
+ * See also **groupZ**, **groupX**, and **group** if you need to group by
+ * series, *x*, or both *x* and *y*.
+ */
 export function groupY<T>(outputs?: GroupOutputs, options?: T): Transformed<T>;
 
+/**
+ * Aggregates ordinal or categorical data—such as names—into groups and then
+ * computes summary statistics for each group such as a count or sum. Typically
+ * used with the **cell** mark for a categorical heatmap. Groups are computed on
+ * *x* and *y*, and the first channel of *z*, *fill*, or *stroke*, if any.
+ *
+ * ```js
+ * Plot.group({fill: "count"}, {x: "island", y: "species"})
+ * ```
+ *
+ * The following aggregation methods are supported:
+ *
+ * * *first* - the first value, in input order
+ * * *last* - the last value, in input order
+ * * *count* - the number of elements (frequency)
+ * * *sum* - the sum of values
+ * * *proportion* - the sum proportional to the overall total (weighted
+ *   frequency)
+ * * *proportion-facet* - the sum proportional to the facet total
+ * * *min* - the minimum value
+ * * *min-index* - the zero-based index of the minimum value
+ * * *max* - the maximum value
+ * * *max-index* - the zero-based index of the maximum value
+ * * *mean* - the mean value (average)
+ * * *median* - the median value
+ * * *mode* - the value with the most occurrences
+ * * *pXX* - the percentile value, where XX is a number in [00,99]
+ * * *deviation* - the standard deviation
+ * * *variance* - the variance per [Welford’s
+ *   algorithm](https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Welford's_online_algorithm)
+ * * a function - passed the array of values for each group
+ * * an object with a *reduce* method, an optionally a *scope*
+ *
+ * Most aggregation methods require binding the output channel to an input
+ * channel; for example, if you want the **r** output channel to be a *sum* (not
+ * merely a count), there should be a corresponding **r** input channel
+ * specifying which values to sum.
+ *
+ * You can control whether a channel is computed before or after grouping. If a
+ * channel is declared only in *options* (and it is not a special group-eligible
+ * channel such as *x*, *y*, *z*, *fill*, or *stroke*), it will be computed
+ * after grouping and be passed the grouped data: each datum is the array of
+ * input data corresponding to the current group.
+ *
+ * The default reducer for the **title** channel returns a summary list of the
+ * top 5 values with the corresponding number of occurrences.
+ *
+ * The outputs may also include *filter* and *sort* options (with *reverse*) to
+ * specify which groups are generated. Use filter: null to generate empty
+ * groups, for example to impute sum=0 for empty cells.
+ *
+ * See also **groupZ**, **groupX**, and **groupY** if you need to group by
+ * series, *x*, or *y*.
+ */
 export function group<T>(outputs?: GroupOutputs, options?: T): Transformed<T>;
