6b5deb00b010f29cfedf746c7f584141cb43d47e
Philippe Rivière <fil@rezo.net>
2023-03-25 18:25:42 +0100

    document hexgrid

--- src/marks/hexgrid.d.ts
+++ src/marks/hexgrid.d.ts
@@ -1,9 +1,18 @@
 import type {MarkOptions, RenderableMark} from "../mark.js";
 
+/** Options for the hexgrid mark. */
 export interface HexgridOptions extends MarkOptions {
+  /**
+   * The distance between centers of neighboring hexagons, in pixels; defaults
+   * to 20. Should reflect the binWidth of the **hexbin** transform.
+   */
   binWidth?: number;
 }
 
+/**
+ * The hexgrid mark can be used to support marks using the **hexbin** transform.
+ */
 export function hexgrid(options?: HexgridOptions): Hexgrid;
 
+/** The hexgrid mark. */
 export class Hexgrid extends RenderableMark {}
