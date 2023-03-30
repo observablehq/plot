100464b82c56e94332c7744b21feb0b0d0a09444
Mike Bostock <mbostock@gmail.com>
2023-03-30 08:45:49 -0700

    edits

--- src/marks/frame.d.ts
+++ src/marks/frame.d.ts
@@ -1,31 +1,40 @@
 import type {InsetOptions} from "../inset.js";
 import type {MarkOptions, RenderableMark} from "../mark.js";
 
+/** Options for the frame decoration mark. */
 export interface FrameOptions extends MarkOptions, InsetOptions {
   /**
-   * If not null, only renders that side of the frame. The *rx*, *ry*, *fill*,
-   * and *fillOpacity* options are then ignored.
+   * If null (default), the rectangular outline of the frame is drawn; otherwise
+   * the frame is drawn as a line only on the given side, and the **rx**,
+   * **ry**, **fill**, and **fillOpacity** options are ignored.
    */
   anchor?: "top" | "right" | "bottom" | "left" | null;
 
   /**
-   * Rounded corner radius, in pixels.
+   * The rounded corner [*x*-radius][1], either in pixels or as a percentage of
+   * the frame width. If **rx** is not specified, it defaults to **ry** if
+   * present, and otherwise draws square corners.
+   *
+   * [1]: https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/rx
    */
   rx?: number | string;
 
   /**
-   * Rounded corner radius, in pixels (if different from *rx*, for elliptical
-   * corners).
+   * The rounded corner [*y*-radius[1], either in pixels or as a percentage of
+   * the frame height. If **ry** is not specified, it defaults to **rx** if
+   * present, and otherwise draws square corners.
+   *
+   * [1]: https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/ry
    */
   ry?: number | string;
 }
 
 /**
- * Draws a simple frame around the entire plot (or facet).
- *
- * Use **stroke** to change the color, or **fill** to add a background color.
+ * Draws a rectangle around the plot’s frame, or if an **anchor** is given, a
+ * line on the given side. Useful for visual separation of facets, or in
+ * conjunction with axes and grids to fill the frame’s background.
  */
 export function frame(options?: FrameOptions): Frame;
 
-/** The frame mark */
+/** The frame decoration mark. */
 export class Frame extends RenderableMark {}
