e51af860509fd91cf9456cefa4d7ee218bcf417b
Mike Bostock <mbostock@gmail.com>
2023-03-30 09:02:59 -0700

    edits

--- src/marks/tick.d.ts
+++ src/marks/tick.d.ts
@@ -5,14 +5,18 @@ import type {Data, MarkOptions, RenderableMark} from "../mark.js";
 /** Options for the tickX mark. */
 export interface TickXOptions extends MarkOptions, Omit<InsetOptions, "insetLeft" | "insetRight"> {
   /**
-   * The horizontal position of the tick; a channel typically bound to the *x*
-   * scale.
+   * The required horizontal position of the tick; a channel typically bound to
+   * the *x* scale.
    */
   x?: ChannelValueSpec;
 
   /**
-   * The vertical position of the tick; an optional categorical channel
-   * typically bound to the *y* scale.
+   * The optional vertical position of the tick; a categorical channel typically
+   * bound to the *y* scale. If not specified, the tick spans the vertical
+   * extent of the frame; otherwise the *y* scale must be a *band* scale.
+   *
+   * If **y** represents quantitative or temporal values, use a ruleX mark
+   * instead.
    */
   y?: ChannelValueSpec;
 }
@@ -20,45 +24,52 @@ export interface TickXOptions extends MarkOptions, Omit<InsetOptions, "insetLeft
 /** Options for the tickY mark. */
 export interface TickYOptions extends MarkOptions, Omit<InsetOptions, "insetTop" | "insetBottom"> {
   /**
-   * The vertical position of the tick; a channel typically bound to the *y*
-   * scale.
+   * The required vertical position of the tick; a channel typically bound to
+   * the *y* scale.
    */
   y?: ChannelValueSpec;
 
   /**
-   * The horizontal position of the tick; an optional categorical channel
-   * typically bound to the *x* scale.
+   * The optional horizontal position of the tick; a categorical channel
+   * typically bound to the *x* scale. If not specified, the tick spans the
+   * horizontal extent of the frame; otherwise the *x* scale must be a *band*
+   * scale.
+   *
+   * If **x** represents quantitative or temporal values, use a ruleY mark
+   * instead.
    */
   x?: ChannelValueSpec;
 }
 
 /**
- * Returns a new tickX mark for the given *data* and *options*. The **x**
- * channel specifies the horizontal position of the tick, and the optional **y**
- * categorical channel specifies its vertical position. For example, to create
- * a horizontal barcode plot of penguins’ weights:
+ * Returns a new horizontally-positioned tickX mark (a vertical line, |) for the
+ * given *data* and *options*. The **x** channel specifies the tick’s horizontal
+ * position and defaults to identity, assuming that *data* = [*x₀*, *x₁*, *x₂*,
+ * …]; the optional **y** categorical channel specifies its vertical position.
+ * For example, for a horizontal barcode plot of penguins’ weights:
  *
  * ```js
- * Plot.tickX(penguins, { x: "body_mass_g", y: "sex", stroke: "species" })
+ * Plot.tickX(penguins, {x: "body_mass_g", y: "sex", stroke: "species"})
  * ```
  *
- * **x** defaults to identity, assuming that *data* contains numbers. If the
- * secondary dimension **y** is quantitative instead of ordinal, use **ruleX**.
+ * If **y** represents quantitative or temporal values, use a ruleX mark
+ * instead.
  */
 export function tickX(data?: Data, options?: TickXOptions): TickX;
 
 /**
- * Returns a new tickY mark for the given *data* and *options*. The **y**
- * channel specifies the vertical position of the tick, and the optional **x**
- * categorical channel specifies its horizontal position. For example, to create
- * a vertical barcode plot of penguins’ weights:
+ * Returns a new vertically-positioned tickY mark (a horizontal line, —) for the
+ * given *data* and *options*. The **y** channel specifies the vertical position
+ * of the tick and defaults to identity, assuming that *data* = [*y₀*, *y₁*,
+ * *y₂*, …]; the optional **x** categorical channel specifies its horizontal
+ * position. For example, fpr a vertical barcode plot of penguins’ weights:
  *
  * ```js
- * Plot.tickY(penguins, { y: "body_mass_g", x: "sex", stroke: "species" })
+ * Plot.tickY(penguins, {y: "body_mass_g", x: "sex", stroke: "species"})
  * ```
  *
- * **y** defaults to identity, assuming that *data* contains numbers. If the
- * secondary dimension **x** is quantitative instead of ordinal, use **ruleY**.
+ * If **x** represents quantitative or temporal values, use a ruleY mark
+ * instead.
  */
 export function tickY(data?: Data, options?: TickYOptions): TickY;
 
