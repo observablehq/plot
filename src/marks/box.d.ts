9dc8ccf85925b92d47f50d80b4fa344d02113cca
Mike Bostock <mbostock@gmail.com>
2023-03-30 19:53:12 -0700

    edits

--- src/marks/box.d.ts
+++ src/marks/box.d.ts
@@ -11,45 +11,43 @@ export type BoxXOptions = DotOptions & BarXOptions & TickXOptions & RuleXOptions
 export type BoxYOptions = DotOptions & BarYOptions & TickYOptions & RuleYOptions;
 
 /**
- * Draws horizontal boxplots where *x* is quantitative and *y* is ordinal (if
- * present). The box mark is a composite mark consisting of four marks:
+ * Returns a box mark that draws horizontal boxplots where **x** is quantitative
+ * or temporal and **y**, if present, is ordinal. The box mark is a compound
+ * mark consisting of four marks:
  *
- * - a **ruleY** representing the extreme values (not including outliers)
- * - a **barX** representing the interquartile range (trimmed to the data)
- * - a **tickX** representing the median value, and
- * - a **dot** representing outliers, if any
+ * - a rule representing the extreme values (not including outliers),
+ * - a bar representing the interquartile range (trimmed to the data),
+ * - a tick representing the median value, and
+ * - a dot representing outliers, if any.
  *
  * The given *options* are passed through to these underlying marks, with the
  * exception of the following options:
  *
  * - **fill** - the fill color of the bar; defaults to gray
  * - **fillOpacity** - the fill opacity of the bar; defaults to 1
- * - **stroke** - the stroke color of the rule, tick, and dot; defaults to
- *   *currentColor*
- * - **strokeOpacity** - the stroke opacity of the rule, tick, and dot; defaults
- *   to 1
- * - **strokeWidth** - the stroke width of the tick; defaults to 1
+ * - **stroke** - the stroke color of the rule, tick, and dot; defaults to *currentColor*
+ * - **strokeOpacity** - the stroke opacity of the rule, tick, and dot; defaults to 1
+ * - **strokeWidth** - the stroke width of the tick; defaults to 2
  */
 export function boxX(data?: Data, options?: BoxXOptions): CompoundMark;
 
 /**
- * Draws vertical boxplots where *y* is quantitative and *x* is ordinal (if
- * present). The box mark is a composite mark consisting of four marks:
+ * Returns a box mark that draws vertical boxplots where **y** is quantitative
+ * or temporal and **x**, if present, is ordinal. The box mark is a compound
+ * mark consisting of four marks:
  *
- * - a **ruleX** representing the extreme values (not including outliers)
- * - a **barY** representing the interquartile range (trimmed to the data)
- * - a **tickY** representing the median value, and
- * - a **dot** representing outliers, if any
+ * - a rule representing the extreme values (not including outliers),
+ * - a bar representing the interquartile range (trimmed to the data),
+ * - a tick representing the median value, and
+ * - a dot representing outliers, if any.
  *
  * The given *options* are passed through to these underlying marks, with the
  * exception of the following options:
  *
  * - **fill** - the fill color of the bar; defaults to gray
  * - **fillOpacity** - the fill opacity of the bar; defaults to 1
- * - **stroke** - the stroke color of the rule, tick, and dot; defaults to
- *   *currentColor*
- * - **strokeOpacity** - the stroke opacity of the rule, tick, and dot; defaults
- *   to 1
- * - **strokeWidth** - the stroke width of the tick; defaults to 1
+ * - **stroke** - the stroke color of the rule, tick, and dot; defaults to *currentColor*
+ * - **strokeOpacity** - the stroke opacity of the rule, tick, and dot; defaults to 1
+ * - **strokeWidth** - the stroke width of the tick; defaults to 2
  */
 export function boxY(data?: Data, options?: BoxYOptions): CompoundMark;
