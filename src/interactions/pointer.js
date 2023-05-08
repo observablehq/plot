7a7291192c51aae2db899a8734b7f2ba0622ac17
Mike Bostock <mbostock@gmail.com>
2023-05-08 11:27:23 -0700

    only one pointer across facets

--- src/interactions/pointer.js
+++ src/interactions/pointer.js
@@ -11,22 +11,42 @@ function pointerK(kx, ky, {px, py, maxRadius = 40, channels, ...options} = {}) {
     render(index, scales, values, dimensions, context) {
       const mark = this;
       const svg = context.ownerSVGElement;
+      const faceted = index.fi != null;
+      const facetState = faceted ? getFacetState(mark, svg) : null;
       const {x: X0, y: Y0, x1: X1, y1: Y1, x2: X2, y2: Y2, px: X = X0, py: Y = Y0} = values;
       const [cx, cy] = applyFrameAnchor(this, dimensions);
       let sticky = false;
       let i; // currently focused index
       let g; // currently rendered mark
 
-      function render(ii) {
+      function render(ii, ri) {
+        // When faceting, if more than one pointer would be visible, only show
+        // this one if it is the closest. This is a simple linear scan because
+        // we don’t expect many facets with simultaneously-visible pointers.
+        if (faceted) {
+          if (ii == null) {
+            facetState.delete(index.fi);
+          } else {
+            facetState.set(index.fi, ri);
+            if (facetState.size > 1) {
+              for (const [fi, r] of facetState) {
+                if (fi !== index.fi && r < ri) {
+                  ii = null;
+                  break;
+                }
+              }
+            }
+          }
+        }
         if (i === ii) return; // the tooltip hasn’t moved
         i = ii;
         const I = i == null ? [] : [i];
-        if (index.fi != null) (I.fx = index.fx), (I.fy = index.fy), (I.fi = index.fi);
+        if (faceted) (I.fx = index.fx), (I.fy = index.fy), (I.fi = index.fi);
         const r = mark.render(I, scales, values, dimensions, context);
         if (g) {
-          const p = g.parentNode;
-          if (p !== svg) {
+          if (faceted) {
             // when faceting, preserve swapped mark and facet transforms
+            const p = g.parentNode;
             const ft = g.getAttribute("transform");
             const mt = r.getAttribute("transform");
             ft ? r.setAttribute("transform", ft) : r.removeAttribute("transform");
@@ -39,7 +59,7 @@ function pointerK(kx, ky, {px, py, maxRadius = 40, channels, ...options} = {}) {
 
       function pointermove(event) {
         if (sticky || (event.pointerType === "mouse" && event.buttons === 1)) return; // dragging
-        const [xp, yp] = pointof(event, g.parentNode === svg ? g.parentNode : g); // detect faceting
+        const [xp, yp] = pointof(event, faceted ? g : g.parentNode);
         let ii = null;
         let ri = maxRadius * maxRadius;
         for (const j of index) {
@@ -50,7 +70,7 @@ function pointerK(kx, ky, {px, py, maxRadius = 40, channels, ...options} = {}) {
           const rj = dx * dx + dy * dy;
           if (rj <= ri) (ii = j), (ri = rj);
         }
-        render(ii);
+        render(ii, ri);
       }
 
       function pointerdown(event) {
@@ -89,4 +109,17 @@ export function pointerX(options) {
 
 export function pointerY(options) {
   return pointerK(0.01, 1, options);
+}
+
+const facetStateByMark = new WeakMap();
+
+// This isolates facet state per-mark, per-plot. Most of the time a separate
+// pointer will be instantiated per mark, but it’s possible to reuse the same
+// pointer instance with multiple marks so we protect against it.
+function getFacetState(mark, svg) {
+  let stateBySvg = facetStateByMark.get(mark);
+  if (!stateBySvg) facetStateByMark.set(mark, (stateBySvg = new WeakMap()));
+  let state = stateBySvg.get(svg);
+  if (!state) stateBySvg.set(svg, (state = new Map()));
+  return state;
 }
