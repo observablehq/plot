b09d5a667020b5ea71e4dd70cc03191d8e724ff3
Mike Bostock <mbostock@gmail.com>
2023-05-13 15:23:14 -0400

    Update PlotRender.js

--- docs/components/PlotRender.js
+++ docs/components/PlotRender.js
@@ -127,7 +127,7 @@ export default {
       ...this.options,
       className: "plot"
     };
-    if (this.defer || typeof document !== "undefined") {
+    if (this.defer) { // || typeof document !== "undefined") {
       const mounted = (el) => {
         disconnect(); // remove old listeners
         function observed() {
