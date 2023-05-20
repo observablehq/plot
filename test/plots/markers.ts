a4b4435e67f12965c99d7181bc15368001f3f400
Mike Bostock <mbostock@gmail.com>
2023-05-20 08:07:22 -0700

    arrow-reverse marker

--- test/plots/markers.ts
+++ test/plots/markers.ts
@@ -1,17 +1,17 @@
 import * as Plot from "@observablehq/plot";
 
 export async function markerRuleX() {
-  return Plot.ruleX([1, 2, 3], {marker: "arrow", inset: 3}).plot();
+  return Plot.ruleX([1, 2, 3], {marker: "arrow-reverse", inset: 3}).plot();
 }
 
 export async function markerRuleY() {
-  return Plot.ruleY([1, 2, 3], {marker: "arrow", inset: 3}).plot();
+  return Plot.ruleY([1, 2, 3], {marker: "arrow-reverse", inset: 3}).plot();
 }
 
 export async function markerTickX() {
-  return Plot.tickX([1, 2, 3], {marker: "arrow", inset: 3}).plot();
+  return Plot.tickX([1, 2, 3], {marker: "arrow-reverse", inset: 3}).plot();
 }
 
 export async function markerTickY() {
-  return Plot.tickY([1, 2, 3], {marker: "arrow", inset: 3}).plot();
+  return Plot.tickY([1, 2, 3], {marker: "arrow-reverse", inset: 3}).plot();
 }
