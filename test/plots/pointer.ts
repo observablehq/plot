d11a38622af56cd6021557e4f01e28360ace02e6
Mike Bostock <mbostock@gmail.com>
2023-05-24 10:53:54 -0700

    textarea

--- test/plots/pointer.ts
+++ test/plots/pointer.ts
@@ -1,5 +1,6 @@
 import * as Plot from "@observablehq/plot";
 import * as d3 from "d3";
+import {html} from "htl";
 
 export async function pointerRenderCompose() {
   const penguins = await d3.csv<any>("data/penguins.csv", d3.autoType);
@@ -26,7 +27,8 @@ export async function pointerRenderCompose() {
 
 export async function pointerViewof() {
   const penguins = await d3.csv<any>("data/penguins.csv", d3.autoType);
+  const textarea = html`<textarea rows=10 style="width: 640px; resize: none;">`;
   const plot = Plot.dot(penguins, {x: "culmen_length_mm", y: "culmen_depth_mm", tip: true}).plot();
-  plot.oninput = () => console.log(plot.value);
-  return plot;
+  plot.oninput = () => textarea.value = JSON.stringify(plot.value, null, 2);
+  return html`<figure>${plot}${textarea}</figure>`;
 }
