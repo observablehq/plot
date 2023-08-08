090ad3a053bca2068b4b4e12852bec348a2da858
Philippe Rivière <fil@rezo.net>
2023-08-09 01:42:09 +0200

    normalize links in JSDoc comments: (#1794)
    - use link number format
    - move github/d3 to d3js.org
    - add https

--- src/format.d.ts
+++ src/format.d.ts
@@ -2,11 +2,12 @@
  * Returns a function that formats a given month number (from 0 = January to 11
  * = December) according to the specified *locale* and *format*.
  *
- * @param locale a [BCP 47 language tag](https://tools.ietf.org/html/bcp47);
- * defaults to U.S. English.
- * @param format a [month
- * format](https://tc39.es/ecma402/#datetimeformat-objects): either *2-digit*,
- * *numeric*, *narrow*, *short*, *long*; defaults to *short*.
+ * @param locale a [BCP 47 language tag][1]; defaults to U.S. English.
+ * @param format a [month format][2]: either *2-digit*, *numeric*, *narrow*,
+ * *short*, *long*; defaults to *short*.
+ *
+ * [1]: https://tools.ietf.org/html/bcp47
+ * [2]: https://tc39.es/ecma402/#datetimeformat-objects
  */
 export function formatMonth(
   locale?: string,
@@ -17,11 +18,12 @@ export function formatMonth(
  * Returns a function that formats a given week day number (from 0 = Sunday to 6
  * = Saturday) according to the specified *locale* and *format*.
  *
- * @param locale a [BCP 47 language tag](https://tools.ietf.org/html/bcp47);
- * defaults to U.S. English.
- * @param format a [weekday
- * format](https://tc39.es/ecma402/#datetimeformat-objects): either *narrow*,
- * *short*, or *long*; defaults to *short*.
+ * @param locale a [BCP 47 language tag][1]; defaults to U.S. English.
+ * @param format a [weekday format][2]: either *narrow*, *short*, or *long*;
+ * defaults to *short*.
+ *
+ * [1]: https://tools.ietf.org/html/bcp47
+ * [2]: https://tc39.es/ecma402/#datetimeformat-objects
  */
 export function formatWeekday(locale?: string, format?: "long" | "short" | "narrow"): (i: number) => string;
 
