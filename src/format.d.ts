84e3f7073f768bdc2cab8ed086897b552595feca
Philippe Rivière <fil@rezo.net>
2023-08-09 17:45:33 +0200

    avoid repetition of format options (#1796)
    (as suggested by https://github.com/observablehq/plot/pull/1794#discussion_r1287786741; note that the second line is still in the ugly "paragraph" mode in VSCode, even though it's shorter that the first line 🤷

--- src/format.d.ts
+++ src/format.d.ts
@@ -2,12 +2,11 @@
  * Returns a function that formats a given month number (from 0 = January to 11
  * = December) according to the specified *locale* and *format*.
  *
- * @param locale a [BCP 47 language tag][1]; defaults to U.S. English.
- * @param format a [month format][2]: either *2-digit*, *numeric*, *narrow*,
- * *short*, *long*; defaults to *short*.
- *
  * [1]: https://tools.ietf.org/html/bcp47
  * [2]: https://tc39.es/ecma402/#datetimeformat-objects
+ *
+ * @param locale - a [BCP 47 language tag][1]; defaults to U.S. English.
+ * @param format - a [month format][2]; defaults to *short*.
  */
 export function formatMonth(
   locale?: string,
@@ -18,12 +17,11 @@ export function formatMonth(
  * Returns a function that formats a given week day number (from 0 = Sunday to 6
  * = Saturday) according to the specified *locale* and *format*.
  *
- * @param locale a [BCP 47 language tag][1]; defaults to U.S. English.
- * @param format a [weekday format][2]: either *narrow*, *short*, or *long*;
- * defaults to *short*.
- *
  * [1]: https://tools.ietf.org/html/bcp47
  * [2]: https://tc39.es/ecma402/#datetimeformat-objects
+ *
+ * @param locale a [BCP 47 language tag][1]; defaults to U.S. English.
+ * @param format a [weekday format][2]; defaults to *short*.
  */
 export function formatWeekday(locale?: string, format?: "long" | "short" | "narrow"): (i: number) => string;
 
