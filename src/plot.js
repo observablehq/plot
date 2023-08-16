604c6e0ac17c241de18d9ff0e336e97da089f182
Philippe Rivière <fil@rezo.net>
2023-08-16 17:04:30 +0200

    naming

--- src/plot.js
+++ src/plot.js
@@ -221,10 +221,10 @@ export function plot(options = {}) {
     addScaleChannels(newChannelsByScale, stateByMark, options, (key) => newByScale.has(key));
     addScaleChannels(channelsByScale, stateByMark, options, (key) => newByScale.has(key));
     const newScaleDescriptors = inheritScaleLabels(createScales(newChannelsByScale, options), scaleDescriptors);
-    const {scales: newIntantiatedScales, ...newScales} = createScaleFunctions(newScaleDescriptors);
+    const {scales: newExposedScales, ...newScales} = createScaleFunctions(newScaleDescriptors);
     Object.assign(scaleDescriptors, newScaleDescriptors);
     Object.assign(scales, newScales);
-    Object.assign(scales.scales, newIntantiatedScales);
+    Object.assign(scales.scales, newExposedScales);
   }
 
   // Sort and filter the facets to match the fx and fy domains; this is needed
