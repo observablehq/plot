dbf913b7e594ae3da6756a92fcadc1d56b8a166c
Philippe Rivière <fil@rezo.net>
2023-08-24 00:58:46 +0200

    tests for coverage (#1825)
    * stack rejects an invalid order
    
    * these tests used the default labelAnchor
    
    * test curve tension
    
    * coverage: valid and invalid curves
    
    * move curve tests to a features directory
    
    * test channel scales: false, invalid
    
    * invalid domain sort options
    
    * The aspectRatio option rejects unsupported scale type
    
    * test style
    
    * test polish
    
    * add missing test
    
    ---------
    
    Co-authored-by: Mike Bostock <mbostock@gmail.com>

--- test/transforms/stack-test.ts
+++ test/transforms/stack-test.ts
@@ -1,15 +1,19 @@
 import * as Plot from "@observablehq/plot";
 import assert from "assert";
 
-it("Plot.stack returns the expected values", () => {
+it("stackY(options) returns the expected values", () => {
   const y = [1, 2, -2, -1];
   const {
     channels: {
       y1: {value: Y1},
       y2: {value: Y2}
     }
-  } = Plot.barY(y, Plot.stackY({y})).initialize();
+  } = (Plot.barY(y, Plot.stackY({y})) as any).initialize();
   assert.deepStrictEqual(y, [1, 2, -2, -1]);
   assert.deepStrictEqual(Y1, Float64Array.of(0, 1, 0, -2));
   assert.deepStrictEqual(Y2, Float64Array.of(1, 3, -2, -3));
+});
+
+it("stackY({order}) rejects an invalid order", () => {
+  assert.throws(() => Plot.barY([], {y: 1, order: 42 as any}), /^Error: invalid order: 42$/);
 });
