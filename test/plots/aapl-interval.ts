59d871c936b6c86fbbe4d9405f3b3392c81ebf4f
Mike Bostock <mbostock@gmail.com>
2023-08-23 18:00:42 -0700

    prettier

--- test/plots/aapl-interval.ts
+++ test/plots/aapl-interval.ts
@@ -3,11 +3,5 @@ import * as d3 from "d3";
 
 export async function aaplInterval() {
   const aapl = await d3.csv<any>("data/aapl.csv", d3.autoType);
-  return Plot.areaY(aapl.slice(0, 81), {
-    x: "Date",
-    y1: "Low",
-    y2: "High",
-    interval: "day",
-    curve: "step"
-  }).plot();
+  return Plot.areaY(aapl.slice(0, 81), {x: "Date", y1: "Low", y2: "High", interval: "day", curve: "step"}).plot();
 }
