854a5b67986c8ff2c62ed551eef433f1036f1408
Mike Bostock <mbostock@gmail.com>
2023-08-30 11:26:08 -0700

    delegate render to derived mark

--- test/plots/highlight.ts
+++ test/plots/highlight.ts
@@ -1,61 +1,83 @@
 import * as Plot from "@observablehq/plot";
 import * as d3 from "d3";
 
+function highlight({selected = {}, unselected = {}, ...options}: any = {}) {
+  return {
+    ...options,
+    creator() {
+      const s = (this.selected = new this.constructor(this.data, {...options, ...selected}));
+      const u = (this.unselected = new this.constructor(this.data, {...options, ...unselected}));
+      for (const c in selected) if (c in s.channels) this.channels[`selected:${c}`] = s.channels[c];
+      for (const c in unselected) if (c in u.channels) this.channels[`unselected:${c}`] = u.channels[c];
+    },
+    render(index, scales, values, dimensions, context, next) {
+      const svg = context.ownerSVGElement;
+      const s = this.selected;
+      const u = this.unselected;
+      const svalues = {...values};
+      const uvalues = {...values};
+      for (const c in selected) svalues[c] = c in s.channels ? values[`selected:${c}`] : null;
+      for (const c in unselected) uvalues[c] = c in u.channels ? values[`unselected:${c}`] : null;
+      const g = next(index, scales, values, dimensions, context);
+      let ug: ChildNode;
+      let sg: ChildNode;
+
+      function pointerenter(event) {
+        if (!ug) {
+          g.replaceWith((sg = document.createComment("selected")));
+          sg.parentNode.insertBefore((ug = document.createComment("unselected")), sg);
+        }
+        pointermove(event);
+      }
+
+      function pointermove(event) {
+        const [px, py] = d3.pointer(event);
+        const {x, y} = values;
+        const S = [];
+        const U = [];
+
+        for (const i of index) {
+          (Math.hypot(x[i] - px, y[i] - py) < 100 ? S : U).push(i);
+        }
+
+        ug.replaceWith((ug = u.render(U, scales, uvalues, dimensions, context)));
+        sg.replaceWith((sg = s.render(S, scales, svalues, dimensions, context)));
+      }
+
+      function pointerleave() {
+        if (ug) {
+          ug.replaceWith(g);
+          sg.remove();
+          sg = null;
+          ug = null;
+        }
+      }
+
+      svg.addEventListener("pointerenter", pointerenter);
+      svg.addEventListener("pointermove", pointermove);
+      svg.addEventListener("pointerleave", pointerleave);
+      const f = svg.ownerDocument.createDocumentFragment();
+      f.append(g, svg.ownerDocument.createComment("brush"));
+      return f as any; // TODO return a G element instead?
+    }
+  };
+}
+
 export async function highlightDot() {
   const penguins = await d3.csv<any>("data/penguins.csv", d3.autoType);
   return Plot.plot({
+    color: {legend: true},
     marks: [
-      Plot.dot(penguins, {
-        x: "culmen_length_mm",
-        y: "culmen_depth_mm",
-        r: d3.randomLcg(42),
-        fill: "species",
-        fillOpacity: 0.1,
-        stroke: "species",
-        render(index, scales, values, dimensions, context, next) {
-          const svg = context.ownerSVGElement;
-          const ovalues = {...values, fill: null, stroke: null};
-          const g = next(index, scales, values, dimensions, context);
-          const gg = svg.ownerDocument.createElementNS(svg.namespaceURI, "g");
-          gg.appendChild(g);
-          let og: Element | Comment; // =
-          let ig: Element | Comment; // = gg.appendChild(document.createComment("selected"));
-
-          function pointerenter(event) {
-            // g.replaceWith(gg);
-            g.remove();
-            if (!ig) ig = gg.appendChild(document.createComment("unselected"));
-            if (!og) og = gg.appendChild(document.createComment("unselected"));
-            pointermove(event);
-          }
-
-          function pointermove(event) {
-            const [px, py] = d3.pointer(event);
-            const {x, y} = values;
-            const I = [];
-            const O = [];
-
-            for (const i of index) {
-              (Math.hypot(x[i] - px, y[i] - py) < 100 ? I : O).push(i);
-            }
-
-            ig.replaceWith((ig = next(O, scales, ovalues, dimensions, context)));
-            og.replaceWith((og = next(I, scales, values, dimensions, context)));
-          }
-
-          function pointerleave() {
-            // gg.replaceWith(g);
-            if (ig) ig.remove(), (ig = null);
-            if (og) og.remove(), (og = null);
-            gg.insertBefore(g, gg.firstChild);
-          }
-
-          svg.addEventListener("pointerenter", pointerenter);
-          svg.addEventListener("pointermove", pointermove);
-          svg.addEventListener("pointerleave", pointerleave);
-          return gg;
-        }
-      })
+      Plot.dot(
+        penguins,
+        highlight({
+          x: "culmen_length_mm",
+          y: "culmen_depth_mm",
+          stroke: "species",
+          selected: {r: d3.randomLcg(), symbol: "asterisk"},
+          unselected: {stroke: "#ccc"}
+        })
+      )
     ]
   });
 }
