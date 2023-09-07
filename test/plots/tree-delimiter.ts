5aea07dd3b64c9601981dd044e9f2f7079622992
Mike Bostock <mbostock@gmail.com>
2023-09-07 09:02:59 -0700

    add delimiter test

--- test/plots/tree-delimiter.ts
+++ test/plots/tree-delimiter.ts
@@ -3,18 +3,43 @@ import * as Plot from "@observablehq/plot";
 export async function treeDelimiter() {
   return Plot.plot({
     axis: null,
-    height: 100,
+    height: 120,
     margin: 10,
     marginLeft: 40,
     marginRight: 190,
     marks: [
       Plot.tree(
         [
-          "foo;bar;http://www.example.com",
-          "foo;bar;https://www.example.com/posts/1",
-          "foo;baz;https://www.example.com/posts/2"
+          "foo;bar;https://example.com",
+          "foo;bar;https://example.com/posts/1",
+          "foo;baz;https://example.com/posts/2",
+          "foo;bar\\;baz;https://example2.com", // “bar;baz” should be a single node
+          "foo;bar/baz;https://example4.com", // "bar/baz" should be a single node, distinct from “bar;baz”
+          "foo;bar\\/baz;https://example3.com" // “bar\/baz” should be a single node
         ],
         {delimiter: ";"}
+      )
+    ]
+  });
+}
+
+export async function treeDelimiter2() {
+  return Plot.plot({
+    axis: null,
+    height: 120,
+    margin: 10,
+    marginLeft: 40,
+    marginRight: 190,
+    marks: [
+      Plot.tree(
+        [
+          "foo/bar/https:\\/\\/example.com",
+          "foo/bar/https:\\/\\/example.com\\/posts\\/1",
+          "foo/baz/https:\\/\\/example.com\\/posts\\/2",
+          "foo/bar;baz/https:\\/\\/example2.com", // “bar;baz” should be a single node
+          "foo/bar\\/baz/https:\\/\\/example4.com", // "bar/baz" should be a single node, distinct from “bar;baz”
+          "foo/bar\\\\/baz/https:\\/\\/example3.com" // “bar\/baz” should be a single node
+        ]
       )
     ]
   });
