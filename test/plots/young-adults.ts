f1cee3daa8c8e63bb582c28058ec897bf26e6a65
Philippe Rivière <fil@rezo.net>
2023-11-06 15:15:43 +0100

    adopt Plot.find

--- test/plots/young-adults.ts
+++ test/plots/young-adults.ts
@@ -2,26 +2,11 @@ import * as Plot from "@observablehq/plot";
 import * as d3 from "d3";
 
 export async function youngAdults() {
-  const ilc_lvps08 = await d3.csv<any>("data/ilc_lvps08.csv", d3.autoType);
   const ages = ["Y16-19", "Y20-24", "Y25-29"];
   const geos = ["SE", "FR", "DE", "TR", "IT"];
-  const wider = d3
-    .flatRollup(
-      ilc_lvps08.filter(({age, geo}) => ages.includes(age) && geos.includes(geo)),
-      (v) =>
-        Object.fromEntries(
-          d3
-            .index(
-              v.map(({sex, OBS_VALUE}) => [sex, OBS_VALUE]),
-              (d) => d[0]
-            )
-            .values()
-        ),
-      (d) => d.geo,
-      (d) => d.age,
-      (d) => d.TIME_PERIOD
-    )
-    .map(([geo, age, year, d]) => ({geo, age, year, ...d}));
+  const ilc_lvps08 = await d3.csv<any>("data/ilc_lvps08.csv", (d) =>
+    ages.includes(d.age) && geos.includes(d.geo) ? d3.autoType(d) : null
+  );
   return Plot.plot({
     title: "Share of young adults living with their parents (%)",
     subtitle: "…by age and sex. Data: Eurostat",
@@ -32,20 +17,28 @@ export async function youngAdults() {
     y: {grid: true, nice: true},
     marks: [
       Plot.frame(),
-      Plot.differenceY(wider, {
-        x: "year",
-        y1: "F",
-        y2: "M",
-        sort: {fx: {value: "y", reduce: "mean"}},
-        fillOpacity: 0.5,
-        negativeFill: "grey",
-        positiveFill: "age",
-        fx: "geo",
-        curve: "basis",
-        z: (d) => `${d.age}, ${d.geo}`,
-        channels: {M: "M", F: "F"},
-        tip: true
-      })
+      Plot.differenceY(
+        ilc_lvps08,
+        Plot.groupX(
+          {
+            y1: Plot.find((d) => d.sex === "F"),
+            y2: Plot.find((d) => d.sex === "M"),
+            positiveFill: "first"
+          },
+          {
+            x: "TIME_PERIOD",
+            y: "OBS_VALUE",
+            negativeFill: "grey",
+            positiveFill: "age",
+            z: "age",
+            sort: {fx: {value: "y", reduce: "mean"}},
+            fillOpacity: 0.5,
+            fx: "geo",
+            curve: "basis",
+            tip: true
+          }
+        )
+      )
     ]
   });
 }
