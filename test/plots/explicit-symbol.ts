49da12a8c1335e18cb1f651f16acac2fbcfce3b8
Mike Bostock <mbostock@gmail.com>
2024-11-23 08:07:29 -0800

    prettier

--- test/plots/explicit-symbol.ts
+++ test/plots/explicit-symbol.ts
@@ -4,12 +4,19 @@ import * as d3 from "d3";
 // A channel of explicit symbols does not show in the tip nor has a legend.
 export async function explicitSymbol() {
   const penguins = await d3.csv<any>("data/penguins.csv", d3.autoType);
-  return Plot.dot(
-    penguins,
-    Plot.dodgeY({
-      x: "culmen_length_mm",
-      symbol: (d) => (d.sex === "FEMALE" ? "square" : "star"),
-      tip: true
-    })
-  ).plot({height: 220, symbol: {legend: true}});
+  return Plot.plot({
+    height: 220,
+    symbol: {legend: true},
+    marks: [
+      Plot.dot(
+        penguins,
+        Plot.dodgeY({
+          x: "culmen_length_mm",
+          symbol: (d) => (d.sex === "FEMALE" ? "square" : "star"),
+          fill: "sex",
+          tip: true
+        })
+      )
+    ]
+  });
 }
