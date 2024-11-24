11dadc505ef89a67922ad5bf95b5547a3a2bb39b
Mike Bostock <mbostock@gmail.com>
2024-11-23 19:14:06 -0800

    plot.rescale

--- test/plots/index.ts
+++ test/plots/index.ts
@@ -259,6 +259,7 @@ export * from "./raster-vapor.js";
 export * from "./raster-walmart.js";
 export * from "./rect-band.js";
 export * from "./reducer-scale-override.js";
+export * from "./rescale.js";
 export * from "./rounded-rect.js";
 export * from "./seattle-precipitation-density.js";
 export * from "./seattle-precipitation-rule.js";
