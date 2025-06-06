936bdd8f157f8b641d77310dfe6683d3e028c636
Philippe Rivière <fil@rezo.net>
2025-06-06 14:57:32 +0200

    a human fix: when y is undefined, default to y: reduce

--- src/marks/area.js
+++ src/marks/area.js
@@ -2,7 +2,7 @@ import {area as shapeArea} from "d3";
 import {create} from "../context.js";
 import {maybeCurve} from "../curve.js";
 import {Mark} from "../mark.js";
-import {first, identity, indexOf, maybeZ, second} from "../options.js";
+import {first, indexOf, maybeZ, second} from "../options.js";
 import {
   applyDirectStyles,
   applyIndirectStyles,
@@ -77,15 +77,11 @@ export function area(data, options) {
 }
 
 export function areaX(data, options) {
-  const {y = indexOf, ...rest} = maybeDenseIntervalY(
-    options?.interval != null ? {...options, x: options.x ?? identity} : options
-  );
+  const {y = indexOf, ...rest} = maybeDenseIntervalY(options);
   return new Area(data, maybeStackX(maybeIdentityX({...rest, y1: y, y2: undefined}, y === indexOf ? "x2" : "x")));
 }
 
 export function areaY(data, options) {
-  const {x = indexOf, ...rest} = maybeDenseIntervalX(
-    options?.interval != null ? {...options, y: options.y ?? identity} : options
-  );
+  const {x = indexOf, ...rest} = maybeDenseIntervalX(options);
   return new Area(data, maybeStackY(maybeIdentityY({...rest, x1: x, x2: undefined}, x === indexOf ? "y2" : "y")));
 }
