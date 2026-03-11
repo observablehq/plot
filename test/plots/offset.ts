92f4cd79679319728d97564e3276891ac4e593b3
Philippe Rivière <fil@rezo.net>
2026-03-11 13:06:45 +0100

    add test

---
+++ test/plots/offset.ts
@@ -0,0 +1,33 @@
+import * as Plot from "@observablehq/plot";
+import * as d3 from "d3";
+import {html} from "htl";
+import {setOffset} from "../../src/style.js";
+
+export async function offsets() {
+  const penguins = await d3.csv<any>("data/penguins.csv", d3.autoType);
+  return html`${Array.from([0, 0.5], (offset) => {
+    setOffset(offset);
+    const color = offset ? "blue" : "red";
+    return Plot.plot({
+      style: "position: absolute; top: 70px;",
+      grid: color,
+      height: 300,
+      marks: [
+      Plot.axisX({fill: color, stroke: color}),
+      Plot.axisY({fill: color, stroke: color}),
+        Plot.rect(
+          penguins,
+          Plot.bin(
+            {fillOpacity: "count"},
+            {
+              x: "culmen_depth_mm",
+              y: "culmen_length_mm",
+              fill: color,
+              thresholds: 50
+            }
+          )
+        )
+      ]
+    });
+  })}`;
+}
