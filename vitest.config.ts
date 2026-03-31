6213f4a4d36dc5c2581c660c758f71a8d047e103
Mike Bostock <mbostock@gmail.com>
2026-03-30 20:56:24 -0700

    don’t need alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,6 @@
-import path from "path";
 import {defineConfig} from "vitest/config";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@observablehq/plot": path.resolve("./src/index.js")
-    }
-  },
   test: {
     environment: "jsdom",
     setupFiles: ["./test/setup.js"],
