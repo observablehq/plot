1cb1ee07347a014b925ce7c5f61de0a066cc9d75
Mike Bostock <mbostock@gmail.com>
2026-04-01 09:57:36 -0700

    lineY; inline raw

--- test/plots/year-format.ts
+++ test/plots/year-format.ts
@@ -3,16 +3,18 @@ import * as d3 from "d3";
 import {test} from "test/plot";
 
 test(async function yearFormat() {
-  const raw = await d3.csv<any>("data/bls-industry-unemployment.csv", d3.autoType);
   const data = d3
     .rollups(
-      raw,
-      (v) => d3.median(v, (d) => d.unemployed),
+      await d3.csv<any>("data/bls-industry-unemployment.csv", d3.autoType),
+      (D) => d3.median(D, (d) => d.unemployed),
       (d) => d.date.getUTCFullYear(),
       (d) => d.industry
     )
     .flatMap(([year, industries]) => industries.map(([industry, unemployed]) => ({year, industry, unemployed})));
   return Plot.plot({
-    marks: [Plot.line(data, {x: "year", y: "unemployed", stroke: "industry", marker: true, tip: true}), Plot.ruleY([0])]
+    marks: [
+      Plot.lineY(data, {x: "year", y: "unemployed", stroke: "industry", marker: true, tip: true}),
+      Plot.ruleY([0])
+    ]
   });
 });
