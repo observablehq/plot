8ecf8f69da4d02140132dda33039552541e4d9a2
Philippe Rivière <fil@rezo.net>
2026-04-01 16:15:32 +0200

    update test

--- test/transforms/dodge-test.ts
+++ test/transforms/dodge-test.ts
@@ -1,6 +1,5 @@
+import {assert, it} from "vitest";
 import * as Plot from "@observablehq/plot";
-import assert from "assert";
-import it from "../jsdom.js";
 
 it("dodgeY throws on non-positive constant r", () => {
   assert.throws(() => Plot.dot([1, 2, 3], Plot.dodgeY({x: Plot.identity, r: -1})).plot(), /invalid dodge radius/);
