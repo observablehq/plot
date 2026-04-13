b1d4a92a7ac2b2105947bf21356dc47d3c1a2ee2
Philippe Rivière <fil@rezo.net>
2026-04-13 11:16:06 +0200

    prasnier

--- test/plots/rect-rotate.ts
+++ test/plots/rect-rotate.ts
@@ -15,7 +15,13 @@ class RotatedRect extends Plot.Rect {
     super(data, options);
     this.rx = rx;
   }
-  render(index: number[], scales: Plot.ScaleFunctions, values: Plot.ChannelValues, dimensions: Plot.Dimensions, context: Plot.Context) {
+  render(
+    index: number[],
+    scales: Plot.ScaleFunctions,
+    values: Plot.ChannelValues,
+    dimensions: Plot.Dimensions,
+    context: Plot.Context
+  ) {
     const g = super.render(index, scales, values, dimensions, context);
     if (g) {
       let i = 0;
@@ -39,8 +45,24 @@ test(function rectRotate() {
     axis: null,
     color: {domain: d3.range(10), range: colors},
     marks: [
-      new RotatedRect(d3.range(10), {x1: Plot.identity, x2: (d) => d + 1, y1: 0, y2: 1, fill: Plot.identity, stroke: "white", inset: -2, rx: 8}),
-      Plot.vector(d3.range(10), {x: (d) => d + 0.5, y: 0.5, rotate: (d) => d * 10, stroke: "white", length: 10, strokeWidth: 1.5})
+      new RotatedRect(d3.range(10), {
+        x1: Plot.identity,
+        x2: (d) => d + 1,
+        y1: 0,
+        y2: 1,
+        fill: Plot.identity,
+        stroke: "white",
+        inset: -2,
+        rx: 8
+      }),
+      Plot.vector(d3.range(10), {
+        x: (d) => d + 0.5,
+        y: 0.5,
+        rotate: (d) => d * 10,
+        stroke: "white",
+        length: 10,
+        strokeWidth: 1.5
+      })
     ]
   });
 });
