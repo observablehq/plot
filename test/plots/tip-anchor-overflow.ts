94d8bd77d668370e50cedbc856428b2d93e75659
Philippe Rivière <fil@rezo.net>
2026-04-15 10:47:48 +0200

    better test

--- test/plots/tip-anchor-overflow.ts
+++ test/plots/tip-anchor-overflow.ts
@@ -2,11 +2,19 @@ import * as Plot from "@observablehq/plot";
 import {test} from "test/plot";
 
 test(async function tipAnchorOverflow() {
-  return Plot.rectX([1, 1, 1, 1, 1], {
+  const plot = Plot.rectX([1, 1, 1, 1, 1], {
     x: Plot.identity,
     fill: Plot.indexOf,
     title: () =>
       "Lorem ipsum lorem ipsum lorem ipsum Lorem ipsum lorem ipsum lorem ipsum Lorem ipsum lorem ipsum lorem ipsum",
     tip: true
-  }).plot({height: 100, marginTop: 20});
+  }).plot({height: 80, marginTop: 20, axis: null});
+  plot.dispatchEvent(
+    new PointerEvent("pointermove", {
+      pointerType: "mouse",
+      clientX: 580,
+      clientY: 30
+    })
+  );
+  return Object.assign(plot, {ready: new Promise((resolve) => setTimeout(resolve, 100))});
 });
